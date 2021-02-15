e8b0a757c7a13837232600517ddfc551a60a20b7
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2021-02-15 12:09:54 +0800

    Update mod.rs

--- src/system/caching/mod.rs
+++ src/system/caching/mod.rs
@@ -13,10 +13,16 @@
 ///  limitations under the License.
 
 use std::{
-    collections::HashMap,
+    collections::{
+        BTreeMap,
+        HashMap
+    },
     fmt::Debug,
     hash::Hash,
-    sync::Arc
+    sync::{
+        Arc,
+        Mutex
+    }
 };
 
 use tokio::{
@@ -29,66 +35,143 @@ use tokio::{
     }
 };
 
-mod entry;
-
-crate use entry::CachedEntry;
-
 #[derive(Debug, Clone)]
 crate struct SystemCache<K: Hash + Eq + Debug + Send + Clone, V: Clone + Debug + Send> {
-    crate cache_table: HashMap<K, CachedEntry<V>>,
+    shared: Arc<SharedInternal<K, V>>
+}
 
-    crate background_task: Arc<Notify>
+#[derive(Debug)]
+struct SharedInternal<K: Hash + Eq + Debug + Send + Clone, V: Clone + Debug + Send> {
+    state: Mutex<StateInternal<K, V>>,
+    background_task: Notify
+}
+
+#[derive(Debug)]
+struct StateInternal<K: Hash + Eq + Debug + Send + Clone, V: Clone + Debug + Send> {
+    entries: HashMap<K, SystemCacheEntry<V>>,
+    expirations: BTreeMap<(Instant, u64), K>,
+    next_id: u64
+}
+
+#[derive(Debug)]
+struct SystemCacheEntry<V: Clone + Debug + Send> {
+    entry_id: u64,
+    data: V,
+    expires_at: Option<Instant>
 }
 
 impl<K: Hash + Eq + Debug + Send + Clone + 'static, V: Clone + Debug + Send + 'static> SystemCache<K, V> {
     crate fn new() -> Self {
-        let cache = Self {
-            cache_table: HashMap::new(),
+        let shared = Arc::new(
+            SharedInternal {
+                state: Mutex::new(
+                    StateInternal {
+                        entries: HashMap::new(),
+                        expirations: BTreeMap::new(),
+                        next_id: 0
+                    }
+                ),
+                background_task: Notify::new()
+            }
+        );
 
-            background_task: Arc::new(Notify::new())
-        };
+        tokio::spawn(purged_expired_entries_task(shared.clone()));
 
-        cache
+        Self {
+            shared
+        }
     }
 
-    crate fn contains_key(&self, key: &K) -> bool {
-        self
-            .cache_table
-            .get(key)
-            .is_some()
+    crate fn get(&self, key: &K) -> Option<V> {
+        let state = self.shared.state.lock().unwrap();
+
+        state.entries.get(key).map(|entry| entry.data.clone())
     }
 
-    crate fn get(&mut self, key: &K) -> Option<&V> {
-        self
-            .cache_table
-            .get(&key)
-            .map(|entry| &entry.value)
-    }
+    crate fn insert(&self, key: K, value: V, expire: Option<Duration>) {
+        let mut state = self.shared.state.lock().unwrap();
 
-    crate fn insert(&mut self, key: K, value: V, lifetime: Option<Duration>) -> Option<V> {
-        if let Some(lifetime) = lifetime {
-            return self
-                .cache_table
-                .insert(key, CachedEntry::new(value, lifetime))
-                .map(|entry| entry.value)
+        let id = state.next_id;
+        state.next_id += 1;
+
+        let mut notify = false;
+
+        let expire_at = expire.map(|duration| {
+            let when = Instant::now() + duration;
+
+            notify = state
+                .next_expiration()
+                .map(|expiration| expiration > when)
+                .unwrap_or(true);
+
+            state.expirations.insert((when, id), key.clone());
+
+            when
+        });
+
+        let previous_key = state.entries.insert(
+            key,
+            SystemCacheEntry {
+                entry_id: id,
+                data: value,
+                expires_at: expire_at
+            }
+        );
+
+        if let Some(previous) = previous_key {
+            if let Some(when) = previous.expires_at {
+                state.expirations.remove(&(when, previous.entry_id));
+            }
         }
 
-        self.background_task.notified();
+        drop(state);
 
-        None
+        if notify {
+            self.shared.background_task.notify_one();
+        }
     }
+}
 
-    crate fn purge_expired_items(&mut self) -> Option<Instant> {
-        let current_time = Instant::now();
+impl<K: Hash + Eq + Debug + Send + Clone, V: Clone + Debug + Send> SharedInternal<K, V> {
+    fn purge_expired_entries(&self) -> Option<Instant> {
+        let mut state = self.state.lock().unwrap();
 
-        while let Some((key, CachedEntry { expiration_time, .. })) = self.cache_table.clone().iter().next() {
-            if *expiration_time > current_time {
-                return Some(*expiration_time);
+        let state = &mut *state;
+        let now = Instant::now();
+
+        while let Some((&(when, id), key)) = state.expirations.iter().next() {
+            if when > now {
+                return Some(when);
             }
 
-            self.cache_table.remove(key);
+            state.entries.remove(key);
+            state.expirations.remove(&(when, id));
         }
 
         None
+    }
+}
+
+impl<K: Hash + Eq + Debug + Send + Clone, V: Clone + Debug + Send> StateInternal<K, V> {
+    fn next_expiration(&self) -> Option<Instant> {
+        self
+            .expirations
+            .keys()
+            .next()
+            .map(|entry| entry.0)
+    }
+}
+
+async fn purged_expired_entries_task<K: Hash + Eq + Debug + Send + Clone, V: Clone + Debug + Send>(shared: Arc<SharedInternal<K, V>>) {
+    loop {
+        if let Some(when) = shared.purge_expired_entries() {
+            tokio::select! {
+                _ = tokio::time::sleep_until(when) => {},
+                _ = shared.background_task.notified() => {}
+            }
+        }
+        else {
+            shared.background_task.notified().await;
+        }
     }
 }
