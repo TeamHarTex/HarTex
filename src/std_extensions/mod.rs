6bbaac86d967b7ba5ebfacfb0f687993e803fb45
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2021-02-15 18:54:13 +0800

    Update mod.rs

--- src/std_extensions/mod.rs
+++ src/std_extensions/mod.rs
@@ -1,1 +1,3 @@
 mod format_as_iec_80000_13_prefix_postfix_decimal_multiplier_string;
+
+crate use format_as_iec_80000_13_prefix_postfix_decimal_multiplier_string::FormatAsIec8000013PrefixPostfixDecimalMultiplerString;
