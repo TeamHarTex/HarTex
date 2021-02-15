6494220785b228387b0ff0448ac9271933eb37c4
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2021-02-15 18:50:47 +0800

    Update format_as_iec_80000_13_prefix_postfix_decimal_multiplier_string.rs

--- src/std_extensions/format_as_iec_80000_13_prefix_postfix_decimal_multiplier_string.rs
+++ src/std_extensions/format_as_iec_80000_13_prefix_postfix_decimal_multiplier_string.rs
@@ -7,3 +7,19 @@ impl FormatAsIec8000013PrefixPostfixDecimalMultiplerString for i8 {
         self.to_string()
     }
 }
+
+impl FormatAsIec8000013PrefixPostfixDecimalMultiplerString for i16 {
+    fn format_as_iec_80000_13_prefix_postfix_decimal_multiplier_string(&self) -> String {
+        let mut index = 0;
+        let mut value = *self;
+        let mut decimal_place = 0;
+
+        while value >= 1000 && index < MULTIPLIERS.len() {
+            index += 1;
+            decimal_place = (value % 100) / 100;
+            value /= 1000;
+        }
+
+        format!("{}.{}{}", value, decimal_place, MULTIPLIERS[index])
+    }
+}
