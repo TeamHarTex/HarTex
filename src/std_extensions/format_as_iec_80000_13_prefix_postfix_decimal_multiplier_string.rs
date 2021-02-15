1f75250d08a8d2c9c4e8c725e423b9639461b257
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2021-02-15 18:39:12 +0800

    Update format_as_iec_80000_13_prefix_postfix_decimal_multiplier_string.rs

--- src/std_extensions/format_as_iec_80000_13_prefix_postfix_decimal_multiplier_string.rs
+++ src/std_extensions/format_as_iec_80000_13_prefix_postfix_decimal_multiplier_string.rs
@@ -1,3 +1,9 @@
 crate trait FormatAsIec8000013PrefixPostfixDecimalMultiplierString {
     fn format_as_iec_80000_13_prefix_postfix_decimal_multiplier_string(&self) -> String;
 }
+
+impl FormatAsIec8000013PrefixPostfixDecimalMultiplerString for i8 {
+    fn format_as_iec_80000_13_prefix_postfix_decimal_multiplier_string(&self) -> String {
+        self.to_string()
+    }
+}
