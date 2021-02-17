bae99dc9337fb7e6bdb5c60692c6a906e39c2c27
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2021-02-17 12:34:31 +0800

    Update rank.rs

--- src/plugins/levelling_system/rank.rs
+++ src/plugins/levelling_system/rank.rs
@@ -222,10 +222,11 @@ async fn levelling_system_rank_command(ctx: CommandContext<'_>, user: Option<Str
 
     let expected_text_width = u32::from_f32(temp_width).unwrap();
     let out_of_xp_text_position = 860 - expected_text_width;
+    let xp_expected_text_width = u32::from_f32(xp_temp_width).unwrap();
     
     draw_text_mut(&mut image, Rgb([164u8, 176u8, 176u8]), out_of_xp_text_position, 170, Scale { x: 28.5, y: 28.5 }, &montserrat_regular, &text);
-    draw_text_mut(&mut image, Rgb([255u8, 255u8, 255u8]), out_of_xp_text_position - 50, 170, Scale { x: 28.5, y: 28.5 } , &montserrat_bold, &experience.format_as_iec_80000_13_prefix_postfix_decimal_multiplier_string());
-
+    draw_text_mut(&mut image, Rgb([255u8, 255u8, 255u8]), out_of_xp_text_position - xp_expected_text_width, 170, Scale { x: 28.5, y: 28.5 } , &montserrat_bold, &experience.format_as_iec_80000_13_prefix_postfix_decimal_multiplier_string());
+    
     image.save("rank_card/card.png")?;
 
     ctx.http_client
