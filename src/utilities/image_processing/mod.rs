ca1ab5c74d650746c26d8df562de987776e164a5
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2021-02-19 11:16:09 +0800

    Add files via upload

---
+++ src/utilities/image_processing/mod.rs
@@ -0,0 +1,17 @@
+///  Copyright 2020 - 2021 The HarTex Project Developers
+///
+///  Licensed under the Apache License, Version 2.0 (the "License");
+///  you may not use this file except in compliance with the License.
+///  You may obtain a copy of the License at
+///
+///      http://www.apache.org/licenses/LICENSE-2.0
+///
+///  Unless required by applicable law or agreed to in writing, software
+///  distributed under the License is distributed on an "AS IS" BASIS,
+///  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+///  See the License for the specific language governing permissions and
+///  limitations under the License.
+
+crate fn pixel_is_in_circle(pixel_x: u32, pixel_y: u32, circle_radius: u32) -> bool {
+    pixel_x.pow(2) + pixel_y.pow(2) >= circle_radius.pow(2)
+}
