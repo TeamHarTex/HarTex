5c29cbb976ef20c43c3db83cd7e7bde672acf453
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2021-02-20 11:47:32 +0800

    Update get_guild_leaderoard.rs

--- src/system/twilight_http_client_extensions/levelling_system/get_guild_leaderoard.rs
+++ src/system/twilight_http_client_extensions/levelling_system/get_guild_leaderoard.rs
@@ -12,11 +12,21 @@
 ///  See the License for the specific language governing permissions and
 ///  limitations under the License.
 
+use std::{
+    future::Future,
+    pin::Pin,
+    task::{
+        Context,
+        Poll
+    }
+};
+
 use twilight_model::{
     id::GuildId
 };
 
 use crate::{
+    logging::logger::Logger,
     models::{
         levelling_system::Leaderboard
     },
@@ -30,6 +40,48 @@ use crate::{
 
 crate struct GetGuildLeaderboard {
     future: Option<Pending<Leaderboard>>,
-    
+
     guild_id: GuildId
+}
+
+impl GetGuildLeaderboard {
+    crate fn new(guild_id: GuildId) -> Self {
+        Self {
+            future: None,
+
+            guild_id
+        }
+    }
+
+    fn start(&mut self) -> ClientExtensionResult<()> {
+        Logger::log_debug("Attempting to create connection to HarTexBetaLevellingSystem database.");
+
+        self.future.replace(Box::pin(
+            request(self.guild_id)
+        ));
+
+        Ok(())
+    }
+}
+
+impl Future for GetGuildLeaderboard {
+    type Output = ClientExtensionResult<Leaderboard>;
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        loop {
+            if let Some(future) = self.as_mut().future.as_mut() {
+                return future.as_mut().poll(cx);
+            }
+
+            if let Err(error) = self.start() {
+                return Poll::Ready(Err(error));
+            }
+        }
+    }
+}
+
+unsafe impl Send for GetGuildLeaderboard {}
+
+async fn request(guild_id: GuildId) -> ClientExtensionResult<Leaderboard> {
+    todo!()
 }
