0e10fa857360269267be87847e6149b080316f6a
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2021-02-20 22:10:53 +0800

    Update mod.rs

--- src/xml_deserialization/plugin_management/levels/censorship/mod.rs
+++ src/xml_deserialization/plugin_management/levels/censorship/mod.rs
@@ -11,3 +11,12 @@
 ///  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 ///  See the License for the specific language governing permissions and
 ///  limitations under the License.
+
+extern crate serde;
+extern crate quick_xml;
+
+#[derive(Debug, Serialize, Deserialize)]
+crate struct CensorshipLevel {
+    #[serde(rename = "Level")]
+    level: u8
+}
