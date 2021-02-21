8681f1e1effca49ae84f37a1c0fdf51820b6fdf5
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2021-02-21 11:44:33 +0800

    Update has_role_permissions.rs

--- src/command_system/precommand_checks/has_role_permissions.rs
+++ src/command_system/precommand_checks/has_role_permissions.rs
@@ -14,95 +14,177 @@
 
 use std::{
     future::Future,
-    pin::Pin,
-    sync::Arc
+    pin::Pin
+};
+
+use sha3::{
+    Digest,
+    Sha3_224
+};
+
+use twilight_cache_inmemory::InMemoryCache;
+
+use twilight_mention::{
+    Mention,
+    parse::ParseMention,
 };
 
 use twilight_model::{
-    guild::Role
-};
-
-use crate::{
-    command_system::{
-        CommandContext,
-        CommandError,
-        PrecommandCheckParameters
-    },
-    system::{
-        twilight_http_client_extensions::{
-            GetGuildConfiguration
-        },
-        SystemResult
-    },
-    xml_deserialization::{
-        BotConfig
+    id::{
+        RoleId,
+        UserId
     }
 };
 
-use super::PrecommandCheck;
+use crate::command_system::{
+    parser::{
+        Arguments
+    },
+    Command,
+    CommandContext,
+    CommandError,
+    PrecommandCheckParameters
+};
 
-crate struct HasRolePermissions;
+use crate::system::{
+    model::{
+        infractions::InfractionType
+    },
+    twilight_http_client_extensions::{
+        AddUserInfraction,
+        GetGuildConfiguration
+    },
+    SystemResult
+};
 
-impl PrecommandCheck for HasRolePermissions {
-    fn execute_check<'asynchronous_trait>(ctx: CommandContext<'asynchronous_trait>, params: PrecommandCheckParameters)
-        -> Pin<Box<dyn Future<Output = SystemResult<()>> + Send + 'asynchronous_trait>> {
-        Box::pin(has_role_permissions(ctx, params))
+use crate::utilities::FutureResult;
+
+use crate::xml_deserialization::BotConfig;
+
+crate struct MuteCommand;
+
+impl Command for MuteCommand {
+    fn fully_qualified_name(&self) -> String {
+        String::from("mute")
+    }
+
+    fn aliases(&self) -> Vec<String> {
+        vec![String::from("dmmute")]
+    }
+
+    fn execute_command<'asynchronous_trait>(ctx: CommandContext<'asynchronous_trait>,
+                                            mut arguments: Arguments<'asynchronous_trait>, _cache: InMemoryCache)
+        -> Pin<Box<dyn Future<Output=SystemResult<()>> + Send + 'asynchronous_trait>> {
+        let to_parse = arguments.next().unwrap();
+        let user_id = if let Ok(uid) = UserId::parse(to_parse) {
+            Some(uid)
+        }
+        else if let Ok(uid) = to_parse.parse() {
+            Some(UserId(uid))
+        }
+        else {
+            None
+        };
+
+        let reason = arguments.into_remainder().unwrap().to_string();
+
+        Box::pin(infractions_mute_command(ctx, user_id, reason))
+    }
+
+    fn precommand_check<'asynchronous_trait, C>(ctx: CommandContext<'asynchronous_trait>,
+                                                params: PrecommandCheckParameters, check: C)
+        -> Pin<Box<dyn Future<Output=SystemResult<()>> + Send + 'asynchronous_trait>>
+        where
+            C: Fn(CommandContext<'asynchronous_trait>, PrecommandCheckParameters)
+                -> Pin<Box<dyn Future<Output=SystemResult<()>> + Send + 'asynchronous_trait>> {
+        Box::pin(FutureResult::resolve(check(ctx, params)))
     }
 }
 
-async fn has_role_permissions(ctx: CommandContext<'asynchronous_trait>, params: PrecommandCheckParameters)
+async fn infractions_mute_command(ctx: CommandContext<'_>, user_id: Option<UserId>, reason: String)
     -> SystemResult<()> {
-    if let Some(gid) = ctx.message.guild_id {
-        let config_str = ctx.http_client.clone().get_guild_configuration(gid).await?;
+    let guild_id = ctx.message.guild_id.unwrap();
+    let channel_id = ctx.message.channel_id;
 
-        let config = quick_xml::de::from_str::<BotConfig>(&config_str)?;
+    let guild_name = if let Ok(Some(guild)) = ctx.http_client.clone().guild(guild_id).await {
+        guild.name
+    }
+    else {
+        "unknown".to_string()
+    };
 
-        if let Some(cache) = params.cache {
-            let mut roles = ctx.http_client
-                .clone()
-                .guild_member(gid, ctx.author.id)
-                .await?
-                .unwrap()
-                .roles
-                .iter()
-                .map(|&role| {
-                    cache.role(role).unwrap()
-                })
-                .collect::<Vec<Arc<Role>>>();
+    if let Some(uid) = user_id {
+        let guild_config = ctx.http_client.clone().get_guild_configuration(guild_id).await?;
+        let config = quick_xml::de::from_str::<BotConfig>(guild_config.as_str())?;
 
-            roles.sort_by(|previous, now| {
-                now.position.cmp(&previous.position)
-            });
+        let warning_id = format!("{:x}", Sha3_224::digest(
+            format!("{}{}{}", guild_id, uid, reason).as_str().as_bytes()));
 
-            if let Some(role_permission_levels) = config.role_permission_levels {
-                if role_permission_levels.contains_key(&roles.first().unwrap().id.0) {
-                    if let Some(minimum_permission_level) = params.minimum_permission_level {
-                        let set_level = role_permission_levels.get(&roles.first().unwrap().id.0).unwrap();
+        if let Some(plugins) = config.plugins {
+            return if let Some(infraction_plugin) = plugins.infractions_plugin {
+                if let Some(mute_command) = infraction_plugin.mute_command {
+                    if let Some(muted_role) = mute_command.muted_role {
+                        let role_id = RoleId(muted_role.role_id);
 
-                        if minimum_permission_level <= *set_level {
+                        if let Ok(Some(user)) = ctx.http_client.user(uid).await {
+                            ctx.http_client.clone().add_user_infraction(warning_id.clone(),
+                                                                        guild_id, uid, reason.clone(),
+                                                                        InfractionType::Mute).await?;
+
+                            ctx.http_client.clone().add_guild_member_role(guild_id, uid, role_id).await?;
+
+                            if let Some(role_to_remove) = mute_command.role_to_remove {
+                                ctx.http_client.clone().remove_guild_member_role(guild_id, uid,
+                                                                                 RoleId(role_to_remove.role_id)).await?;
+                            }
+
+                            ctx.http_client.clone().create_message(ctx.message.channel_id)
+                                .content(
+                                    format!(
+                                        "<:green_check:705623382682632205> Successfully muted user {} (ID: `{}`). Reason: `{}`. Infraction ID: `{}`",
+                                        user.mention(), uid.0, reason, warning_id))?
+                                .allowed_mentions().replied_user(false).build().reply(ctx.message.id).await?;
+
+                            let dm_channel = ctx.http_client.clone().create_private_channel(uid).await?;
+
+                            ctx.http_client.clone()
+                                .create_message(dm_channel.id)
+                                .content(format!("You have been muted in guild `{}` (ID: `{}`). Reason: `{}`",
+                                                 guild_name, guild_id.0, reason))?
+                                .await?;
+                            
                             Ok(())
                         }
                         else {
-                            Err(box CommandError("The user does not have the minimum required permission to execute this command.".to_string()))
+                            Err(box CommandError("User not found.".to_string()));
                         }
                     }
                     else {
-                        Err(box CommandError("Minimum permission level required cannot be none, as required by the check.".to_string()))
+                        Err(box CommandError("Muted role is not set.".to_string()));
                     }
                 }
                 else {
-                    Err(box CommandError("The user does not have the minimum required permission to execute this command".to_string()))
+                    Err(box CommandError("Cannot find MuteCommand property in configuration.".to_string()));
                 }
             }
             else {
-                Err(box CommandError("Permission levels are not set.".to_string()))
+                Err(box CommandError("Cannot find configuration for InfractionsPlugin.".to_string()));
             }
         }
         else {
-            Err(box CommandError("Cache cannot be none, as required by the check.".to_string()))
+            ctx.http_client
+                .clone().create_message(channel_id)
+                .content("<:red_x:705623424675872859> Muted role is not set.")?
+                .allowed_mentions()
+                .replied_user(false)
+                .build()
+                .reply(ctx.message.id)
+                .await?;
+
+            Err(box CommandError("Muted role is not set.".to_string()))
         }
     }
     else {
-        Err(box CommandError("Guild ID cannot be none.".to_string()))
+        Err(box CommandError("User ID cannot be none.".to_string()))
     }
 }
