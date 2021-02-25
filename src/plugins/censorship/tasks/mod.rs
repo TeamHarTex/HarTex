f312645f868d758ee5a1e97fd6effb62e5ddb355
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2021-02-25 17:37:29 +0800

    Update mod.rs

--- src/plugins/censorship/tasks/mod.rs
+++ src/plugins/censorship/tasks/mod.rs
@@ -12,6 +12,7 @@
 //!  See the License for the specific language governing permissions and
 //!  limitations under the License.
 
+mod invite_detection;
 mod zalgo_detection;
 
 crate use zalgo_detection::ZalgoDetectionTask;
