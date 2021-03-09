4d5a47ae15896c3d243576a935257be9c0ef1e46
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2021-03-09 22:36:25 +0800

    Update mod.rs

--- src/macros/mod.rs
+++ src/macros/mod.rs
@@ -12,7 +12,8 @@
 //!  See the License for the specific language governing permissions and
 //!  limitations under the License.
 
-crate macro handle_command {
-
+crate macro execute_command {
+    ($command:ty, ) => {
+    
+    }
 }
-
