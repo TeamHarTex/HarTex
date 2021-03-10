d2c278feef0a80f5511c6740fe70ff4ad1a3b541
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2021-03-10 22:27:02 +0800

    Update mod.rs

--- src/macros/mod.rs
+++ src/macros/mod.rs
@@ -13,8 +13,11 @@
 //!  limitations under the License.
 
 crate macro execute_command {
-    ($command_struct:ty, $context:expr, $arguments:expr, $cache:expr, $http_client:expr, $message: expr, $emitter:expr) => {
-        match $command_struct::execute_command($context, $arguments, $cache).await {
+    ($command_struct:ident, $context:expr, $arguments:expr, $cache:expr, $http_client:expr,
+        $message: expr, $emitter:expr, $command_name:literal) => {
+        use $crate::command_system::Command;
+
+        match <$command_struct>::execute_command($context, $arguments, $cache).await {
             Ok(()) => {
                 let guild = match $http_client.guild($message.guild_id.unwrap()).await? {
                     Some(guild) => guild.name,
@@ -22,25 +25,27 @@ crate macro execute_command {
                 };
 
                 $emitter.event($crate::command_system::events::events::SystemEvent::CommandExecuted(box $crate::system::model::payload::CommandExecuted {
-                    command: $command_struct::fully_qualified_name(),
+                    command: $command_name,
                     guild_name: guild,
                     context: $context
                 }))
             },
             Err(error) => {
                 $emitter.event($crate::command_system::events::eventsSystemEvent::CommandFailed(box $crate::system::model::payload::CommandFailed {
-                    command: $command_struct::fully_qualified_name(),
+                    command: $command_name,
                     error: format!("{}", error)
                 }))
             }
         }
     },
 
-    ($command_struct:ty, $precommand_checks:expr, $precommand_checks_parameters:expr, $context:expr,
-        $arguments:expr, $cache:expr, $http_client:expr, $message: expr, $emitter:expr) => {
-        match $command_struct::precommand_checks($context, $precommand_checks_parameters, $precommand_checks).await {
+    ($command_struct:ident, $precommand_checks:expr, $precommand_checks_parameters:expr, $context:expr,
+        $arguments:expr, $cache:expr, $http_client:expr, $message: expr, $emitter:expr, $command_name:literal) => {
+        use $crate::command_system::Command;
+
+        match <$command_struct>::precommand_checks($context, $precommand_checks_parameters, $precommand_checks).await {
             Ok(()) => {
-                match $command_struct::execute_command($context, $arguments, $cache).await {
+                match <$command_struct>::execute_command($context, $arguments, $cache).await {
                     Ok(()) => {
                         let guild = match $http_client.guild($message.guild_id.unwrap()).await? {
                             Some(guild) => guild.name,
@@ -48,14 +53,14 @@ crate macro execute_command {
                         };
 
                         $emitter.event($crate::command_system::events::events::SystemEvent::CommandExecuted(box $crate::system::model::payload::CommandExecuted {
-                            command: $command_struct::fully_qualified_name(),
+                            command: $command_name,
                             guild_name: guild,
                             context: $context
                         }))
                     },
                     Err(error) => {
                         $emitter.event($crate::command_system::events::events::SystemEvent::CommandFailed(box $crate::system::model::payload::CommandFailed {
-                            command: $command_struct::fully_qualified_name(),
+                            command: $command_name,
                             error: format!("{}", error)
                         }))
                     }
@@ -63,7 +68,7 @@ crate macro execute_command {
             },
             Err(error) => {
                 $emitter.event($crate::command_system::events::events::SystemEvent::CommandFailed(box $crate::system::model::payload::CommandFailed {
-                    command: $command_struct::fully_qualified_name(),
+                    command: $command_name,
                     error: format!("{}", error)
                 }))
             }
