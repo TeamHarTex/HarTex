5ab27c001e0bbd8e4c99672733b2df4e11a32b00
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2021-03-11 10:38:43 +0800

    Add files via upload

---
+++ src/plugins/infractions/nodm/mod.rs
@@ -0,0 +1,45 @@
+//!  Copyright 2020 - 2021 The HarTex Project Developers
+//!
+//!  Licensed under the Apache License, Version 2.0 (the "License");
+//!  you may not use this file except in compliance with the License.
+//!  You may obtain a copy of the License at
+//!
+//!      http://www.apache.org/licenses/LICENSE-2.0
+//!
+//!  Unless required by applicable law or agreed to in writing, software
+//!  distributed under the License is distributed on an "AS IS" BASIS,
+//!  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//!  See the License for the specific language governing permissions and
+//!  limitations under the License.
+
+mod ban;
+mod clean_ban;
+mod kick;
+mod mban;
+mod mkick;
+mod mmute;
+mod munban;
+mod munmute;
+mod mute;
+mod mwarn;
+mod tempban;
+mod tempmute;
+mod unban;
+mod unmute;
+mod warn;
+
+crate use ban::BanCommand as NodmBanCommand;
+crate use clean_ban::CleanBanCommand as NodmCleanBanCommand;
+crate use kick::KickCommand as NodmKickCommand;
+crate use mban::MbanCommand as NodmMbanCommand;
+crate use mkick::MkickCommand as NodmMkickCommand;
+crate use mmute::MmuteCommand as NodmMmuteCommand;
+crate use munban::MunbanCommand as NodmMunbanCommand;
+crate use munmute::MunmuteCommand as NodmMunmuteCommand;
+crate use mute::MuteCommand as NodmMuteCommand;
+crate use mwarn::MwarnCommand as NodmMwarnCommand;
+crate use tempban::TempbanCommand as NodmTempbanCommand;
+crate use tempmute::TempmuteCommand as NodmTempmuteCommand;
+crate use unban::UnbanCommand as NodmUnbanCommand;
+crate use unmute::UnmuteCommand as NodmUnmuteCommand;
+crate use warn::WarnCommand as NodmWarnCommand;
