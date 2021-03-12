7434652d291d93a79f855babab3a27a73daefe1d
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2021-03-12 13:29:00 +0800

    Update invite_detection.rs

--- src/plugins/censorship/tasks/invite_detection.rs
+++ src/plugins/censorship/tasks/invite_detection.rs
@@ -23,31 +23,57 @@ use crate::{
         TaskContext
     },
     system::{
+        twilight_http_client_extensions::GetGuildConfiguration,
+        twilight_id_extensions::IntoInnerU64,
         SystemResult,
     },
     utilities::{
         invite_detection::invite_detected
-    }
+    },
+    xml_deserialization::BotConfig
 };
 
 crate struct InviteDetectionTask;
 
 impl Task for InviteDetectionTask {
-    fn execute_task<'asynchronous_trait>(ctx: TaskContext)
-        -> Pin<Box<dyn Future<Output=SystemResult<()>> + Send>> {
-        Box::pin(censorship_invite_detection_task(ctx))
+    fn execute_task<'asynchronous_trait>(ctx: TaskContext, config: BotConfig)
+        -> Pin<Box<dyn Future<Output=SystemResult<()>> + Send + 'asynchronous_trait>> {
+        Box::pin(censorship_invite_detection_task(ctx, config))
     }
 }
 
-async fn censorship_invite_detection_task(ctx: TaskContext) -> SystemResult<()> {
+async fn censorship_invite_detection_task(ctx: TaskContext, config: BotConfig) -> SystemResult<()> {
     if let TaskContext::MessageCreate(payload) = ctx {
         let message = payload.message.clone();
-        
+
         if let (true, Some(invite)) = invite_detected(message.content) {
-            
+            if let Some(ref plugin) = config.plugins {
+                if let Some(ref censorship) = plugin.censorship_plugin {
+                    for level in &censorship.levels {
+                        if level.filter_invite_links == Some(true) {
+                            if let Some(whitelisted) = level.clone().whitelisted_guild_invites {
+                                let twilight_invite = payload.http_client.clone().invite(invite.clone().code).await?;
+
+                                if let Some(actual_invite) = twilight_invite {
+                                    if let Some(invite_guild) = actual_invite.guild {
+                                        if !whitelisted.whitelists.iter()
+                                            .any(|whitelist|
+                                                whitelist.id == Some(invite_guild.id.into_inner_u64())
+                                            ) {
+                                            payload.http_client.clone()
+                                                .delete_message(payload.message.channel_id, payload.message.id)
+                                                .await?;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
-        
-        todo!()
+
+        Ok(())
     }
     else {
         unreachable!()
