9dc1b3e4a337b83e29a00ff79fe048c4289c30b2
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2021-03-12 10:07:43 +0800

    Update invite_detection.rs

--- src/plugins/censorship/tasks/invite_detection.rs
+++ src/plugins/censorship/tasks/invite_detection.rs
@@ -11,3 +11,23 @@
 //!  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //!  See the License for the specific language governing permissions and
 //!  limitations under the License.
+
+use std::{
+    future::Future,
+    pin::Pin
+};
+
+use crate::{
+    command_system::{
+        Task,
+        TaskContext
+    },
+    system::{
+        SystemResult,
+    },
+    utilities::{
+        invite_detection::invite_detected
+    }
+};
+
+crate struct InviteDetectionTask;
