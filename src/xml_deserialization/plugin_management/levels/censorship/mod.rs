4d3a1cf6d66b652ca4e9f27515e7a25e48a29538
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2021-03-14 21:40:43 +0800

    Update mod.rs

--- src/xml_deserialization/plugin_management/levels/censorship/mod.rs
+++ src/xml_deserialization/plugin_management/levels/censorship/mod.rs
@@ -15,18 +15,25 @@
 extern crate serde;
 extern crate quick_xml;
 
-use crate::xml_deserialization::plugin_management::models::censorship::{
-    whitelisted_guild_invites::{
-        WhitelistedGuildInvites
-    },
-    zalgo_channel_whitelist::ZalgoChannelWhitelist,
-    CensoredUri
+use crate::xml_deserialization::plugin_management::{
+    levels::Level,
+    models::censorship::{
+        invites_channel_whitelist::InvitesChannelWhitelist,
+        whitelisted_guild_invites::{
+            WhitelistedGuildInvites
+        },
+        whitelisted_invite_codes::{
+            WhitelistedInviteCodes
+        },
+        zalgo_channel_whitelist::ZalgoChannelWhitelist,
+        CensoredUri
+    }
 };
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 crate struct CensorshipLevel {
     #[serde(rename = "Level")]
-    crate level: u8,
+    crate level: Level,
 
     #[serde(rename = "FilterZalgo", default)]
     crate filter_zalgo: Option<bool>,
@@ -38,5 +45,11 @@ crate struct CensorshipLevel {
     crate filter_invite_links: Option<bool>,
 
     #[serde(rename = "WhitelistedGuildInvites", default)]
-    crate whitelisted_guild_invites: Option<WhitelistedGuildInvites>
+    crate whitelisted_guild_invites: Option<WhitelistedGuildInvites>,
+
+    #[serde(rename = "InvitesChannelWhitelist", default)]
+    crate invites_channel_whitelist: Option<InvitesChannelWhitelist>,
+
+    #[serde(rename = "WhitelistedInviteCodes", default)]
+    crate whitelisted_invite_codes: Option<WhitelistedInviteCodes>
 }
