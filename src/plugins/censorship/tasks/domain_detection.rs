6963f7e68c232daadd8721f79b359dcab7d521ae
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2021-03-18 10:53:18 +0800

    Update domain_detection.rs

--- src/plugins/censorship/tasks/domain_detection.rs
+++ src/plugins/censorship/tasks/domain_detection.rs
@@ -11,3 +11,30 @@
 //!  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //!  See the License for the specific language governing permissions and
 //!  limitations under the License.
+
+use std::{
+    future::Future,
+    pin::Pin
+};
+
+use crate::{
+    command_system::{
+        Task,
+        TaskContext
+    }
+};
+use crate::xml_deserialization::BotConfig;
+use crate::command_system::precommand_checks::SystemResult;
+
+crate struct DomainDetectionTask;
+
+impl Task for DomainDetectionTask {
+    fn execute_task<'asynchronous_trait>(ctx: TaskContext, config: BotConfig) 
+        -> Pin<Box<dyn Future<Output=SystemResult<()>> + Send + 'asynchronous_trait>> {
+        Box::pin(censorship_domain_detection_task(ctx, config))
+    }
+}
+
+async fn censorship_domain_detection_task(ctx: TaskContext, config: BotConfig) -> SystemResult<()> {
+    todo!()
+}
