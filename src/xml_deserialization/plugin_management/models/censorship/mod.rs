0c71965afac950686e37c31e5410598045bec2fb
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2021-03-18 21:14:30 +0800

    Update mod.rs

--- src/xml_deserialization/plugin_management/models/censorship/mod.rs
+++ src/xml_deserialization/plugin_management/models/censorship/mod.rs
@@ -16,6 +16,7 @@ extern crate serde;
 extern crate quick_xml;
 
 crate mod blacklisted_invite_codes;
+crate mod domains_channel_whitelist;
 crate mod invites_channel_whitelist;
 crate mod whitelisted_guild_invites;
 crate mod whitelisted_invite_codes;
