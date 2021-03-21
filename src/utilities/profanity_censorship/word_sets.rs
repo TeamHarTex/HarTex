e6c7ef378ad4f2fa039cb0a8c66b1dd371216f8f
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2021-03-21 13:21:46 +0800

    Update word_sets.rs

--- src/utilities/profanity_censorship/word_sets.rs
+++ src/utilities/profanity_censorship/word_sets.rs
@@ -24,22 +24,75 @@ use std::{
     }
 };
 
-crate static STAMDARD_WORD_SET: SyncLazy<HashSet<&str>> = SyncLazy::new(|| {
+crate static STAMDARD_WORD_SET: SyncLazy<HashSet<String>> = SyncLazy::new(|| {
     HashSet::from_iter(vec![
-        "ass",
-        "asshole",
-        "bitch",
-        "cock",
-        "cunt",
-        "fag",
-        "fagot",
-        "faggot",
-        "fuck",
-        "nigger",
-        "piss",
-        "pussy",
-        "shit",
-        "twat",
-        "whore"
+        "ass".to_string(),
+        "asshole".to_string(),
+        "bitch".to_string(),
+        "cock".to_string(),
+        "cunt".to_string(),
+        "fag".to_string(),
+        "fagot".to_string(),
+        "faggot".to_string(),
+        "fuck".to_string(),
+        "nigger".to_string(),
+        "piss".to_string(),
+        "pussy".to_string(),
+        "shit".to_string(),
+        "twat".to_string(),
+        "whore".to_string()
+    ])
+});
+
+crate static ZEALOUS_WORD_SET: SyncLazy<HashSet<String>> = SyncLazy::new(|| {
+    HashSet::from_iter(vec![
+        "crap".to_string(),
+        "damn".to_string(),
+        "goddamn".to_string(),
+        "hell".to_string(),
+        "suck".to_string()
+    ])
+});
+
+crate static SEX_WORD_SET: SyncLazy<HashSet<String>> = SyncLazy::new(|| {
+    HashSet::from_iter(vec![
+        "ass".to_string(),
+        "asshole".to_string(),
+        "blowjob".to_string(),
+        "boob".to_string(),
+        "boobie".to_string(),
+        "boobies".to_string(),
+        "boobjob".to_string(),
+        "breast".to_string(),
+        "clitoris".to_string(),
+        "cock".to_string(),
+        "condom".to_string(),
+        "cunnilingus".to_string(),
+        "cunt".to_string(),
+        "dick".to_string(),
+        "doggystyle".to_string(),
+        "ejaculate".to_string(),
+        "felate".to_string(),
+        "felatio".to_string(),
+        "fetish".to_string(),
+        "foreskin".to_string(),
+        "handjob".to_string(),
+        "labia".to_string(),
+        "masturbate".to_string(),
+        "masturbation".to_string(),
+        "masterbate".to_string(),
+        "masterbation".to_string(),
+        "penis".to_string(),
+        "pussy".to_string(),
+        "rimjob".to_string(),
+        "semen".to_string(),
+        "sex".to_string(),
+        "tits".to_string(),
+        "tittie".to_string(),
+        "titties".to_string(),
+        "titty".to_string(),
+        "twat".to_string(),
+        "vagina".to_string(),
+        "vulva".to_string()
     ])
 });
