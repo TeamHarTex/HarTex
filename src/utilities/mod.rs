4f877469f84f6d95603bdcbad8c9df06b22ac05f
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2021-03-22 17:39:50 +0800

    Update mod.rs

--- src/utilities/mod.rs
+++ src/utilities/mod.rs
@@ -12,6 +12,7 @@
 //!  See the License for the specific language governing permissions and
 //!  limitations under the License.
 
+crate mod blocked_words_or_tokens_detection;
 crate mod constants;
 crate mod duration;
 crate mod image_processing;
