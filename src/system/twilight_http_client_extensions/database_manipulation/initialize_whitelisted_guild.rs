40fb0d6e255784438ba474fb696c62e6c0c0850c
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2021-03-24 10:58:23 +0800

    Update initialize_whitelisted_guild.rs

--- src/system/twilight_http_client_extensions/database_manipulation/initialize_whitelisted_guild.rs
+++ src/system/twilight_http_client_extensions/database_manipulation/initialize_whitelisted_guild.rs
@@ -1,16 +1,16 @@
-///  Copyright 2020 - 2021 The HarTex Project Developers
-///
-///  Licensed under the Apache License, Version 2.0 (the "License");
-///  you may not use this file except in compliance with the License.
-///  You may obtain a copy of the License at
-///
-///      http://www.apache.org/licenses/LICENSE-2.0
-///
-///  Unless required by applicable law or agreed to in writing, software
-///  distributed under the License is distributed on an "AS IS" BASIS,
-///  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-///  See the License for the specific language governing permissions and
-///  limitations under the License.
+//!  Copyright 2020 - 2021 The HarTex Project Developers
+//!
+//!  Licensed under the Apache License, Version 2.0 (the "License");
+//!  you may not use this file except in compliance with the License.
+//!  You may obtain a copy of the License at
+//!
+//!      http://www.apache.org/licenses/LICENSE-2.0
+//!
+//!  Unless required by applicable law or agreed to in writing, software
+//!  distributed under the License is distributed on an "AS IS" BASIS,
+//!  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//!  See the License for the specific language governing permissions and
+//!  limitations under the License.
 
 use std::{
     env::*,
@@ -142,16 +142,9 @@ async fn request(guild_id: GuildId, http: Client) -> ClientExtensionResult<BotCo
                     permission_level: 3
                 }]
             },
-            bot_customization: BotCustomization::default(),
-            role_permission_levels: RolePermissionLevels::default(),
-            plugins: Plugins {
-                infractions_plugin: InfractionsPlugin {
-                    mute_command: MuteCommand {
-                        muted_role: None,
-                        role_to_remove: None
-                    }
-                }
-            }
+            bot_customization: None,
+            role_permission_levels: None,
+            plugins: None
         };
 
         quick_xml::se::to_writer(writer.inner().get_mut(), &config)?;
