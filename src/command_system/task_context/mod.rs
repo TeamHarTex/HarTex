c12d06e702115e758ec899ae65e99f03a1f97cb6
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2021-03-25 20:52:56 +0800

    Update mod.rs

--- src/command_system/task_context/mod.rs
+++ src/command_system/task_context/mod.rs
@@ -12,6 +12,7 @@
 //!  See the License for the specific language governing permissions and
 //!  limitations under the License.
 
+mod member_update;
 mod message_create;
 
 crate use message_create::{
