54cf6e87b8f951144f2c1c7442b46177150e6ff6
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2021-03-26 08:46:16 +0800

    Update member_update.rs

--- src/command_system/task_context/member_update.rs
+++ src/command_system/task_context/member_update.rs
@@ -11,3 +11,38 @@
 //!  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //!  See the License for the specific language governing permissions and
 //!  limitations under the License.
+
+use std::{
+    ops::Deref,
+    sync::Arc
+};
+
+use twilight_http::Client;
+
+use twilight_model::{
+    guild::Member
+};
+
+crate struct MemberUpdateTaskContext(crate Arc<MemberUpdateTaskContextRef>);
+
+impl Deref for MemberUpdateTaskContext {
+    type Target = MemberUpdateTaskContextRef;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+crate struct MemberUpdateTaskContextRef {
+    crate http_client: Client,
+    crate member: Member
+}
+
+impl MemberUpdateTaskContextRef {
+    crate fn new(http_client: Client, member: Member) -> MemberUpdateTaskContextRef {
+        Self {
+            http_client,
+            member
+        }
+    }
+}
