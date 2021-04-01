9c8fd0dd091629ce8434d3c858d50b8a4bf05cad
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2021-04-01 13:21:14 +0800

    Update zalgo_nickname_detection.rs

--- src/plugins/censorship/tasks/zalgo_nickname_detection.rs
+++ src/plugins/censorship/tasks/zalgo_nickname_detection.rs
@@ -31,22 +31,40 @@ use crate::{
     xml_deserialization::BotConfig
 };
 
-crate struct ZalgoNicknameDetectionTask;
+crate struct BlockedNicknameDetectionTask;
 
-impl Task for ZalgoNicknameDetectionTask {
+impl Task for BlockedNicknameDetectionTask {
     fn execute_task<'asynchronous_trait>(ctx: TaskContext, config: BotConfig)
         -> Pin<Box<dyn Future<Output=SystemResult<()>> + Send + 'asynchronous_trait>> {
-        Box::pin(censorship_zalgo_nickname_detection(ctx, config))
+        Box::pin(censorship_blocked_nickname_detection_task(ctx, config))
     }
 }
 
-async fn censorship_zalgo_nickname_detection(ctx: TaskContext, config: BotConfig) -> SystemResult<()> {
+async fn censorship_blocked_nickname_detection_task(ctx: TaskContext, config: BotConfig) -> SystemResult<()> {
     if let TaskContext::MemberUpdate(payload) = ctx {
         if let Some(ref plugins) = config.plugins {
             if let Some(ref censorship_plugin) = plugins.censorship_plugin {
                 for level in &censorship_plugin.censorship_levels.levels {
-                    if level.filter_zalgo_nicknames == Some(true) {
-                        if let Some(nickname) = payload.member.nick.clone() {
+                    if level.filter_zalgo_nicknames != Some(true) {
+                        continue;
+                    }
+
+                    if let Some(nickname) = payload.member.nick.clone() {
+                        if let Some(ref list) = level.blocked_nicknames {
+                            if list.blocked_nicknames.contains(&nickname) {
+                                payload.http_client
+                                    .update_guild_member(payload.member.guild_id, payload.member.user.id)
+                                    .nick(Some(if let Some(default_name) = level.zalgo_filtered_default_nickname.clone() {
+                                        default_name
+                                    }
+                                    else {
+                                        String::from("Censored Nickname")
+                                    }))?
+                                    .await;
+
+                                break;
+                            }
+                            
                             if zalgo_detected(&nickname) {
                                 payload.http_client
                                     .update_guild_member(payload.member.guild_id, payload.member.user.id)
@@ -57,10 +75,10 @@ async fn censorship_zalgo_nickname_detection(ctx: TaskContext, config: BotConfig
                                         String::from("Censored Nickname")
                                     }))?
                                     .await;
+
+                                break;
                             }
                         }
-
-                        break;
                     }
                 }
             }
