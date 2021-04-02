14382711a08c17647af06a9d5bcda5cb5f0f1acb
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2021-04-02 16:47:06 +0800

    Update blocked_mentions_detection.rs

--- src/plugins/censorship/tasks/blocked_mentions_detection.rs
+++ src/plugins/censorship/tasks/blocked_mentions_detection.rs
@@ -29,7 +29,18 @@ use crate::{
     system::{
         SystemResult,
     },
-    xml_deserialization::BotConfig
+    xml_deserialization::{
+        plugin_management::{
+            models::{
+                censorship::{
+                    blocked_mentions::{
+                        BlockedMention
+                    }
+                }
+            }
+        },
+        BotConfig
+    }
 };
 
 crate struct BlockedMentionsDetectionTask;
@@ -44,12 +55,73 @@ impl Task for BlockedMentionsDetectionTask {
 async fn censorship_blocked_mentions_detection_task(ctx: TaskContext, config: BotConfig) -> SystemResult<()> {
     if let TaskContext::MessageCreate(payload) = ctx {
         let regex = Regex::new(r"<((@)|(#)|(@&))([0-9]+)>").unwrap();
+        if let Some(ref plugins) = config.plugins {
+            if let Some(ref censorship_plugin) = plugins.censorship_plugin {
+                for level in &censorship_plugin.censorship_levels.levels {
+                    if let Some(ref blocked_mentions) = level.prohibited_mentions {
+                        if let Some(captures) = regex.captures(&payload.message.content) {
+                            for blocked in &blocked_mentions.blocked_mentions {
+                                match blocked {
+                                    BlockedMention::ChannelId(value) => {
+                                        if let Some(r#match) = captures.get(1) {
+                                            if r#match.as_str() != "#" {
+                                                return Ok(());
+                                            }
 
-        if let Some(captures) = regex.captures(&payload.message.content) {
-            dbg!(captures);
+                                            if let Some(r#match) = captures.get(5) {
+                                                if value != &r#match.as_str().parse::<u64>().unwrap() {
+                                                    return Ok(());
+                                                }
+
+                                                payload.http_client.clone()
+                                                    .delete_message(payload.message.channel_id, payload.message.id)
+                                                    .await?;
+                                            }
+                                        }
+                                    },
+                                    BlockedMention::RoleId(value) => {
+                                        if let Some(r#match) = captures.get(1) {
+                                            if r#match.as_str() != "@&" {
+                                                return Ok(());
+                                            }
+
+                                            if let Some(r#match) = captures.get(5) {
+                                                if value != &r#match.as_str().parse::<u64>().unwrap() {
+                                                    return Ok(());
+                                                }
+
+                                                payload.http_client.clone()
+                                                    .delete_message(payload.message.channel_id, payload.message.id)
+                                                    .await?;
+                                            }
+                                        }
+                                    },
+                                    BlockedMention::UserId(value) => {
+                                        if let Some(r#match) = captures.get(1) {
+                                            if r#match.as_str() != "@" {
+                                                return Ok(());
+                                            }
+
+                                            if let Some(r#match) = captures.get(5) {
+                                                if value != &r#match.as_str().parse::<u64>().unwrap() {
+                                                    return Ok(());
+                                                }
+
+                                                payload.http_client.clone()
+                                                    .delete_message(payload.message.channel_id, payload.message.id)
+                                                    .await?;
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
 
-        todo!()
+        Ok(())
     }
     else {
         unreachable!()
