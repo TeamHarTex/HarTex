fc7def5856e573b397d7546ce3cbbe6e6da0da61
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2021-04-03 20:34:53 +0800

    Complete init impl for consec. cap. let'rs censorship

--- src/plugins/censorship/tasks/consecutive_capital_letters_detection.rs
+++ src/plugins/censorship/tasks/consecutive_capital_letters_detection.rs
@@ -13,7 +13,9 @@
 //!  limitations under the License.
 
 use std::{
+    convert::TryInto,
     future::Future,
+    iter::IntoIterator,
     pin::Pin
 };
 
@@ -59,12 +61,37 @@ async fn censorship_consecutive_capital_letters_detection_task(ctx: TaskContext,
                     };
 
                     let mut message = payload.message.content.clone();
+                    let is_uppercase = char::is_uppercase;
 
                     // We remove all spaces from the string to count consecutive capital letters,
                     // whilst completely ignoring spaces.
                     message.retain(|character| !character.is_whitespace());
 
+                    let groups = message.char_indices().group_by(|&(_idx, c)| is_uppercase(c));
 
+                    let longest_capital_substring = groups
+                        .into_iter()
+                        .filter_map(|(is_capital, mut group)| {
+                            is_capital.then(|| {
+                                let first = group.next().expect("group can't be empty");
+                                let (end_idx, end_chr) = group.last().unwrap_or(first);
+                                let (start_idx, _) = first;
+
+                                &message[start_idx..end_idx + end_chr.len_utf8()]
+                            })
+                        })
+                        .max_by_key(|s| s.len());
+
+                    if longest_capital_substring.is_none() {
+                        return Ok(());
+                    }
+
+                    // It is ok to .unwrap() here because we already know that the variable is not none.
+                    if longest_capital_substring.unwrap().len() as u64 >= minimum_caps {
+                        payload.http_client.clone()
+                            .delete_message(payload.message.channel_id, payload.message.id)
+                            .await?;
+                    }
                 }
             }
         }
