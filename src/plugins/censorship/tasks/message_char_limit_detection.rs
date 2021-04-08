b55e9630bb7b740252b123c669e3dbe092a3166d
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2021-04-08 20:55:19 +0800

    Update message_char_limit_detection.rs

--- src/plugins/censorship/tasks/message_char_limit_detection.rs
+++ src/plugins/censorship/tasks/message_char_limit_detection.rs
@@ -23,7 +23,56 @@ use crate::{
         TaskContext
     },
     system::{
+        twilight_id_extensions::IntoInnerU64,
         SystemResult,
     },
-    xml_deserialization::BotConfig
+    xml_deserialization::{
+        plugin_management::models::channel_id::ChannelId,
+        BotConfig
+    }
 };
+
+crate struct MessageCharLimitDetectionTask;
+
+impl Task for MessageCharLimitDetectionTask {
+    fn execute_task<'asynchronous_trait>(ctx: TaskContext, config: BotConfig)
+        -> Pin<Box<dyn Future<Output=SystemResult<()>> + Send + 'asynchronous_trait>> {
+        Box::pin(censorship_memory_character_limit_exceeded_detection_task(ctx, config))
+    }
+}
+
+async fn censorship_memory_character_limit_exceeded_detection_task(ctx: TaskContext, config: BotConfig)
+    -> SystemResult<()> {
+    if let TaskContext::MessageCreate(payload) = ctx {
+        if let Some(ref plugins) = config.plugins {
+            if let Some(ref censorship_plugin) = plugins.censorship_plugin {
+                for level in &censorship_plugin.censorship_levels.levels {
+                    if level.message_length_limit == 0 {
+                        return Ok(());
+                    }
+
+                    if let Some(ref char_limit_channel_whitelist) = level.message_length_limit_channel_whitelist {
+                        if char_limit_channel_whitelist.channel_ids.contains(
+                            &ChannelId {
+                                id: payload.message.channel_id.into_inner_u64()
+                            }
+                        ) {
+                            return Ok(());
+                        }
+
+                        if payload.message.content.len() as u64 > level.message_length_limit {
+                            payload.http_client.clone()
+                                .delete_message(payload.message.channel_id, payload.message.id)
+                                .await?;
+                        }
+                    }
+                }
+            }
+        }
+
+        Ok(())
+    }
+    else {
+        unreachable!()
+    }
+}
