1503f88839bff4b48a7c426375829355dbfb0661
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2021-04-12 19:16:40 +0800

    Update leaderboard.rs

--- src/plugins/levelling_system/leaderboard.rs
+++ src/plugins/levelling_system/leaderboard.rs
@@ -21,6 +21,11 @@ use std::{
 
 use twilight_cache_inmemory::InMemoryCache;
 
+use twilight_embed_builder::{
+    EmbedBuilder,
+    EmbedFieldBuilder
+};
+
 use twilight_mention::ParseMention;
 
 use twilight_model::{
@@ -55,15 +60,39 @@ impl Command for LeaderboardCommand {
         String::from("leaderboard")
     }
 
-    fn execute_command<'asynchronous_trait>(ctx: CommandContext<'asynchronous_trait>, _arguments: Arguments<'asynchronous_trait>, _cache: InMemoryCache)
+    fn execute_command<'asynchronous_trait>(ctx: CommandContext<'asynchronous_trait>, _arguments: Arguments<'asynchronous_trait>, cache: InMemoryCache)
         -> Pin<Box<dyn Future<Output=SystemResult<()>> + Send + 'asynchronous_trait>> {
-        Box::pin(levelling_system_leaderboard_command(ctx))
+        Box::pin(levelling_system_leaderboard_command(ctx, cache))
     }
 }
 
-async fn levelling_system_leaderboard_command(ctx: CommandContext<'_>) -> SystemResult<()> {
+async fn levelling_system_leaderboard_command(ctx: CommandContext<'_>, cache: InMemoryCache) -> SystemResult<()> {
     let guild_id = ctx.message.guild_id.unwrap();
+    let guild = cache.guild(ctx.message.guild_id.unwrap()).unwrap();
     let leaderboard = ctx.http_client.clone().get_guild_leaderboard(guild_id).await?;
-    
+    let channel_id = ctx.message.channel_id;
+
+    let embed = EmbedBuilder::new()
+        .color(0x03_BE_FC)?
+        .title(format!("{}'s Levelling System Leaderboard", guild.name))?;
+
+    for (index, entry) in leaderboard.into_iter().enumerate() {
+        let member = cache.member(guild_id, entry.user_id).expect("Cannot find member.");
+
+        embed.clone().field(
+            EmbedFieldBuilder::new(
+                format!(
+                    "Rank #{} - {}#{}",
+                    index + 1,
+                    member.nick.as_ref().unwrap_or(&member.user.name.clone()),
+                    member.user.discriminator
+                ), format!("Level {}: {} XP", entry.level, entry.experience)
+            )?
+        );
+    }
+
+    ctx.http_client.create_message(channel_id).embed(embed.build()?)?.reply(ctx.message.id).allowed_mentions()
+        .replied_user(false).build().await?;
+
     Ok(())
 }
