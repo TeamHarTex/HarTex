76661d50ad4011c07595ef12406c96a363e34a33
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2021-05-13 13:18:46 +0800

    Update coinflip.rs

--- src/plugins/utilities/coinflip.rs
+++ src/plugins/utilities/coinflip.rs
@@ -59,8 +59,6 @@ async fn utilities_coinflip_command(ctx: CommandContext<'_>) -> SystemResult<()>
         .create_message(ctx.message.channel_id)
         .content("Flipping a coin; please wait...")?
         .allowed_mentions(AllowedMentions::default())
-        .replied_user(false)
-        .build()
         .reply(ctx.message.id)
         .await?;
     let allowed_mentions = AllowedMentions::default();
