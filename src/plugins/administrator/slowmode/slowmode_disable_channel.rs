39095b9b15aa10b13892d01b10b5a0cede717a86
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2021-05-13 10:51:59 +0800

    Update slowmode_disable_channel.rs

--- src/plugins/administrator/slowmode/slowmode_disable_channel.rs
+++ src/plugins/administrator/slowmode/slowmode_disable_channel.rs
@@ -26,6 +26,7 @@ use twilight_mention::{
 };
 
 use twilight_model::{
+    channel::message::AllowedMentions,
     id::ChannelId
 };
 
@@ -102,9 +103,7 @@ async fn administrator_slowmode_disable_channel_command(ctx: CommandContext<'_>,
     ctx.http_client
         .clone()
         .create_message(channel_id)
-        .allowed_mentions()
-        .replied_user(false)
-        .build()
+        .allowed_mentions(AllowedMentions::default())
         .content(
             format!("<:green_check:705623382682632205> Disabled slowmode for channel <#{}>", channel_id.0)
         )?
