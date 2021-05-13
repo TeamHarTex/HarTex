ff4a692d37ac0cec39272cfdbb316f41ad525c24
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2021-05-13 11:02:10 +0800

    Update help.rs

--- src/plugins/general/help.rs
+++ src/plugins/general/help.rs
@@ -20,11 +20,14 @@ use std::{
 use twilight_cache_inmemory::{
     InMemoryCache
 };
+
 use twilight_embed_builder::{
     EmbedBuilder,
     EmbedFieldBuilder,
     EmbedFooterBuilder
 };
+
+use twilight_model::channel::message::AllowedMentions;
 
 use crate::command_system::{
     parser::{
@@ -53,21 +56,20 @@ impl Command for HelpCommand {
 async fn general_help_command(ctx: CommandContext<'_>) -> SystemResult<()> {
     let channel_id = ctx.message.channel_id;
     let embed = EmbedBuilder::new()
-        .title("HarTex - Moderation Commands Reference")?
+        .title("HarTex - Moderation Commands Reference")
         .description(
             "The following list are the common commands used by the moderators. A more detailed list can be found at the"
                 .to_owned()
-                + " documentation website: SOON:tm:")?
+                + " documentation website: SOON:tm:")
         .field(EmbedFieldBuilder::new(
-            "warn <User ID> [Reason]", "Adds an infraction to a user with the type `Warning`.")?)
+            "warn <User ID> [Reason]", "Adds an infraction to a user with the type `Warning`."))
         .footer(EmbedFooterBuilder::new(
             "For parameters wrapped in `<>`, it can either be a user mention or a User ID. For parameters wrapped"
-                .to_owned() + " in `[]`, it is optional.")?)
-        .color(0x03_BE_FC)?
+                .to_owned() + " in `[]`, it is optional."))
+        .color(0x03_BE_FC)
         .build()?;
 
-    ctx.http_client.create_message(channel_id).reply(ctx.message.id).allowed_mentions().replied_user(false)
-        .build().embed(embed)?.await?;
+    ctx.http_client.create_message(channel_id).reply(ctx.message.id).allowed_mentions(AllowedMentions::default()).embed(embed)?.await?;
 
     Ok(())
 }
