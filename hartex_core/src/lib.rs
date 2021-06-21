e93d65e6d0ff69dbaa4bcae62c09eb3d63d1e138
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2021-06-21 19:26:12 +0800

    add `hartex_core` library

---
+++ hartex_core/src/lib.rs
@@ -0,0 +1,14 @@
+//! # `hartex_core` - The HarTex Core Library
+//!
+//! The `hartex_core` library contains the core functionality for the HarTex Discord bot.
+//!
+//! ## Features
+//!
+//! - `twilight-bundled`: bundles most of the `twilight` ecosystem of crates with the library,
+//!                       removes the need to include the dependencies repeatedly across the
+//!                       HarTex crates.
+
+#[cfg(feature = "twilight-bundled")]
+pub mod discord;
+
+pub mod error;
