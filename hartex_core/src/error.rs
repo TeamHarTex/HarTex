b6fc69e070b2d5826949036f9f7d53efa6ca9ac4
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2021-06-21 19:26:12 +0800

    add `hartex_core` library

---
+++ hartex_core/src/error.rs
@@ -0,0 +1,24 @@
+//! # The `error` Module
+//!
+//! This module defines several types for error handling in the HarTex Discord bot.
+
+/// # Enum `HarTexError`
+///
+/// An enumeration representing the various error types used within HarTex.
+pub enum HarTexError {
+    /// # Enum Variant `Custom`
+    ///
+    /// Represents a custom error that cannot be represented with any other variants of this
+    /// enumeration.
+    ///
+    /// ## Fields
+    /// - `message`, type `String`: the error message.
+    Custom {
+        message: String
+    }
+}
+
+/// # Type Alias `HarTexResult<T>`
+///
+/// A type alias for `Result<T, HarTexError>`, used for error-handling.
+pub type HarTexResult<T> = Result<T, HarTexError>;
