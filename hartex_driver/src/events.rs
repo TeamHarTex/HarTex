e75ee303262f7e6f2969e382265d7f0865ebed1b
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2021-06-24 22:57:21 +0800

    Update events.rs

--- hartex_driver/src/events.rs
+++ hartex_driver/src/events.rs
@@ -9,18 +9,18 @@ use hartex_core::{
     events::EventType
 };
 
-use hartex_logging::Logger;
+// use hartex_logging::Logger;
 
 use crate::handler::EventHandler;
 
-pub async fn handle_event(shard_id: u64, (event_type, twilight): (EventType, Option<Event>)) -> HarTexResult<()> {
+pub async fn handle_event(_shard_id: u64, (event_type, twilight): (EventType, Option<Event>)) -> HarTexResult<()> {
     // Logger::verbose(format!("shard {} received an event; handling event", shard_id), Some(module_path!()));
 
     match event_type {
         EventType::Twilight if twilight.is_some() => {
             match twilight.unwrap() {
                 Event::Ready(payload) => {
-                    EventHandler::ready(payload).await
+                    EventHandler::ready(payload).await?
                 },
                 _ => ()
             }
