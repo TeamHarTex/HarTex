a7685cfdc3c45bf7c04777636e85315753eb74ea
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2021-06-24 18:31:46 +0800

    complete `Logger` implementation

--- hartex_logging/src/lib.rs
+++ hartex_logging/src/lib.rs
@@ -2,7 +2,14 @@
 //!
 //! The `hartex_logging` library contains an implementation of a logger for the HarTex Discord bot.
 
-use hartex_core::ansi::SgrParam;
+use hartex_core::{
+    ansi::{
+        ansi_display,
+        AnsiColour,
+        SgrParam
+    },
+    time::Local,
+};
 
 pub mod level;
 
@@ -49,5 +56,153 @@ impl Logger {
     where
         T: Into<String> {
         let module_name = module.unwrap_or(module_path!());
+
+        println!(
+            "[HarTex v{}: {}+08:00] [{}{}{}] [{}] {}",
+            env!("CARGO_PKG_VERSION"),
+            Local::now().format("%Y-%m-%d %H:%M:%S"),
+            ansi_display(SgrParam::SetColour {
+                colour: AnsiColour::CustomU8 {
+                    n: 2
+                },
+                foreground: true
+            }.into_i32s()),
+            level::LogLevel::Info.display(),
+            ansi_display(SgrParam::Reset.into_i32s()),
+            module_name.into(),
+            message.into()
+        );
+    }
+
+    /// # Static Method `Logger::debug`
+    ///
+    /// Logs a message to the console with the "debug" log level.
+    ///
+    /// ## Generic Parameters
+    /// - `T`; `Into<String>`
+    ///
+    /// ## Parameters
+    /// - `message`, type `T`: the message to be logged
+    /// - `module`, type `Option<T>`: the module the where the static method is invoked; can be
+    ///                               `None`, and defaults to the `hartex_logging` module.
+    pub fn debug<T>(message: T, module: Option<T>)
+    where
+        T: Into<String> {
+        let module_name = module.unwrap_or(module_path!());
+
+        println!(
+            "[HarTex v{}: {}+08:00] [{}{}{}] [{}] {}",
+            env!("CARGO_PKG_VERSION"),
+            Local::now().format("%Y-%m-%d %H:%M:%S"),
+            ansi_display(SgrParam::SetColour {
+                colour: AnsiColour::CustomU8 {
+                    n: 33
+                },
+                foreground: true
+            }.into_i32s()),
+            level::LogLevel::Debug.display(),
+            ansi_display(SgrParam::Reset.into_i32s()),
+            module_name.into(),
+            message.into()
+        );
+    }
+
+    /// # Static Method `Logger::warn`
+    ///
+    /// Logs a message to the console with the "warning" log level.
+    ///
+    /// ## Generic Parameters
+    /// - `T`; `Into<String>`
+    ///
+    /// ## Parameters
+    /// - `message`, type `T`: the message to be logged
+    /// - `module`, type `Option<T>`: the module the where the static method is invoked; can be
+    ///                               `None`, and defaults to the `hartex_logging` module.
+    pub fn warn<T>(message: T, module: Option<T>)
+    where
+        T: Into<String> {
+        let module_name = module.unwrap_or(module_path!());
+
+        println!(
+            "[HarTex v{}: {}+08:00] [{}{}{}] [{}] {}",
+            env!("CARGO_PKG_VERSION"),
+            Local::now().format("%Y-%m-%d %H:%M:%S"),
+            ansi_display(SgrParam::SetColour {
+                colour: AnsiColour::CustomU8 {
+                    n: 226
+                },
+                foreground: true
+            }.into_i32s()),
+            level::LogLevel::Warn.display(),
+            ansi_display(SgrParam::Reset.into_i32s()),
+            module_name.into(),
+            message.into()
+        );
+    }
+
+    /// # Static Method `Logger::error`
+    ///
+    /// Logs a message to the console with the "error" log level.
+    ///
+    /// ## Generic Parameters
+    /// - `T`; `Into<String>`
+    ///
+    /// ## Parameters
+    /// - `message`, type `T`: the message to be logged
+    /// - `module`, type `Option<T>`: the module the where the static method is invoked; can be
+    ///                               `None`, and defaults to the `hartex_logging` module.
+    pub fn error<T>(message: T, module: Option<T>)
+    where
+        T: Into<String> {
+        let module_name = module.unwrap_or(module_path!());
+
+        println!(
+            "[HarTex v{}: {}+08:00] [{}{}{}] [{}] {}",
+            env!("CARGO_PKG_VERSION"),
+            Local::now().format("%Y-%m-%d %H:%M:%S"),
+            ansi_display(SgrParam::SetColour {
+                colour: AnsiColour::CustomU8 {
+                    n: 1
+                },
+                foreground: true
+            }.into_i32s()),
+            level::LogLevel::Error.display(),
+            ansi_display(SgrParam::Reset.into_i32s()),
+            module_name.into(),
+            message.into()
+        );
+    }
+
+    /// # Static Method `Logger::verbose`
+    ///
+    /// Logs a message to the console with the "verbose" log level.
+    ///
+    /// ## Generic Parameters
+    /// - `T`; `Into<String>`
+    ///
+    /// ## Parameters
+    /// - `message`, type `T`: the message to be logged
+    /// - `module`, type `Option<T>`: the module the where the static method is invoked; can be
+    ///                               `None`, and defaults to the `hartex_logging` module.
+    pub fn verbose<T>(message: T, module: Option<T>)
+    where
+        T: Into<String> {
+        let module_name = module.unwrap_or(module_path!());
+
+        println!(
+            "[HarTex v{}: {}+08:00] [{}{}{}] [{}] {}",
+            env!("CARGO_PKG_VERSION"),
+            Local::now().format("%Y-%m-%d %H:%M:%S"),
+            ansi_display(SgrParam::SetColour {
+                colour: AnsiColour::CustomU8 {
+                    n: 1
+                },
+                foreground: true
+            }.into_i32s()),
+            level::LogLevel::Verbose.display(),
+            ansi_display(SgrParam::Reset.into_i32s()),
+            module_name.into(),
+            message.into()
+        );
     }
 }
