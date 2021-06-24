a4ab6fb43262e0c67aed9ad0bbd4b9aaa942e4c9
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2021-06-24 19:52:03 +0800

    update async main

--- hartex_driver/src/lib.rs
+++ hartex_driver/src/lib.rs
@@ -3,7 +3,30 @@
 //! This `hartex_driver` crate contains effectively the "main function" of the bot as well as some
 //! "moving pieces" that are required for the bot to work.
 
-use hartex_core::error::HarTexResult;
+use std::{
+    env,
+    process
+};
+
+use hartex_core::{
+    discord::{
+        gateway::{
+            cluster::{
+                Cluster,
+                ShardScheme,
+            },
+            Intents,
+        },
+        http::Client,
+        cache_inmemory::{
+            InMemoryCache,
+            ResourceType
+        }
+    },
+    error::HarTexResult
+};
+
+use hartex_logging::Logger;
 
 /// # Asynchronous Function `hartex_main`
 ///
@@ -14,6 +37,63 @@ use hartex_core::error::HarTexResult;
 pub async fn hartex_main() -> HarTexResult<()> {
     // loads the .env file to obtain environment variables
     dotenv::dotenv().ok();
+
+    Logger::verbose("loaded environment variables", Some(module_path!()));
+
+    // obtains the token from the environment variables
+    let token = match env::var("HARTEX_TOKEN") {
+        Ok(token) => token,
+        Err(var_error) => {
+            Logger::error(
+                format!(
+                    "could not retrieve the bot token from the environment due to an error: {}",
+                    var_error
+                ),
+                Some(module_path!())
+            );
+
+            process::exit(-1)
+        }
+    };
+
+    Logger::verbose("successfully retrieved bot token", Some(module_path!()));
+
+    let shard_scheme = ShardScheme::Auto;
+
+    Logger::verbose("building bot cluster", Some(module_path!()));
+    Logger::verbose("registering gateway intents [all]", Some(module_path!()));
+
+    let intents = Intents::all();
+
+    let (cluster, mut events) = Cluster::builder(&token, intents)
+        .shard_scheme(shard_scheme)
+        .build()
+        .await?;
+
+    let cluster_spawn = cluster.clone();
+
+    tokio::spawn(async move {
+        cluster_spawn.up().await
+    });
+
+    Logger::verbose("building http client", Some(module_path!()));
+
+    let http = Client::new(&token);
+
+    Logger::verbose("building in-memory cache", Some(module_path!()));
+
+    let resource_types = ResourceType::all();
+    let cache = InMemoryCache::builder()
+        .resource_types(resource_types)
+        .build();
+
+    Logger::verbose("registering ctrl-c handler", Some(module_path!()));
+
+    ctrlc::set_handler(|| {
+        Logger::warn("ctrl-c signal received; terminating process", Some(module_path!()));
+
+        process::exit(0)
+    })?;
 
     Ok(())
 }
