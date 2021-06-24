2b828fcb31356c186d610acee7c998e5b33e2607
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2021-06-24 22:42:18 +0800

    Update handler.rs

--- hartex_driver/src/handler.rs
+++ hartex_driver/src/handler.rs
@@ -3,9 +3,30 @@
 //! This module defines the `EventHandler` struct, which defines various function handlers for
 //! individual events.
 
+use hartex_core::{
+    discord::model::gateway::payload::Ready,
+    error::HarTexResult
+};
+
+use hartex_logging::Logger;
+
 /// # Struct `EventHandler`
 ///
 /// This structure defines various function handlers for individual events.
 pub struct EventHandler;
 
+impl EventHandler {
+    pub async fn ready(payload: Box<Ready>) -> HarTexResult<()> {
+        let user = payload.user;
 
+        Logger::info(format!(
+            "{}#{} [id: {}] has successfully startup; using discord api v{}",
+            user.name,
+            user.discriminator,
+            user.id,
+            payload.version
+        ), Some(module_path!()));
+
+        Ok(())
+    }
+}
