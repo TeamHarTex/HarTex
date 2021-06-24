cb8bac970647e119ac15e5c37b6e08ef52756476
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2021-06-24 19:28:43 +0800

    update `main` function

--- hartex/src/main.rs
+++ hartex/src/main.rs
@@ -1,3 +1,25 @@
-pub fn main() {
+//! # `hartex` - The HarTex Binary Crate
+//!
+//! This crate contains the main function which calls the `hartex_main` "main function" in the
+//! `hartex_driver` crate which glues everything together.
 
+use std::time::Duration;
+
+use tokio::runtime::Builder;
+
+use hartex_core::error::HarTexResult;
+
+pub fn main() -> HarTexResult<()> {
+    let tokio_runtime = Builder::new_multi_thread()
+        .enable_time()
+        .thread_name("hartex")
+        .build()
+        .unwrap();
+
+    tokio_runtime.block_on(async move {
+        hartex_driver::hartex_main().await
+    })?;
+    tokio_runtime.shutdown_timeout(Duration::from_millis(100));
+
+    Ok(())
 }
