1f8822e6203a61e9abf0ad24e5624b3a71eeec3f
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2021-06-24 22:31:13 +0800

    Create handler.rs

---
+++ hartex_driver/src/handler.rs
@@ -0,0 +1,11 @@
+//! # The `handler` Module
+//!
+//! This module defines the `EventHandler` struct, which defines various function handlers for
+//! individual events.
+
+/// # Struct `EventHandler`
+///
+/// This structure defines various function handlers for individual events.
+pub struct EventHandler;
+
+
