1d2389219e85e2858aa931c06ced1e57d2cd4fd4
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2021-06-25 20:18:04 +0800

    add parser implementation

--- hartex_cmdsys/src/parser/mod.rs
+++ hartex_cmdsys/src/parser/mod.rs
@@ -3,3 +3,19 @@
 //! This module contains an implementation of a command parser.
 
 pub mod args;
+pub mod config;
+
+/// # Struct `CommandParser`
+///
+/// The command parser.
+pub struct CommandParser {
+    config: config::CommandParserConfig
+}
+
+impl CommandParser {
+    pub fn new(config: config::CommandParserConfig) -> Self {
+        Self {
+            config
+        }
+    }
+}
