c0999c6fd2b969a48a8085ba3c884f3ec9b5235b
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2021-06-25 20:13:38 +0800

    add `framework` module

--- hartex_cmdsys/src/lib.rs
+++ hartex_cmdsys/src/lib.rs
@@ -3,4 +3,5 @@
 //! The `hartex_cmdsys` library contains an implementation of a command system for HarTex Discord
 //! bot, including a command parser, as well as various utilities for the implementation.
 
+pub mod framework;
 pub mod parser;
