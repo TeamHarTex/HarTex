a96aaa0869e665914e66db82a333d71a760ead14
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2021-06-25 18:09:53 +0800

    add `parser` module

--- hartex_cmdsys/src/lib.rs
+++ hartex_cmdsys/src/lib.rs
@@ -2,3 +2,5 @@
 //!
 //! The `hartex_cmdsys` library contains an implementation of a command system for HarTex Discord
 //! bot, including a command parser, as well as various utilities for the implementation.
+
+pub mod parser;
