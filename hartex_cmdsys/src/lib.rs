353dc98709dbf1b2c31cb3caa07a0ca70833e71b
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2021-06-25 11:58:10 +0800

    add `cmdsys` crate

---
+++ hartex_cmdsys/src/lib.rs
@@ -0,0 +1,4 @@
+//! # `hartex_cmdsys` - The HarTex Command System Library
+//!
+//! The `hartex_cmdsys` library contains an implementation of a command system for HarTex Discord
+//! bot, including a command parser, as well as various utilities for the implementation.
