a13642ade26568a8120964081e36f6361fef13ac
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2021-06-25 11:13:20 +0800

    remove commented code

--- hartex_driver/src/events.rs
+++ hartex_driver/src/events.rs
@@ -9,13 +9,9 @@ use hartex_core::{
     events::EventType
 };
 
-// use hartex_logging::Logger;
-
 use crate::handler::EventHandler;
 
 pub async fn handle_event(_shard_id: u64, (event_type, twilight): (EventType, Option<Event>)) -> HarTexResult<()> {
-    // Logger::verbose(format!("shard {} received an event; handling event", shard_id), Some(module_path!()));
-
     match event_type {
         EventType::Twilight if twilight.is_some() => {
             match twilight.unwrap() {
