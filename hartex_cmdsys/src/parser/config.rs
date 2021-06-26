87d7f4b9c7f4201d1a20ff2fe48b2c9c628f36dc
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2021-06-26 15:16:11 +0800

    add `CommandConfig` and update `CommandParserConfig`

--- hartex_cmdsys/src/parser/config.rs
+++ hartex_cmdsys/src/parser/config.rs
@@ -1,11 +1,52 @@
 //! # The `config` Module
 //!
-//! This module contains configuration utilities for the command parser.
+//! This module contains configuration utilities for a command and the command parser.
+
+/// # Struct `CommandConfig`
+///
+/// Holds the configuration for a command.
+#[derive(Default)]
+pub struct CommandConfig {
+    name: Option<String>,
+    aliases: Option<Vec<String>>
+}
+
+impl CommandConfig {
+    pub fn with_name(name: String) -> Self {
+        Self {
+            name: Some(name),
+            ..Default::default()
+        }
+    }
+
+    pub fn alias(mut self, alias: String) -> Self {
+        if self.aliases.is_none() {
+            self.aliases = Some(vec![alias]);
+
+            return self;
+        }
+
+        self.aliases = self.aliases.map(|mut vec| {
+            vec.push(alias);
+
+            vec
+        });
+
+        self
+    }
+}
 
 /// # Struct `CommandParserConfig`
 ///
 /// The command parser configuration.
 #[derive(Clone, Debug, Default)]
 pub struct CommandParserConfig {
-    commands: Vec<String>
+    commands: Vec<CommandConfig>
+}
+
+impl CommandParserConfig {
+    pub fn command(mut self, config: CommandConfig) -> Self {
+        self.commands.push(config);
+        self
+    }
 }
