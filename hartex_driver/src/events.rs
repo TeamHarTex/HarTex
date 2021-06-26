54f19ff30fae9308161794533ee8f8c636db7b94
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2021-06-26 16:10:16 +0800

    document `handle_event`

--- hartex_driver/src/events.rs
+++ hartex_driver/src/events.rs
@@ -11,6 +11,16 @@ use hartex_core::{
 
 use crate::handler::EventHandler;
 
+/// # Function `handle_event`
+///
+/// Handles the incoming event asynchronously.
+///
+/// ## Parameters
+/// - `shard_id`, type `u64`: the shard id of the shard that received the event
+/// - `event_type`, type `EventType`: the event type of the event, whether it is a custom event or
+///                                   a twilight event
+/// - `twilight`, type `Option<Event>`: the twilight event; should only be set to `Some(...)` when
+///                                     the `event_type` parameter is set to `EventType::twilight`
 pub async fn handle_event(_shard_id: u64, (event_type, twilight): (EventType, Option<Event>)) -> HarTexResult<()> {
     match event_type {
         EventType::Twilight if twilight.is_some() => {
