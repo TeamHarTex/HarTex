c76ed86ac997da53e57befab731d8b4602198fc3
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2021-06-26 21:12:07 +0800

    finish parser implementation

--- hartex_cmdsys/src/parser/mod.rs
+++ hartex_cmdsys/src/parser/mod.rs
@@ -14,9 +14,9 @@ pub struct CommandParser<'a> {
 
 impl<'a> CommandParser<'a> {
     /// # Constructor `CommandParser::new`
-    /// 
+    ///
     /// Creates a new `CommandParser` with the given `CommandParserConfig`.
-    /// 
+    ///
     /// ## Parameters
     /// - `config`, type `CommandParserConfig`: the configuraiton to use for this parser
     pub fn new(config: config::CommandParserConfig<'a>) -> Self {
@@ -25,8 +25,59 @@ impl<'a> CommandParser<'a> {
         }
     }
 
+    /// # Instance Method `CommandParser::parse_command`
+    ///
+    /// Parses the given string as a command if possible, with the provided prefix.
+    ///
+    /// ## Parameters
+    /// - `prefix`, type `&str`: the prefix to parse with
+    /// - `buffer`, type `&str`: the string to parse with
     pub fn parse_command(&'a self, prefix: &'a str, buffer: &'a str) -> Option<ParsedCommand<'a>> {
-        todo!()
+        if !buffer.starts_with(prefix) {
+            return None;
+        }
+
+        let mut index = prefix.len();
+        let command_buffer = buffer.get(index..)?;
+        let command = self.find_command(command_buffer)?;
+
+        index += command.len();
+        index += command_buffer.len() - command_buffer.trim_start().len();
+
+        Some(ParsedCommand {
+            args: args::CommandArgs::new(buffer.get(index..)?),
+            name: command
+        })
+    }
+
+    /// # Private Instance Method `CommandParser::find_command`
+    ///
+    /// Finds a command from the given string.
+    ///
+    /// ## Parameters
+    /// - `buffer`, type `&str`: the string to find a command
+    fn find_command(&'a self, buffer: &'a str) -> Option<&'a str> {
+        let buffer = buffer.split_whitespace().next()?;
+
+        self.config.commands.iter().find_map(|config| {
+            let command = config.name;
+
+            if command == buffer {
+                return Some(command)
+            }
+
+            if config.aliases.is_none() {
+                return None;
+            }
+
+            // it is now safe to call `.unwrap` on the value since we have checked beforehand
+            // whether the value is `None`; would have definitely not been `None` if the code
+            // reaches this branch
+            config.aliases.as_ref().unwrap().iter().find(|&&alias| {
+                alias == command
+            })
+                .map(|value| *value)
+        })
     }
 }
 
