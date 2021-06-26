18096aa43aa99f28f4c7acffdb8b205e789fbc49
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2021-06-26 15:23:59 +0800

    add documentation

--- hartex_cmdsys/src/parser/config.rs
+++ hartex_cmdsys/src/parser/config.rs
@@ -7,18 +7,30 @@
 /// Holds the configuration for a command.
 #[derive(Default)]
 pub struct CommandConfig {
-    name: Option<String>,
+    name: String,
     aliases: Option<Vec<String>>
 }
 
 impl CommandConfig {
+    /// # Static Method `CommandConfig::with_name`
+    ///
+    /// Creates a new `CommandConfig` with the given name.
+    ///
+    /// ## Parameters
+    /// - `name`, type `String`: the command name
     pub fn with_name(name: String) -> Self {
         Self {
-            name: Some(name),
+            name,
             ..Default::default()
         }
     }
 
+    /// # Instance Method `CommandConfig::alias`
+    ///
+    /// Adds an alias to the current command.
+    ///
+    /// ## Parameters
+    /// - `alias`, type `String`: the alias to add
     pub fn alias(mut self, alias: String) -> Self {
         if self.aliases.is_none() {
             self.aliases = Some(vec![alias]);
@@ -45,6 +57,13 @@ pub struct CommandParserConfig {
 }
 
 impl CommandParserConfig {
+    /// # Instance Method `CommandParserConfig::command`
+    ///
+    /// Adds a command (its configuration) to the parser configuration.
+    ///
+    /// ## Parameters
+    /// - `config`, type `CommandConfig`: the command configuratino to add to the parser
+    ///                                   configuration
     pub fn command(mut self, config: CommandConfig) -> Self {
         self.commands.push(config);
         self
