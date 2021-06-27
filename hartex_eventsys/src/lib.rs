daa63309197b490eff914931d46950d1983a6ade
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2021-06-27 12:21:19 +0800

    add `hartex_eventsys` and `hartex_model`

---
+++ hartex_eventsys/src/lib.rs
@@ -0,0 +1,4 @@
+//! # `hartex_eventsys` - A Custom Event System for HarTex Discord bot
+//!
+//! The `hartex_eventsys` crate contains a custom event system as well as several custom events
+//! for the HarTex Discord bot.
