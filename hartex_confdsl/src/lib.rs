c7713abac1d183ca762e9cb23171fcda2dc339ad
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2021-06-28 21:43:00 +0800

    Update lib.rs

--- hartex_confdsl/src/lib.rs
+++ hartex_confdsl/src/lib.rs
@@ -1,7 +1,5 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+//! # `hartex_confdsl` - The HarTex Configuration DSL
+//! 
+//! This library contains an implementation of the Hartex Configuration DSL. It provides an
+//! interface for parsing the DSL into useful Rust objects for use in various functionalities
+//! of the bot.
