ca632a636141b042beec04876f82eb044a307c7f
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2021-07-01 16:41:16 +0800

    add some methods

--- hartex_confdsl/src/read/slice.rs
+++ hartex_confdsl/src/read/slice.rs
@@ -1,6 +1,11 @@
 //! # The `slice` Module
-//! 
+//!
 //! This module defines a reader that operates on a slice of bytes.
+
+use crate::read::{
+    sealed,
+    Pos
+};
 
 /// # Struct `SliceRead`
 ///
@@ -9,3 +14,39 @@ pub struct SliceRead<'bytes> {
     slice: &'bytes [u8],
     index: usize
 }
+
+impl<'bytes> SliceRead<'bytes> {
+    /// # Constructor `SliceRead::new`
+    /// 
+    /// Creates a new `SliceRead` with the given slice of bytes.
+    /// 
+    /// ## Parameters
+    /// - `slice`, of type `&[u8]`: the slice of bytes
+    pub fn new(slice: &'bytes [u8]) -> Self {
+        Self {
+            slice,
+            index: 0
+        }
+    }
+
+    fn pos_or_index(&self, index: usize) -> Pos {
+        let mut pos = Pos {
+            line: 1,
+            column: 0
+        };
+
+        for refbyte in &self.slice[index..] {
+            match *refbyte {
+                b'\n' => {
+                    pos.line += 1;
+                    pos.column = 0;
+                }
+                _ => pos.column += 1
+            }
+        }
+
+        pos
+    }
+}
+
+impl<'bytes> sealed::SealedTrait for SliceRead<'bytes> { }
