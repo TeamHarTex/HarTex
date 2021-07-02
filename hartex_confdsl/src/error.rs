8dd9b0e4742273a4fcd73f05bfaad83dcefbef4f
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2021-07-02 10:18:39 +0800

    remove `error` module

--- hartex_confdsl/src/error.rs
+++
@@ -1,8 +0,0 @@
-//! # The `error` Module
-//!
-//! This module contains definitions and implementations for `HarTexError` from the `hartex_core`
-//! crate.
-//!
-//! Errors that may be generated when serializing or deserializing the HarTex Configuration DSL.
-
-
