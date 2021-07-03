00a67c4df974a80edb1b4c5f8a3249ad8a797e04
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2021-07-03 16:45:29 +0800

    revert "add `compiler` module"

--- hartex_confdsl/compiler/mod.rs
+++
@@ -1,6 +0,0 @@
-//! # The `compiler` Module
-//!
-//! This module contains an implementation of a compiler which would help into converting into
-//! Rust objects via deserialization.
-
-pub mod lexer;
