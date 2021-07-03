aa7911431a6faa4795ffbc9ea782d2f76a4c1554
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2021-07-03 16:45:49 +0800

    add `compiler` module

---
+++ hartex_confdsl/src/compiler/lexer/mod.rs
@@ -0,0 +1,5 @@
+//! # The `lexer` Module
+//!
+//! This module implements a lexical analyzer for the configuration DSL.
+
+pub mod lexeme;
---
+++ hartex_confdsl/src/compiler/mod.rs
@@ -0,0 +1,6 @@
+//! # The `compiler` Module
+//!
+//! This module contains an implementation of a compiler which would help into converting into
+//! Rust objects via deserialization.
+
+pub mod lexer;
