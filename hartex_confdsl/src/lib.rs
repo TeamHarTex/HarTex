711a8646eb9d979adb79d762e1e636d5f4754734
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2021-07-03 16:46:23 +0800

    add `compiler` module

--- hartex_confdsl/src/lib.rs
+++ hartex_confdsl/src/lib.rs
@@ -3,3 +3,5 @@
 //! This library contains an implementation of the Hartex Configuration DSL. It provides an
 //! interface for serializing and deserializing the DSL into useful Rust objects for use in
 //! various functionalities of the bot.
+
+pub mod compiler;
