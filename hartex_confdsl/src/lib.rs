0f5dfa5ff74246dbb3dc2c86c563f7461d5f1503
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2021-07-04 13:03:14 +0800

    remove `hartex_confdsl` crate

--- hartex_confdsl/src/lib.rs
+++
@@ -1,7 +0,0 @@
-//! # `hartex_confdsl` - The HarTex Configuration DSL
-//!
-//! This library contains an implementation of the Hartex Configuration DSL. It provides an
-//! interface for serializing and deserializing the DSL into useful Rust objects for use in
-//! various functionalities of the bot.
-
-pub mod compiler;
