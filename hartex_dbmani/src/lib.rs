7cafc1fedd73bd3f230bb3cfd1c13f45400f4751
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2021-07-04 19:29:30 +0800

    Update lib.rs

--- hartex_dbmani/src/lib.rs
+++ hartex_dbmani/src/lib.rs
@@ -2,3 +2,20 @@
 //!
 //! The `hartex_dbmani` provides an interface for manipulating the PostgreSQL databases used by
 //! HarTex Discord bot.
+
+use std::{
+    future::Future,
+    pin::Pin
+};
+
+use hartex_core::error::HarTexResult;
+
+pub mod whitelist;
+
+/// # Typealias `PendingFuture`
+/// 
+/// Represents a pending future that is yet to return.
+/// 
+/// ## Generic Parameters
+/// - `T`: represents the type that the pending future is to return.
+type PendingFuture<T> = Pin<Box<dyn Future<Output = HarTexResult<T>>>>;
