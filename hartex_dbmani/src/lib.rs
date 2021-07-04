cb92c8a5f7e9585b1c98b4c6113382daa98459a5
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2021-07-04 18:45:59 +0800

    add `hartex_dbmani` crate

---
+++ hartex_dbmani/src/lib.rs
@@ -0,0 +1,4 @@
+//! # `hartex_dbmani` - The HarTex PostgreSQL Database Manipulation Library
+//!
+//! The `hartex_dbmani` provides an interface for manipulating the PostgreSQL databases used by
+//! HarTex Discord bot.
