457b8223ebb32412d7c0c4b8b97cedb30cf8bf64
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2021-07-05 17:07:46 +0800

    remove the `HarTexError` lifetime because that's causing too much trouble

--- hartex_dbmani/src/lib.rs
+++ hartex_dbmani/src/lib.rs
@@ -3,6 +3,8 @@
 //! The `hartex_dbmani` provides an interface for manipulating the PostgreSQL databases used by
 //! HarTex Discord bot.
 
+#![allow(non_snake_case)]
+
 use std::{
     future::Future,
     pin::Pin
@@ -13,9 +15,9 @@ use hartex_core::error::HarTexResult;
 pub mod whitelist;
 
 /// # Typealias `PendingFuture`
-/// 
+///
 /// Represents a pending future that is yet to return.
-/// 
+///
 /// ## Generic Parameters
 /// - `T`: represents the type that the pending future is to return.
 type PendingFuture<T> = Pin<Box<dyn Future<Output = HarTexResult<T>>>>;
