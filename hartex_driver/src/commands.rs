124214f5b70794596fd790593d4adb9cf439312a
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2021-07-07 15:12:23 +0800

    add `handle_command`

--- hartex_driver/src/commands.rs
+++ hartex_driver/src/commands.rs
@@ -1,3 +1,24 @@
 //! # The `commands` Module
-//! 
+//!
 //! This module defines the command handler, which is used when a command is detected in a message.
+
+use hartex_cmdsys::parser::ParsedCommand;
+
+use hartex_core::{
+    discord::{
+        http::Client,
+        cache_inmemory::InMemoryCache
+    },
+    error::HarTexResult
+};
+
+use hartex_eventsys::emitter::EventEmitter;
+
+pub async fn handle_command(
+    command: ParsedCommand<'_>,
+    emitter: EventEmitter,
+    http: Client,
+    cache: InMemoryCache
+) -> HarTexResult<()> {
+    Ok(())
+}
