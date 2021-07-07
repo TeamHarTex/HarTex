8db79865ec4611714007781c69868f5b000307fb
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2021-07-07 16:02:44 +0800

    handle `team` command

--- hartex_driver/src/commands.rs
+++ hartex_driver/src/commands.rs
@@ -2,7 +2,11 @@
 //!
 //! This module defines the command handler, which is used when a command is detected in a message.
 
-use hartex_cmdsys::parser::ParsedCommand;
+use hartex_cmdsys::{
+    command::Command,
+    context::CommandContext,
+    parser::ParsedCommand
+};
 
 use hartex_core::{
     discord::{
@@ -12,17 +16,21 @@ use hartex_core::{
     error::HarTexResult
 };
 
-use hartex_eventsys::emitter::EventEmitter;
+use hartex_eventsys::{
+    emitter::EventEmitter
+};
+
+use hartex_plugins::global::team::Team;
 
 pub async fn handle_command(
     command: ParsedCommand<'_>,
-    emitter: EventEmitter,
-    http: Client,
-    cache: InMemoryCache
+    _: EventEmitter,
+    cache: InMemoryCache,
+    context: CommandContext
 ) -> HarTexResult<()> {
     match command {
-        ParsedCommand { name: "team", .. } => {
-
+        ParsedCommand { name: "team", args } => {
+            Team::execute(context, args, cache).await?;
         },
         _ => ()
     }
