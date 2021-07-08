bb9d9e83146c097b0f1c568fb3c4f4c17055544c
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2021-07-08 19:42:37 +0800

    add `about` command

--- hartex_driver/src/commands.rs
+++ hartex_driver/src/commands.rs
@@ -17,7 +17,12 @@ use hartex_eventsys::{
     emitter::EventEmitter
 };
 
-use hartex_plugins::global::team::Team;
+use hartex_logging::Logger;
+
+use hartex_plugins::global::{
+    about::About,
+    team::Team
+};
 
 pub async fn handle_command(
     command: ParsedCommand<'_>,
@@ -25,7 +30,18 @@ pub async fn handle_command(
     cache: InMemoryCache,
     context: CommandContext
 ) -> HarTexResult<()> {
+    Logger::verbose(
+        "command identified, executing command",
+        Some(module_path!()),
+        file!(),
+        line!(),
+        column!()
+    );
+
     match command {
+        ParsedCommand { name: "about", args } => {
+            About::execute(context, args, cache).await?;
+        }
         ParsedCommand { name: "team", args } => {
             Team::execute(context, args, cache).await?;
         },
