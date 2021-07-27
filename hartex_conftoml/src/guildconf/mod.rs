fc9634c9f4f0701bd14acca38b261cba4327b200
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2021-07-27 16:50:35 +0800

    add timezone enum deserialization

---
+++ hartex_conftoml/src/guildconf/mod.rs
@@ -0,0 +1,47 @@
+//! # The `guildconf` Module
+//!
+//! This module contains configuration models specifically for guild-specific configuration.
+
+use serde::{
+    de,
+    Deserialize
+};
+
+mod tz;
+
+/// # Struct `GuildConfiguration`
+///
+/// Represents guild-specific configuration.
+#[derive(Deserialize)]
+pub struct GuildConfiguration {
+    #[serde(default = "default_nickname")]
+    pub nickname: String,
+    #[serde(default = "default_cmd_prefix")]
+    pub commandPrefix: String,
+    #[serde(default = "default_timezone", deserialize_with = "deserialize_timezone")]
+    pub timezone: tz::Timezone,
+    #[serde(default = "default_dm_cant_use_cmd")]
+    pub dmCannotUseCommand: bool
+}
+
+fn default_nickname() -> String {
+    String::from("HarTex")
+}
+
+fn default_cmd_prefix() -> String {
+    String::from("!")
+}
+
+fn default_timezone() -> tz::Timezone {
+    tz::Timezone::UTC
+}
+
+fn default_dm_cant_use_cmd() -> bool {
+    true
+}
+
+fn deserialize_timezone<'deserialize, Deserializer>(deserializer: Deserializer) -> Result<tz::Timezone, Deserializer::Error>
+    where
+        Deserializer: de::Deserializer<'deserialize> {
+    deserializer.deserialize_string(tz::TimezoneDeserializeStringVisitor)
+}
