4f072b4d99053004451fd96b531acc8df5ef2e01
HTG-YT <htgazurex1212.0001@gmail.com>
2021-07-28 16:23:25 +0800

    add two more planned opt-in nightly features

--- hartex_driver/src/handler.rs
+++ hartex_driver/src/handler.rs
@@ -348,7 +348,7 @@ impl EventHandler {
                         line!(),
                         column!()
                     );
-                },
+                }
                 _ => ()
             };
 
