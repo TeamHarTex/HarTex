40762e827480db6699d149f9d6ad3a0d7e61a5c5
HTG-YT <htgazurex1212.0001@gmail.com>
2021-10-01 20:46:34 +0800

    implement some of guildinfo command
    This commit implements a part of the guildinfo command.

--- hartex_plugins/src/information/guildinfo.rs
+++ hartex_plugins/src/information/guildinfo.rs
@@ -10,11 +10,20 @@ use hartex_cmdsys::{
     context::CommandContext
 };
 
-use hartex_core::discord::{
-    cache_inmemory::InMemoryCache,
-    model::application::command::{
-        ChoiceCommandOptionData,
-        CommandOption
+use hartex_core::{
+    discord::{
+        cache_inmemory::InMemoryCache,
+        model::application::{
+            callback::{
+                CallbackData,
+                InteractionResponse
+            },
+            interaction::Interaction
+        },
+    },
+    error::{
+        HarTexError,
+        HarTexResult
     }
 };
 
@@ -38,7 +47,46 @@ impl Command for Guildinfo {
         CommandType::ChatInput
     }
 
-    fn execute<'asynchronous_trait>(&self, _: CommandContext, _: InMemoryCache) -> FutureRetType<'asynchronous_trait, ()> {
-        todo!()
+    fn execute<'asynchronous_trait>(&self, ctx: CommandContext, _: InMemoryCache) -> FutureRetType<'asynchronous_trait, ()> {
+        Box::pin(execute_guildinfo_command(ctx))
     }
+}
+
+/// # Asynchronous Function `execute_guildinfo_command`
+///
+/// Executes the `guildinfo` command.
+///
+/// ## Parameters
+/// - `ctx`, type `CommandContext`: the command context to use.
+async fn execute_guildinfo_command(ctx: CommandContext) -> HarTexResult<()> {
+    let interaction = match ctx.interaction.clone() {
+        Interaction::ApplicationCommand(command) => command,
+        _ => return Err(
+            HarTexError::Custom {
+                message: String::from("invalid interaction type: expected ApplicationCommand")
+            }
+        )
+    };
+
+    if interaction.guild_id.is_none() || interaction.user.is_some() {
+        ctx.http
+            .interaction_callback(
+                interaction.id,
+                &interaction.token,
+                &InteractionResponse::ChannelMessageWithSource(
+                    CallbackData {
+                        allowed_mentions: None,
+                        components: None,
+                        content: Some(String::from(":x: This command can only be used in a guild.")),
+                        embeds: vec![],
+                        flags: None,
+                        tts: None
+                    }
+                )
+            )
+            .exec()
+            .await?;
+    }
+
+    Ok(())
 }
