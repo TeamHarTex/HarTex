dd95482904decc36531fb6e13e33fc7ab1e76b00
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2021-10-12 16:34:08 +0800

    redeny doc_markdown and fix the lint

--- hartex/src/main.rs
+++ hartex/src/main.rs
@@ -1,9 +1,8 @@
-//! # `hartex` - The HarTex Binary Crate
+//! # `hartex` - The `HarTex` Binary Crate
 //!
 //! This crate contains the main function which calls the `hartex_main` "main function" in the
 //! `hartex_driver` crate which glues everything together.
 
-#![allow(clippy::doc_markdown)]
 #![deny(clippy::pedantic)]
 
 use std::time::Duration;
