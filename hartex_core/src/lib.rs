b21ca99391a9e9f8ffc256aa3df32abad0311925
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2021-10-18 14:53:33 +0800

    daily update
    Signed-off-by: HTG-YT <39023054+HTG-YT@users.noreply.github.com>

--- hartex_core/src/lib.rs
+++ hartex_core/src/lib.rs
@@ -24,4 +24,4 @@ pub mod time;
 /// # Static `HARTEX_BUILD`
 ///
 /// Represents the current version and build of HarTex Discord bot.
-pub static HARTEX_BUILD: &'static str = "Version 1.22.0, 22H1 (Build 478)";
+pub static HARTEX_BUILD: &str = "Version 1.22.0, 22H1 (Build 480)";
