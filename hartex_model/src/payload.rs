fd4b85352a4223377be88d7c48af04cdce3eb031
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2021-11-18 20:11:53 +0800

    update tracing spans

--- hartex_model/src/payload.rs
+++ hartex_model/src/payload.rs
@@ -2,13 +2,10 @@
 //!
 //! This module contains various event payloads used for the custom event system.
 
-use hartex_core::discord::model::id::GuildId;
-
 /// # Struct `CommandExecuted`
 ///
-/// The payload for which when the command is executed.
+/// The payload for which when a command is executed and is received by the bot.
 #[derive(Clone)]
 pub struct CommandExecuted {
-    pub command: String,
-    pub guild_id: GuildId
+    pub command: &'static str
 }
