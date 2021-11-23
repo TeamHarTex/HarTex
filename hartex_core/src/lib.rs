85aa8af5c629ba018dd049fd74e1a2ad3c2d2780
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2021-11-23 14:30:37 +0800

    daily update

--- hartex_core/src/lib.rs
+++ hartex_core/src/lib.rs
@@ -23,4 +23,4 @@ pub mod time;
 /// # Static `HARTEX_BUILD`
 ///
 /// Represents the current version and build of `HarTex` Discord bot.
-pub static HARTEX_BUILD: &str = "Version 1.23.0-nightly-22-11-2021 (Build 562)";
+pub static HARTEX_BUILD: &str = "Version 1.23.0-nightly-23-11-2021 (Build 564)";
