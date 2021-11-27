d6c24201b8c60427cbd2087aa3491ac0807ff7a3
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2021-11-27 21:10:13 +0800

    add lifetime mess with permlvl map

--- hartex_conftoml/src/permlvl/roles.rs
+++
@@ -1,6 +0,0 @@
-//! # The `roles` Module
-//!
-//! This module contains the roles counterpart of the permission levels.
-
-#[allow(clippy::module_name_repetitions)]
-pub struct PermissionLevelsRoles;
