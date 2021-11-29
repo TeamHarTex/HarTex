00aae4c46e6bbf7ff44d5f3045c2c1fadf16b396
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2021-11-29 17:38:03 +0800

    daily update

--- hartex_core/src/lib.rs
+++ hartex_core/src/lib.rs
@@ -23,4 +23,4 @@ pub mod time;
 /// # Static `HARTEX_BUILD`
 ///
 /// Represents the current version and build of `HarTex` Discord bot.
-pub static HARTEX_BUILD: &str = "Version 1.23.0-nightly-28-11-2021 (Build 582)";
+pub static HARTEX_BUILD: &str = "Version 1.23.0-nightly-28-11-2021 (Build 584)";
