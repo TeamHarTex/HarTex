ef77c563f857316bd5b0d62665d74eae4ecd328e
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2021-11-29 20:18:24 +0800

    experiment with static assertions

--- hartex_macros/src/lib.rs
+++ hartex_macros/src/lib.rs
@@ -1,3 +1,7 @@
 //! # `hartex_macros` - The `HarTex` Macros Library
 //!
 //! The `hartex_macros` implements some useful macros for use within the bot.
+
+#![feature(decl_macro)]
+
+pub mod static_assert;
---
+++ hartex_macros/src/static_assert.rs
@@ -0,0 +1,15 @@
+//! # Module `static_assert`
+//!
+//! This module contains macros for static assertions.
+
+/// # Macro `static_assert_impl_all`
+///
+/// This macro asserts that an object implements all the listed traits.
+pub macro static_assert_impl_all {
+    (type $type:ty: traits $($trait:path),+ $(,)?) => {
+        const _: fn() = || {
+            fn static_assert_impl_all<$($lt)+, T: ?Sized $(+ $trait)+>() {}
+            static_assert_impl_all<$type>();
+        };
+    }
+}
