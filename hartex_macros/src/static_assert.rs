2b10fdcaab75e9af1d0f672b2ace03e04b38c6ca
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2021-11-29 20:35:25 +0800

    fix macros

--- hartex_macros/src/static_assert.rs
+++ hartex_macros/src/static_assert.rs
@@ -8,7 +8,7 @@
 pub macro static_assert_impl_all {
     (type $type:ty: traits $($trait:path),+ $(,)?) => {
         const _: fn() = || {
-            fn static_assert_impl_all<$($lt)+, T: ?Sized $(+ $trait)+>() {}
+            fn static_assert_impl_all<T: ?Sized $(+ $trait)+>() {}
             static_assert_impl_all<$type>();
         };
     }
