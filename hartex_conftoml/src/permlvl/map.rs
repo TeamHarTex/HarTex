feb9c14322ffbdc0d8f0ce0faffbc9ed00be0e2d
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2021-11-30 20:13:24 +0800

    remove stupid mistake of conflicting implementations

--- hartex_conftoml/src/permlvl/map.rs
+++ hartex_conftoml/src/permlvl/map.rs
@@ -24,7 +24,7 @@ use serde::{
 ///
 /// Represents a permission level map over an `Id` generic parameter.
 #[allow(clippy::module_name_repetitions)]
-#[derive(Clone, Debug, Deserialize, PartialEq)]
+#[derive(Clone, Debug, PartialEq)]
 pub struct PermissionLevelMap<Id: Clone + Eq + Hash> {
     #[allow(dead_code)]
     pub map: DashMap<Id, u8>
