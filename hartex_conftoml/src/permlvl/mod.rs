a3100c0feeb84d3ddb60806db67414b4af81cef7
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2021-12-02 20:28:26 +0800

    update roleid thing

--- hartex_conftoml/src/permlvl/mod.rs
+++ hartex_conftoml/src/permlvl/mod.rs
@@ -9,10 +9,25 @@
 // [PermissionLevels.Users]
 // UserId1 = <perm id>
 
-pub mod map;
+use std::{
+    fmt::{
+        Formatter,
+        Result as FmtResult
+    },
+    num::NonZeroU64
+};
 
-use hartex_core::discord::model::id::RoleId;
-use serde::Deserialize;
+use dashmap::DashMap;
+use serde::{
+    de::{
+        Error,
+        Visitor
+    },
+    Deserialize,
+    Deserializer
+};
+
+pub mod map;
 
 /// # Struct `PermissionLevels`
 ///
@@ -22,18 +37,67 @@ pub struct PermissionLevels {
     pub Roles: map::PermissionLevelMap<RoleId>
 }
 
+impl Default for PermissionLevels {
+    fn default() -> Self {
+        Self {
+            Roles: map::PermissionLevelMap {
+                map: DashMap::new()
+            }
+        }
+    }
+}
+
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+pub struct RoleId(NonZeroU64);
+
+impl<'deserialize> Deserialize<'deserialize> for RoleId {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'deserialize> {
+        deserializer.deserialize_str(PermissionLevelsRolesMapRoleIdDeserializerRefstrVisitor)
+    }
+}
+
+pub struct PermissionLevelsRolesMapRoleIdDeserializerRefstrVisitor;
+
+impl <'visitor> Visitor<'visitor> for PermissionLevelsRolesMapRoleIdDeserializerRefstrVisitor {
+    type Value = RoleId;
+
+    fn expecting(&self, f: &mut Formatter) -> FmtResult {
+        write!(f, "a string representing a role id")
+    }
+
+    fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+        where E: Error {
+        let res = v.parse::<u64>();
+        if res.is_err() {
+            return Err(Error::custom("invalid integer"));
+        }
+
+        let nonzero_uint = NonZeroU64::new(res.unwrap());
+        if nonzero_uint.is_none() {
+            return Err(Error::custom("role id must not be zero"));
+        }
+
+        Ok(RoleId(nonzero_uint.unwrap()))
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use std::fmt::Debug;
+    use std::{
+        fmt::Debug,
+        num::NonZeroU64
+    };
 
     use dashmap::DashMap;
-    use hartex_core::discord::model::id::RoleId;
     use serde_test::Token;
 
     use super::{
         map::PermissionLevelMap,
         Deserialize,
-        PermissionLevels
+        PermissionLevels,
+        RoleId
     };
 
     const _: fn() = || {
@@ -49,11 +113,11 @@ mod tests {
     #[test]
     fn test_dashacc_de() {
         let dashmap = DashMap::new();
-        dashmap.insert(RoleId::new(1234567887654321).unwrap(), 100);
-        dashmap.insert(RoleId::new(2345678998765432).unwrap(), 90);
-        dashmap.insert(RoleId::new(3456789009876543).unwrap(), 80);
-        dashmap.insert(RoleId::new(9876543223456789).unwrap(), 50);
-        dashmap.insert(RoleId::new(8765432112345678).unwrap(), 10);
+        dashmap.insert(RoleId(NonZeroU64(1234567887654321)), 100);
+        dashmap.insert(RoleId(NonZeroU64(2345678998765432)), 90);
+        dashmap.insert(RoleId(NonZeroU64(3456789009876543)), 80);
+        dashmap.insert(RoleId(NonZeroU64(9876543223456789)), 50);
+        dashmap.insert(RoleId(NonZeroU64(8765432112345678)), 10);
 
         serde_test::assert_de_tokens(
             &PermissionLevels {
