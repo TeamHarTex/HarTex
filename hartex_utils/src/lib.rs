0daaf7670e1c894cc07505d69eadb5db443bb2c4
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2021-12-07 18:25:56 +0800

    breaking 1.24.0: use GPL v2

--- hartex_utils/src/lib.rs
+++ hartex_utils/src/lib.rs
@@ -1,3 +1,24 @@
+/*
+ * This file is part of HarTex.
+ *
+ * HarTex
+ * Copyright (c) 2021 HarTex Project Developers
+ *
+ * HarTex is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * HarTex is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
 //! # `hartex_utils` - The `HarTex` Utilities Library
 //!
 //! The `hartex_utils` library provides several utilities for the `HarTex` Discord bot.
