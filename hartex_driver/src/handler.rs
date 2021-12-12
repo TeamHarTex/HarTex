26e1499970a9355201dbe20a32939c8057b26699
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2021-12-12 09:23:57 +0800

    relicense as AGPL v3

--- hartex_driver/src/handler.rs
+++ hartex_driver/src/handler.rs
@@ -1,4 +1,4 @@
-/* SPDX-License-Identifier: GPL-2.0-only
+/* SPDX-License-Identifier: AGPL-3.0-only
  *
  * This file is part of HarTex.
  *
@@ -6,8 +6,8 @@
  * Copyright (c) 2021 HarTex Project Developers
  *
  * HarTex is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
  *
  * HarTex is distributed in the hope that it will be useful,
@@ -15,9 +15,8 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ * You should have received a copy of the GNU Affero General Public License along
+ * with HarTex; if not, If not, see <https://www.gnu.org/licenses/>.
  */
 
 //! # The `handler` Module
