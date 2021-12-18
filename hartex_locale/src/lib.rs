74b54d6aadb9c87d1b9d4097d2f687f5ea24addc
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2021-12-18 13:18:40 +0800

    fix tests

--- hartex_locale/src/lib.rs
+++ hartex_locale/src/lib.rs
@@ -119,6 +119,6 @@ mod tests {
     fn test_en_us_lang_id() {
         let locale = Locale::load(&Path::new("hartex_locale/langcfgs/en_GB.langcfg")).unwrap();
 
-        assert_eq!(locale.lang_id() == String::from("en_US"));
+        assert_eq!(locale.lang_id(), String::from("en_US"));
     }
 }
