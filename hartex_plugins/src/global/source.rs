cce3680ef440a3604a4e9ac071763ac20c64f204
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2021-12-19 18:01:05 +0800

    add localisation for source command

--- hartex_plugins/src/global/source.rs
+++ hartex_plugins/src/global/source.rs
@@ -45,8 +45,11 @@ use hartex_core::{
         HarTexError,
         HarTexResult
     },
-    logging::tracing
+    logging::tracing,
+    STABLE,
 };
+use hartex_dbmani::guildconf::GetGuildConfig;
+use hartex_locale::Locale;
 use hartex_utils::FutureRetType;
 
 /// # Struct `Source`
@@ -94,6 +97,25 @@ async fn execute_source_command(ctx: CommandContext) -> HarTexResult<()> {
         });
     };
 
+    let message = if interaction.guild_id.is_none() || interaction.user.is_some() {
+        String::from("The source code of the bot can be found at:")
+    }
+    else {
+        let config = GetGuildConfig::new(interaction.guild_id.unwrap()).await?;
+
+        if !STABLE && config.NightlyFeatures.localization {
+            let locale = config.GuildConfiguration.locale;
+            let locale_file = Locale::load(&format!("../../langcfgs/{locale}.langcfg"))?;
+
+            locale_file
+                .lookup("GlobalPlugin.SourceCommand.PreRepositoryUriMessage")
+                .unwrap()
+        }
+        else {
+            String::from("The source code of the bot can be found at:")
+        }
+    };
+
     tracing::trace!("responding to interaction");
 
     if let Err(error) = ctx
@@ -105,9 +127,7 @@ async fn execute_source_command(ctx: CommandContext) -> HarTexResult<()> {
                 allowed_mentions: None,
                 components: None,
                 content: Some(
-                    String::from(
-                        "The source code for the bot can be found at: <https://github.com/HarTexTeam/HarTex-rust-discord-bot>."
-                    )
+                    format!("{message} <https://github.com/HarTexTeam/HarTex-rust-discord-bot>.")
                 ),
                 embeds: vec![],
                 flags: None,
