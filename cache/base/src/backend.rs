4ac622f46f1d20485b29b6fc377a974d2d640479
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2021-12-26 20:19:50 +0800

    add cache directory

---
+++ cache/base/src/backend.rs
@@ -0,0 +1,31 @@
+/* SPDX-License-Identifier: AGPL-3.0-only
+ *
+ * This file is part of HarTex.
+ *
+ * HarTex
+ * Copyright (c) 2021 HarTex Project Developers
+ *
+ * HarTex is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * HarTex is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with HarTex. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+//! # `backend` Module
+//!
+//! This module contains a base trait for a cache backend.
+
+/// # Trait `Backend`
+///
+/// A base trait for a cache backend. Implement this trait for your custom cache
+/// backend, an in-memory backend, a database backend, a redis backend, just to
+/// list a few examples.
+pub trait Backend: Send + Sync + Sized + 'static {}
