29b265833ae9e243b94b782891f9448ee5fc26b7
HTGAzureX1212 <39023054+HTG-YT@users.noreply.github.com>
2021-12-31 16:41:39 +0800

    Merge of #72 - HarTexTeam:update-x-py

--- x.py
+++ x.py
@@ -28,8 +28,8 @@ import sys
 
 
 class HarTexBranch:
-    STABLE = {"string": "stable", "stable": "true", "version": "1.25.0"}
-    NIGHTLY = {"string": "nightly", "stable": "false", "version": "1.26.0"}
+    STABLE = {"string": "stable", "stable": "true", "version": "1.26.0"}
+    NIGHTLY = {"string": "nightly", "stable": "false", "version": "1.27.0"}
 
 
 class HarTexBuild:
