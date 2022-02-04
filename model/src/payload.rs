5411eb1bceb568705642adeb5b8320566ae0c97d
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2022-02-04 13:48:38 +0800

    revamp documentation for model crate

--- model/src/payload.rs
+++ model/src/payload.rs
@@ -19,12 +19,8 @@
  * with HarTex. If not, see <https://www.gnu.org/licenses/>.
  */
 
-//! # The `payload` Module
-//!
-//! This module contains various event payloads used for the custom event system.
+//! Event payloads used for the custom event system.
 
-/// # Struct `CommandExecuted`
-///
 /// The payload for which when a command is executed and is received by the bot.
 #[derive(Clone)]
 pub struct CommandExecuted {
