5e318241c5a0dc288e57cff3ad20ca40b1f43974
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2022-02-28 21:17:56 +0800

    add current_user insert

--- cache/discord/src/postgres/repositories.rs
+++ cache/discord/src/postgres/repositories.rs
@@ -1,7 +1,10 @@
 //! Repositories in the PostgreSQL backend.
 
+use std::env;
+
 use cache_base::future::UpsertEntityFuture;
 use cache_base::{Backend, Repository};
+use sqlx::postgres::PgPool;
 
 use crate::entities::users::CachedCurrentUser;
 use crate::postgres::error::PostgresBackendError;
@@ -12,9 +15,13 @@ pub struct CurrentUserRepository;
 impl Repository<PostgresBackend, CachedCurrentUser> for CurrentUserRepository {
     fn upsert(
         &self,
-        current_user: CachedCurrentUser,
+        _: CachedCurrentUser,
     ) -> UpsertEntityFuture<'_, PostgresBackendError> {
         Box::pin(async {
+            let pgsql_creds = env::var("PGSQL_CACHE_DB_CREDENTIALS")?;
+            let _ = PgPool::connect(&pgsql_creds).await?;
+            let _ = include_str!("../../../postgres/repositories/current_user/upsert.sql");
+
             Ok(())
         })
     }
