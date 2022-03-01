b877dbdcd0d5f1eb41c1db5831266f6ec9adee2c
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2022-03-01 10:35:23 +0800

    add binding to prepared statement - still wip

--- cache/discord/src/postgres/repositories.rs
+++ cache/discord/src/postgres/repositories.rs
@@ -13,11 +13,30 @@ use crate::postgres::PostgresBackend;
 pub struct CurrentUserRepository;
 
 impl Repository<PostgresBackend, CachedCurrentUser> for CurrentUserRepository {
-    fn upsert(&self, _: CachedCurrentUser) -> UpsertEntityFuture<'_, PostgresBackendError> {
+    fn upsert(&self, current_user: CachedCurrentUser) -> UpsertEntityFuture<'_, PostgresBackendError> {
         Box::pin(async {
             let pgsql_creds = env::var("PGSQL_CACHE_DB_CREDENTIALS")?;
-            let _ = PgPool::connect(&pgsql_creds).await?;
-            let _ = include_str!("../../include/postgres/repositories/current_user/upsert.sql");
+            let mut pool = PgPool::connect(&pgsql_creds).await?;
+            let query = include_str!("../../include/postgres/repositories/current_user/upsert.sql");
+
+            sqlx::query(query)
+                .bind(current_user.id.to_string())
+                .bind(current_user.username)
+                .bind(current_user.discriminator)
+                .bind(current_user.avatar)
+                .bind(current_user.bot)
+                .bind(current_user.system)
+                .bind(current_user.mfa_enabled)
+                .bind(current_user.banner)
+                .bind(current_user.accent_colour)
+                .bind(current_user.locale)
+                .bind(current_user.verified)
+                .bind(current_user.email)
+                .bind(current_user.flags)
+                .bind(current_user.premium_type)
+                .bind(current_user.public_flags)
+                .execute(&mut pool)
+                .await?;
 
             Ok(())
         })
