b9bbd8812989d05b96ee0a57d5e6b01f319d743d
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2022-03-05 20:50:37 +0800

    add http client

--- rest/src/main.rs
+++ rest/src/main.rs
@@ -23,5 +23,66 @@
 //!
 //! The REST process binary acts as a proxy over the Discord API.
 
+use std::env as stdenv;
+
+use base::error::Result;
+use base::logging;
+use base::panicking;
+use hyper::{Body, Client};
+use hyper_rustls::HttpsConnectorBuilder;
+use hyper_trust_dns::{TrustDnsHttpConnector, TrustDnsResolver};
+
 #[tokio::main]
-pub async fn main() {}
+pub async fn main() -> Result<()> {
+    logging::init();
+    panicking::init();
+
+    if let Err(error) = env::load() {
+        log::error!("env error: {error}");
+        log::warn!("environment variables cannot be loaded; exiting");
+        log::info!("help: please make sure that the required environment variables are present");
+        log::info!(r#"help: see the above errors (those that start with "retrieval failed")"#);
+
+        return Ok(());
+    }
+
+    log::trace!("retrieving port to listen on");
+    let result = stdenv::var("REST_SERVER_PORT");
+    let port = if let Ok(port) = result {
+        let result = port.parse::<u16>();
+        if let Ok(port) = result {
+            port
+        } else {
+            log::error!(
+                "processing error: port is not an integer: {}",
+                result.unwrap_err()
+            );
+            return Ok(());
+        }
+    } else {
+        log::error!("env error: {}", result.unwrap_err());
+        return Ok(());
+    };
+
+    log::trace!("creating https connector for http client");
+    let https_connector = {
+        let mut connector = TrustDnsHttpConnector::new_with_resolver(TrustDnsResolver::new());
+        connector.enforce_http(false);
+
+        let builder = HttpsConnectorBuilder::new()
+            .with_webpki_roots()
+            .https_only()
+            .enable_http1();
+
+        if let Ok(enabled) = stdenv::var("REST_HTTP_2_ENABLED") && matches!(&enabled, "true") {
+            builder.enable_http2().wrap_connector(connector)
+        } else {
+            builder.wrap_connector(connector)
+        }
+    };
+
+    log::trace!("creating http client");
+    let client = Client::builder().build::<_, Body>(https_connector);
+
+    Ok(())
+}
