15596bf26de4fb14b0776945a83ebf05de0f4f3a
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2022-04-23 19:05:33 +0800

    add rest tide server

---
+++ rest/src/proxy.rs
@@ -0,0 +1,32 @@
+/* SPDX-License-Identifier: AGPL-3.0-only
+ *
+ * This file is part of HarTex.
+ *
+ * HarTex
+ * Copyright (c) 2021-2022 HarTex Project Developers
+ *
+ * HarTex is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * HarTex is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with HarTex. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+use hyper::Client;
+use hyper_rustls::HttpsConnector;
+use hyper_trust_dns::TrustDnsHttpConnector;
+use tide::{Request, Response, Result};
+
+pub async fn proxy_requests(
+    _: Request<()>,
+    _: Client<HttpsConnector<TrustDnsHttpConnector>>,
+) -> Result<Response> {
+    todo!()
+}
