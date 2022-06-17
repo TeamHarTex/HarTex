ea87a5fdcde4164b54520d6ddb24ef768677e6b1
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2022-06-17 19:16:17 +0800

    add cmdline stuff

--- gateway/lib/lib.rs
+++ gateway/lib/lib.rs
@@ -18,15 +18,3 @@
  * You should have received a copy of the GNU Affero General Public License along
  * with HarTex. If not, see <https://www.gnu.org/licenses/>.
  */
-
-use base::error::Result;
-use base::logging;
-use base::panicking;
-
-#[tokio::main]
-pub async fn main() -> Result<()> {
-    logging::init();
-    panicking::init();
-
-    Ok(())
-}
