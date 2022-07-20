7b0fc9787e5920916615ebc8839de32b54ee3f54
HTGAzureX1212 <39023054+HTG-YT@users.noreply.github.com>
2022-07-20 15:56:20 +0800

    fix paths and partially add x.py make command

--- bootstrap/bootstrap_make.py
+++ bootstrap/bootstrap_make.py
@@ -18,11 +18,5 @@
 # You should have received a copy of the GNU Affero General Public License along
 # with HarTex. If not, see <https://www.gnu.org/licenses/>.
 
-import bootstrap
-import sys
-
-if sys.version_info.major < 3:
-    print("x: \u001B[1;31mfatal: \u001B[0mthis script is running in Python 2")
-    print("x: \u001B[1;31mfatal: \u001B[0mthis script requires Python 3 or above")
-else:
-    bootstrap.run()
+def run_make():
+    pass
