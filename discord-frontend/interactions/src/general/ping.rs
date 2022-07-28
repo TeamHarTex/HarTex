d27a0f158156659e14b5b0ebe32d662fc27cf315
HTGAzureX1212 <39023054+HTG-YT@users.noreply.github.com>
2022-07-28 12:58:06 +0800

    use custom wrapper for command (to be impl'd)

--- discord-frontend/interactions/src/general/ping.rs
+++ discord-frontend/interactions/src/general/ping.rs
@@ -19,40 +19,20 @@
  * with HarTex. If not, see <https://www.gnu.org/licenses/>.
  */
 
-use base::discord::model::application::command::{Command, CommandType};
-use base::discord::model::application::interaction::Interaction;
+use base::discord::model::application::interaction::InteractionData;
 use base::discord::model::id::Id;
+use ext::discord::model::application::command::HarTexCommand;
 
 use crate::{BaseInteraction, HandleInteractionFuture};
 
 pub struct PingCommand;
 
 impl BaseInteraction for PingCommand {
-    fn commands() -> Vec<Command> {
-        #[cfg(stable)]
-        let application_id = Some(Id::new(936431574310879332));
-        #[cfg(not(stable))]
-        let application_id = Some(Id::new(936432439767740436));
-
-        vec![
-            Command {
-                application_id,
-                default_member_permissions: None,
-                dm_permission: Some(false),
-                description: String::new(),
-                description_localizations: None,
-                guild_id: None,
-                id: None,
-                kind: CommandType::ChatInput,
-                name: String::from("ping"),
-                name_localizations: None,
-                options: vec![],
-                version: Id::new(1),
-            }
-        ]
+    fn commands() -> Vec<HarTexCommand> {
+        vec![]
     }
 
-    fn handle(interation: Interaction) -> HandleInteractionFuture {
+    fn handle(interation: InteractionData) -> HandleInteractionFuture {
         todo!()
     }
 }
