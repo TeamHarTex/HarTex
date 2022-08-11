833993c1dcfc1ab8f5f552b802ce1ee0d459cc32
Lunari8546 <63549306+Lunari8546@users.noreply.github.com>
2022-08-11 14:49:21 +0800

    Cleanup files & code.
    - Cleaned CSS classes.
    - Changed known CRLF to LF.

--- web-fullstack/web/src/pages/HomePage/HomePage.tsx
+++ web-fullstack/web/src/pages/HomePage/HomePage.tsx
@@ -25,9 +25,9 @@ const HomePage = () => {
   return (
     <main>
       <MetaTags title="Home" description="HarTex" />
-      <section className="hero flex justify-center items-center text-center xl:text-left">
+      <section className="flex justify-center items-center text-center xl:text-left">
         <div className="flex-1">
-          <h1 className="text-7xl font-bold">HarTex</h1>
+          <h1>HarTex</h1>
           <br />
           <p className="text-xl font-normal">
             An advanced adminsitration assistant
@@ -42,10 +42,10 @@ const HomePage = () => {
         </div>
       </section>
       <section className="features">
-        <h2 className="text-7xl font-bold">Features</h2>
+        <h2>Features</h2>
       </section>
       <section className="faq">
-        <h2 className="text-7xl font-bold">F.A.Q</h2>
+        <h2>F.A.Q</h2>
       </section>
     </main>
   )
