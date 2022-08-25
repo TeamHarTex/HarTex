a8f214d41739ed96952d38f6b66b2def693b9e7a
HTGAzureX1212 <39023054+HTG-YT@users.noreply.github.com>
2022-08-25 12:41:51 +0800

    create DocumentationPage

---
+++ web-fullstack/web/src/pages/DocumentationPage/DocumentationPage.stories.tsx
@@ -0,0 +1,7 @@
+import DocumentationPage from './DocumentationPage'
+
+export const generated = (args) => {
+  return <DocumentationPage  {...args} />
+}
+
+export default { title: 'Pages/DocumentationPage' }
