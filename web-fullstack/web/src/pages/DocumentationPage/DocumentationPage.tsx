d43c3f645c194ac4e09c77fa1f6f0b3b6feee97f
HTGAzureX1212 <39023054+HTG-YT@users.noreply.github.com>
2022-08-25 16:51:48 +0800

    update styling

--- web-fullstack/web/src/pages/DocumentationPage/DocumentationPage.tsx
+++ web-fullstack/web/src/pages/DocumentationPage/DocumentationPage.tsx
@@ -2,16 +2,16 @@ import { MetaTags } from '@redwoodjs/web'
 
 const DocumentationPage = () => {
   return (
-    <>
+    <main>
       <MetaTags title="Documentation" description="HarTex" />
 
       <div className="flex overflow-hidden">
         <div className="flex-[0_0_350px] overflow-hidden bg-dark-grey">
         </div>
-        <div className="flex-[1_1_1440px] overflow-hidden">
+        <div className="flex-[1_1_1440px] overflow-hidden pr-[17px]">
         </div>
       </div>
-    </>
+    </main>
   )
 }
 
