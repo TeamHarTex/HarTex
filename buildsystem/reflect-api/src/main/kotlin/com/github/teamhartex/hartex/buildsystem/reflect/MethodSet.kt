2fd4efea08b2ba2b33558b34436d3c8c5293c49b
HTGAzureX1212 <39023054+HTG-YT@users.noreply.github.com>
2022-10-08 14:04:48 +0800

    update MethodSet class

--- buildsystem/reflect-api/src/main/kotlin/com/github/teamhartex/hartex/buildsystem/reflect/MethodSet.kt
+++ buildsystem/reflect-api/src/main/kotlin/com/github/teamhartex/hartex/buildsystem/reflect/MethodSet.kt
@@ -2,21 +2,58 @@ package com.github.teamhartex.hartex.buildsystem.reflect
 
 import com.google.common.collect.Maps
 import java.lang.reflect.Method
+import java.lang.reflect.Modifier
+import java.util.Collections
+import java.util.Objects
+import kotlin.collections.Collection as ICollection
 import kotlin.collections.Iterable as IIterable
-import kotlin.collections.List as IList
-import kotlin.collections.Map as IMap
+import kotlin.collections.MutableMap as IMutableMap
 import kotlin.reflect.KClass as IKClass
 
 class MethodSet : IIterable<Method> {
-  private val methods: IMap<MethodKey, Method> = Maps.newLinkedHashMap()
+  private var methods: IMutableMap<MethodKey, Method> = Maps.newLinkedHashMap()
 
-  class MethodKey {
-    private val method: Method
-    private val parameterTypes: IList<IKClass<Any>>
+  fun add(method: Method) {
+    val key = MethodKey(method)
+    val old = methods[key]
 
-    constructor(method: Method) {
-      this.method = method
-      this.parameterTypes = method.parameterTypes.map { it.kotlin }
+    if (old == null || shouldNewReplaceOld(old, method))
+      methods[key] = method
+  }
+
+  fun isEmpty(): Boolean = methods.isEmpty()
+
+  override fun iterator(): Iterator<Method> = values().iterator()
+
+  private fun shouldNewReplaceOld(old: Method, new: Method): Boolean {
+    val isOldAbstract = Modifier.isAbstract(old.modifiers)
+    val isNewAbstract = Modifier.isAbstract(new.modifiers)
+
+    if (isOldAbstract != isNewAbstract)
+      return isOldAbstract
+
+    return old.isBridge && !new.isBridge
+  }
+
+  private fun values(): ICollection<Method> = Collections.unmodifiableCollection(methods.values)
+
+  class MethodKey(private val method: Method) {
+    private val parameterTypes: Array<IKClass<out Any>> = method.parameterTypes.map { it.kotlin }.toTypedArray()
+
+    override fun equals(other: Any?): Boolean {
+      if (this === other)
+        return true
+      else if (other == null || this::class != other::class)
+        return false
+
+      val otherKey = other as MethodKey
+      return Objects.equals(this.method.name, otherKey.method.name)
+        && Objects.equals(this.method.returnType, otherKey.method.returnType)
+        && this.parameterTypes.contentEquals(otherKey.parameterTypes)
+    }
+
+    override fun hashCode(): Int {
+      return Objects.hash(method.name, parameterTypes.size)
     }
   }
 }
