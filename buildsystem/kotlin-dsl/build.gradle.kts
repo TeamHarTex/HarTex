6943cf0928e712fb22f2ba10efcd00a191746a2a
HTGAzureX1212 <39023054+HTG-YT@users.noreply.github.com>
2022-10-08 16:55:37 +0800

    Revert "redo DSL"
    This reverts commit b30e043f3f00d26344869164b9d2187898d12b91.

---
+++ buildsystem/kotlin-dsl/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    kotlin("jvm") version "1.7.20"
+}
+
+group = "com.github.teamhartex"
+version = "0.1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-scripting-jvm:1.7.20")
+    implementation(project(":tooling-api"))
+}
