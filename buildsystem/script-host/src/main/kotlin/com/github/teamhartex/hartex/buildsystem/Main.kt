d249f85d1f4083561ca25463efe6f2f9c355fe8a
HTGAzureX1212 <39023054+HTG-YT@users.noreply.github.com>
2022-10-10 19:09:38 +0800

    a lot of reflection

--- buildsystem/script-host/src/main/kotlin/com/github/teamhartex/hartex/buildsystem/Main.kt
+++ buildsystem/script-host/src/main/kotlin/com/github/teamhartex/hartex/buildsystem/Main.kt
@@ -1,7 +1,12 @@
 package com.github.teamhartex.hartex.buildsystem
 
 import java.io.File
+import java.lang.IllegalArgumentException
+import kotlin.reflect.full.createInstance
+import kotlin.reflect.full.memberProperties
+import kotlin.script.experimental.api.ResultWithDiagnostics
 import kotlin.script.experimental.api.ScriptDiagnostic
+import kotlin.script.experimental.api.onSuccess
 
 fun main(vararg args: String) {
   if (args.isEmpty())
@@ -12,6 +17,22 @@ fun main(vararg args: String) {
     executionResult.reports.forEach {
       if (it.severity > ScriptDiagnostic.Severity.DEBUG)
         println(" : ${it.message}" + if (it.exception == null) "" else ": ${it.exception}")
+    }
+    executionResult.onSuccess {
+      if (args.size > 1) {
+        when (args[1]) {
+          "build" -> {
+            val scriptClass = it.returnValue.scriptClass!!
+            val projectsField = scriptClass.memberProperties.find { field -> field.name == "projects" }!!
+            val projects = projectsField.call(scriptClass.createInstance()) as Projects
+
+            val projectToBuild = projects.projects[args[2]] ?: throw IllegalArgumentException()
+            println(projectToBuild.buildTool)
+          }
+        }
+      }
+
+      ResultWithDiagnostics.Success(it)
     }
   }
 }
