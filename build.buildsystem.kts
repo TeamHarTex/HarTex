08f71da0e7f32e5022f54bc523252dc10016cb28
HTGAzureX1212 <39023054+HTG-YT@users.noreply.github.com>
2022-10-11 18:43:39 +0800

    remove common workspace from Kotlin build script

--- build.buildsystem.kts
+++ build.buildsystem.kts
@@ -1,14 +1,6 @@
 projects {
-  project("common", ProjectType.RUST)
   project("discord-frontend", ProjectType.RUST)
   project("web-frontend", ProjectType.TYPESCRIPT)
-}
-
-buildConfigurationForProject("common") {
-  buildTool(ProjectBuildTool.CARGO)
-  cargoBuildProfile(CargoBuildProfile.RELEASE)
-  includeDebugInformation(true)
-  linker("rust-lld")
 }
 
 buildConfigurationForProject("discord-frontend") {
