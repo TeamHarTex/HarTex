908d9ae545c5658a0c41b9dc23bf1047cdae39a8
HTGAzureX1212 <39023054+HTG-YT@users.noreply.github.com>
2022-10-12 10:41:14 +0800

    test environment variable setting again...

--- buildsystem/script-host/src/main/kotlin/com/github/teamhartex/hartex/buildsystem/ProjectBuilder.kt
+++ buildsystem/script-host/src/main/kotlin/com/github/teamhartex/hartex/buildsystem/ProjectBuilder.kt
@@ -41,7 +41,6 @@ class ProjectBuilder {
             val projectToBuild = projects.projects[args[2]] ?: throw NoSuchElementException("no such project")
             val processBuilder = ProcessBuilder()
               .redirectOutput(ProcessBuilder.Redirect.PIPE)
-            processBuilder.environment().putAll(System.getenv())
 
             when (projectToBuild.buildTool) {
               ProjectBuildTool.CARGO -> {
