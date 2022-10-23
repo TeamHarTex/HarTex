3e61894d12b5bf8e3327f8f35b5c2f855c1f889d
HTGAzureX1212 <39023054+HTG-YT@users.noreply.github.com>
2022-10-23 15:32:33 +0800

    worker consumption working

--- discord-frontend/hartex-discord-worker/src/main.rs
+++ discord-frontend/hartex-discord-worker/src/main.rs
@@ -22,6 +22,10 @@
 use hartex_discord_core::dotenv;
 use hartex_discord_core::log;
 use hartex_discord_core::tokio;
+use lapin::{Connection, ConnectionProperties};
+use lapin::message::DeliveryResult;
+use lapin::options::{BasicAckOptions, BasicConsumeOptions};
+use lapin::types::FieldTable;
 
 #[tokio::main(flavor = "multi_thread")]
 pub async fn main() -> hartex_discord_eyre::Result<()> {
@@ -30,6 +34,26 @@ pub async fn main() -> hartex_discord_eyre::Result<()> {
 
     log::trace!("loading environment variables");
     dotenv::dotenv()?;
+
+    let username = std::env::var("GATEWAY_RABBITMQ_USERNAME")?;
+    let password = std::env::var("GATEWAY_RABBITMQ_PASSWORD")?;
+    let host = std::env::var("RABBITMQ_HOST")?;
+    let port = std::env::var("RABBITMQ_PORT")?;
+    let uri = format!("amqp://{username}:{password}@{host}:{port}");
+    let uri_log = format!("amqp://{username}:<redacted>@{host}:{port}");
+
+    log::trace!("creating rabbitmq amqp connection (uri: {})", &uri_log);
+    let amqp_connection = Connection::connect(&uri, ConnectionProperties::default()).await?;
+
+    let channel_inbound = amqp_connection.create_channel().await?;
+    channel_inbound.basic_consume("gateway.inbound", "consumer", BasicConsumeOptions::default(), FieldTable::default())
+        .await?
+        .set_delegate(move |result: DeliveryResult| async move {
+            if let Ok(Some(delivery)) = result {
+                delivery.ack(BasicAckOptions::default()).await.expect("failed to ack");
+                log::trace!("{}", String::from_utf8(delivery.data).unwrap());
+            }
+        });
 
     Ok(())
 }
