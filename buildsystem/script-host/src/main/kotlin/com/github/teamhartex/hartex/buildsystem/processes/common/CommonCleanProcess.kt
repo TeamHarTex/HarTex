3db896cbf22c2c01968f236c96bf2c934b3c7ed2
HTGAzureX1212 <39023054+HTG-YT@users.noreply.github.com>
2022-10-28 13:40:56 +0800

    Fix Directory Deletion
    This commit fixes the deletion of the "dist" directory in "web-frontend"
    when the buildsystem "clean" command is executed for the "web-frontend"
    workspace.
    
    Previously, executing "rmdir" from Process does not work because the
    "rmdir" program is not found (on Windows I believe, because they are not
    files). This commit fixes it by utilizing the filesystem API instead,
    using the ".deleteRecursively()" function.

--- buildsystem/script-host/src/main/kotlin/com/github/teamhartex/hartex/buildsystem/processes/common/CommonCleanProcess.kt
+++ buildsystem/script-host/src/main/kotlin/com/github/teamhartex/hartex/buildsystem/processes/common/CommonCleanProcess.kt
@@ -4,21 +4,29 @@ import com.github.teamhartex.hartex.buildsystem.Project
 import com.github.teamhartex.hartex.buildsystem.ProjectBuildTool
 import com.github.teamhartex.hartex.buildsystem.ProjectType
 import java.io.File
+import java.nio.file.Path
 
 class CommonCleanProcess {
   companion object {
-    fun new(projectToBuild: Project, args: List<String>): Process {
+    fun new(projectToBuild: Project, args: List<String>): Process? {
       val processBuilder = ProcessBuilder()
         .redirectOutput(ProcessBuilder.Redirect.PIPE)
 
       when (projectToBuild.projectType to projectToBuild.buildTool) {
         ProjectType.RUST to ProjectBuildTool.CARGO -> processBuilder.command("cargo", "clean")
         ProjectType.TYPESCRIPT to ProjectBuildTool.YARN -> {
-          processBuilder.command("rmdir", projectToBuild.outputDirectory)
+          try {
+            if (!File(Path.of(System.getProperty("user.dir"), args[2], "dist").toUri()).deleteRecursively())
+              throw RuntimeException("failed to delete dist directory")
+
+            return null
+          } catch (exception: Throwable) {
+            throw exception
+          }
         }
       }
 
-      val process = processBuilder.directory(File(System.getProperty("user.dir") + """/${args[2]}"""))
+      val process = processBuilder.directory(File(System.getProperty("user.dir") + "/${args[2]}"))
         .start()
       val outputReader = process.errorStream.bufferedReader()
       var line = outputReader.readLine()
