9e8b87d9ac11ba27818028c2120cb8b129ee8086
HTGAzureX1212 <39023054+HTG-YT@users.noreply.github.com>
2022-11-23 11:13:57 +0800

    workaround expansion test

--- discord-frontend/hartex-discord-commands-test/tests/expand/derive.expanded.rs
+++ discord-frontend/hartex-discord-commands-test/tests/expand/derive.expanded.rs
@@ -1,16 +1,6 @@
 use hartex_discord_commands_macros::CommandMetadata;
+#[metadata(command_type = 1)]
+#[metadata(description = "derive macro expand test")]
+#[metadata(interaction_only = true)]
+#[metadata(name = "derive")]
 pub struct Derive;
-impl hartex_discord_commands_core::CommandMetadata for Derive {
-    fn command_type(&self) -> u8 {
-        1
-    }
-    fn description(&self) -> String {
-        String::from("derive macro expand test")
-    }
-    fn interaction_only(&self) -> bool {
-        true
-    }
-    fn name(&self) -> String {
-        String::from("derive")
-    }
-}
