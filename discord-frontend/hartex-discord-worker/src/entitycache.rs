d460f14a2207a1b024a1cb05fe0de028956a9209
HTGAzureX1212 <39023054+HTG-YT@users.noreply.github.com>
2022-12-03 08:28:29 +0800

    fix clippy stuf

--- discord-frontend/hartex-discord-worker/src/entitycache.rs
+++ discord-frontend/hartex-discord-worker/src/entitycache.rs
@@ -20,6 +20,8 @@
  * with HarTex. If not, see <https://www.gnu.org/licenses/>.
  */
 
+#![allow(unused_variables)]
+
 use hartex_discord_core::discord::model::gateway::event::GatewayEvent;
 
 pub async fn update_entitycache(event: GatewayEvent) -> hartex_discord_eyre::Result<()> {
