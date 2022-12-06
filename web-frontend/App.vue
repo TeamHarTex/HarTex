63384cc99367cbd81d73397ecb1a369f9c732177
Lunari8546 <63549306+Lunari8546@users.noreply.github.com>
2022-12-06 22:23:40 +0800

    Update configuration.

--- web-frontend/App.vue
+++ web-frontend/App.vue
@@ -1,5 +1,13 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <p>Hello World!</p>
   </div>
 </template>
+
+<script setup></script>
+
+<style lang="postcss">
+p {
+  @apply text-red-100;
+}
+</style>
