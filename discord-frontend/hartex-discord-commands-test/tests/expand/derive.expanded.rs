34d86388b479db4e3c801777f29bf3a5d177252e
HTGAzureX1212 <39023054+HTG-YT@users.noreply.github.com>
2022-12-10 11:14:11 +0800

    fix procedural macro test (finally)

--- discord-frontend/hartex-discord-commands-test/tests/expand/derive.expanded.rs
+++ discord-frontend/hartex-discord-commands-test/tests/expand/derive.expanded.rs
@@ -1,4 +1,4 @@
-use hartex_discord_commands_macros::CommandMetadata;
+use hartex_discord_commands_core::CommandMetadata;
 #[metadata(command_type = 1)]
 #[metadata(description = "derive macro expand test")]
 #[metadata(interaction_only = true)]
