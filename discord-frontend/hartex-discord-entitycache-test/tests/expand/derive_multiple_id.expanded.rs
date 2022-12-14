5205b4bd1851a67b39d2ac2124ee9d8705ae2739
HTGAzureX1212 <39023054+HTG-YT@users.noreply.github.com>
2022-12-14 10:34:30 +0800

    add multiple-field id Entity derive test

---
+++ discord-frontend/hartex-discord-entitycache-test/tests/expand/derive_multiple_id.expanded.rs
@@ -0,0 +1,17 @@
+use hartex_discord_entitycache_core::Entity;
+pub struct DeriveMultipleId {
+    #[entity(id)]
+    pub id1: u64,
+    #[entity(id)]
+    pub id2: u64,
+}
+const _: () = {
+    extern crate hartex_discord_entitycache_core as _entitycache_core;
+    #[automatically_derived]
+    impl _entitycache_core::traits::Entity for DeriveMultipleId {
+        type Id = (u64, u64);
+        fn id(&self) -> <Self as _entitycache_core::traits::Entity>::Id {
+            (self.id1, self.id2)
+        }
+    }
+};
