6a6c050cfa28ad2568b4d63bf581ede9227632d4
HTGAzureX1212 <39023054+HTG-YT@users.noreply.github.com>
2022-12-21 20:29:35 +0800

    add Int8 Kafka protocol primitive

--- hartex-kafka/src/protocol/primitives/types.rs
+++ hartex-kafka/src/protocol/primitives/types.rs
@@ -46,3 +46,24 @@ impl<W: Write> PrimitiveWrite<W> for Boolean {
         Ok(writer.write_all(&[self.0.into()])?)
     }
 }
+
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+pub struct Int8(pub i8);
+
+impl<R: Read> PrimitiveRead<R> for Int8 {
+    fn read(reader: &mut R) -> Result<Self, super::errors::PrimitiveReadError> {
+        let mut buffer = [0u8; 1];
+        reader.read_exact(&mut buffer)?;
+
+        Ok(Self(i8::from_be_bytes(buffer)))
+    }
+}
+
+impl<W: Write> PrimitiveWrite<W> for Int8 {
+    fn write(&self, writer: &mut W) -> Result<(), super::errors::PrimitiveWriteError> {
+        let buf = self.0.to_be_bytes();
+        writer.write_all(&buf)?;
+        
+        Ok(())
+    }
+}
