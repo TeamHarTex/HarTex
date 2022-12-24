401810ca7a177f51e6f4f5a34533519b42545a22
HTGAzureX1212 <39023054+HTG-YT@users.noreply.github.com>
2022-12-24 23:18:49 +0800

    update RecordBatch reading

--- hartex-kafka/src/protocol/primitives/types/record.rs
+++ hartex-kafka/src/protocol/primitives/types/record.rs
@@ -24,12 +24,58 @@ use std::io::Read;
 
 use super::super::errors::PrimitiveReadError;
 use super::super::traits::PrimitiveRead;
+use super::Int32;
+use super::Int64;
+use super::Int8;
+use crate::blockvec::BlockVec;
 
 #[derive(Clone, Debug, Eq, PartialEq)]
-pub struct RecordBatch;
+pub struct RecordBatch {
+    pub base_offset: Int64,
+    pub batch_length: Int32,
+    pub partition_leader_epoch: Int32,
+}
 
 impl<R: Read> PrimitiveRead<R> for RecordBatch {
     fn read(reader: &mut R) -> Result<Self, PrimitiveReadError> {
-        todo!()
+        let base_offset = Int64::read(reader)?;
+
+        let batch_length = Int32::read(reader)?;
+        let actual_length = usize::try_from(batch_length.0)
+            .map_err(PrimitiveReadError::IntOverflow)?
+            .checked_sub(9)
+            .ok_or_else(|| {
+                PrimitiveReadError::Generic(
+                    format!("record batch length is too small: {}", batch_length.0).into(),
+                )
+            })?;
+
+        let partition_leader_epoch = Int32::read(reader)?;
+
+        let magic = Int8::read(reader)?.0;
+        if magic != 2 {
+            return Err(PrimitiveReadError::Generic(
+                format!("invalid magic for record batch: {magic}").into(),
+            ));
+        }
+
+        let crc = u32::from_be_bytes(Int32::read(reader)?.0.to_be_bytes());
+        let mut data = BlockVec::new(actual_length);
+        data = data.read_exact(reader)?;
+        let data_vec: Vec<u8> = data.into();
+        let actual_crc = crc32c::crc32c(&data_vec);
+
+        if crc != actual_crc {
+            return Err(PrimitiveReadError::Generic(
+                format!("invalid crc32c: expected 0x{crc:x}, found 0x{actual_crc:x} instead")
+                    .into(),
+            ));
+        }
+
+        Ok(Self {
+            base_offset,
+            batch_length,
+            partition_leader_epoch,
+        })
     }
 }
