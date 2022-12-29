cf29cc0e1efa4bbdc43a2049bc1e98899a689bf7
HTGAzureX1212 <39023054+HTG-YT@users.noreply.github.com>
2022-12-29 22:43:10 +0800

    update license header

--- discord-frontend/hartex-discord-entitycache-test/tests/ui/expected_token_group.rs
+++ discord-frontend/hartex-discord-entitycache-test/tests/ui/expected_token_group.rs
@@ -4,7 +4,7 @@
  * This file is part of HarTex.
  *
  * HarTex
- * Copyright (c) 2021-2022 HarTex Project Developers
+ * Copyright (c) 2021-2023 HarTex Project Developers
  *
  * HarTex is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
