b4dfc5f698c191b6c3f6c241a281df184cd50be4
HTGAzureX1212 <39023054+HTG-YT@users.noreply.github.com>
2023-01-06 15:18:30 +0800

    add more stuff

--- discord-frontend/hartex-discord-leader/src/main.rs
+++ discord-frontend/hartex-discord-leader/src/main.rs
@@ -25,9 +25,13 @@
 #![allow(dead_code)]
 
 use std::env;
+use std::time::Duration;
 
 use hartex_discord_core::dotenvy;
 use hartex_discord_core::log;
+use hartex_discord_core::tokio::signal;
+use hartex_discord_core::tokio::time;
+use hartex_discord_core::tokio::sync::watch;
 use hartex_discord_core::tokio;
 use hartex_kafka_utils::serde::ByteArraySerializer;
 use hartex_kafka_utils::traits::ClientConfigUtils;
@@ -57,86 +61,17 @@ pub async fn main() -> hartex_discord_eyre::Result<()> {
         .value_serializer(ByteArraySerializer)
         .create::<FutureProducer>();
 
-    /*let username = std::env::var("GATEWAY_RABBITMQ_USERNAME")?;
-    let password = std::env::var("GATEWAY_RABBITMQ_PASSWORD")?;
-    let host = std::env::var("RABBITMQ_HOST")?;
-    let port = std::env::var("RABBITMQ_PORT")?;
-    let uri = format!("amqp://{username}:{password}@{host}:{port}");
-    let uri_log = format!("amqp://{username}:<redacted>@{host}:{port}");
-
-    log::trace!("creating rabbitmq amqp connection (uri: {})", &uri_log);
-    let amqp_connection = Connection::connect(&uri, ConnectionProperties::default()).await?;
-
-    let channel_inbound = amqp_connection.create_channel().await?;
-    let channel_outbound = amqp_connection.create_channel().await?;
-
-    log::trace!("declaring amqp exchange");
-    channel_inbound
-        .exchange_declare(
-            "gateway",
-            ExchangeKind::Topic,
-            ExchangeDeclareOptions {
-                passive: false,
-                durable: true,
-                auto_delete: false,
-                internal: false,
-                nowait: false,
-            },
-            FieldTable::default(),
-        )
-        .await?;
-
-    log::trace!("declaring amqp outbound queue");
-    channel_outbound
-        .queue_declare(
-            "gateway.outbound",
-            QueueDeclareOptions {
-                passive: false,
-                durable: true,
-                exclusive: false,
-                auto_delete: false,
-                nowait: false,
-            },
-            FieldTable::default(),
-        )
-        .await?;
-
-    log::trace!("declaring and binding amqp inbound queue");
-    channel_inbound
-        .queue_declare(
-            "gateway.inbound",
-            QueueDeclareOptions {
-                passive: false,
-                durable: true,
-                exclusive: false,
-                auto_delete: false,
-                nowait: false,
-            },
-            FieldTable::default(),
-        )
-        .await?;
-    channel_inbound
-        .queue_bind(
-            "gateway.inbound",
-            "gateway",
-            "#",
-            QueueBindOptions::default(),
-            FieldTable::default(),
-        )
-        .await?;
-
     log::trace!("building clusters");
-    let num_shards = std::env::var("NUM_SHARDS")?.parse::<u64>()?;
+    let num_shards = env::var("NUM_SHARDS")?.parse::<u64>()?;
     let queue = queue::obtain()?;
     let mut shards = shards::obtain(num_shards, &queue)?;
 
     let (tx, rx) = watch::channel(false);
 
     log::trace!("launching {num_shards} shard(s)",);
-    let mut rx = rx.clone();
-    let amqp = channel_inbound.clone();
+    // let mut rx = rx.clone();
 
-    tokio::spawn(async move {
+    /*tokio::spawn(async move {
         tokio::select! {
             _ = inbound::handle(shards.iter_mut(), amqp) => {},
             _ = rx.changed() => {
@@ -145,16 +80,14 @@ pub async fn main() -> hartex_discord_eyre::Result<()> {
                 })).await;
             }
         }
-    });
+    });*/
 
     signal::ctrl_c().await?;
 
     log::warn!("ctrl-c signal received, shutting down");
-    channel_inbound.close(1, "user-initiated shutdown").await?;
-    channel_outbound.close(1, "user-initiated shutdown").await?;
 
     tx.send(true)?;
-    time::sleep(Duration::from_secs(5)).await;*/
+    time::sleep(Duration::from_secs(5)).await;
 
     Ok(())
 }
