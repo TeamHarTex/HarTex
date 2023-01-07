e7d611723e01190d967509142dc207687418cc2e
HTGAzureX1212 <39023054+HTG-YT@users.noreply.github.com>
2023-01-07 13:00:05 +0800

    add constants module

---
+++ discord-frontend/hartex-discord-eyre/src/constants.rs
@@ -0,0 +1,42 @@
+/*
+ * SPDX-License-Identifier: AGPL-3.0-only
+ *
+ * This file is part of HarTex.
+ *
+ * HarTex
+ * Copyright (c) 2021-2023 HarTex Project Developers
+ *
+ * HarTex is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * HarTex is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with HarTex. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+pub const IGNORED_SYMBOLS: [&'static str; 7] = [
+    "RtlUserThreadStart",
+    "BaseThreadInitThunk",
+    "__scrt_common_main_seh",
+    "invoke_main",
+    "main",
+    "__libc_start_main",
+    "_start",
+];
+
+pub const IGNORED_CRATES: [&'static str; 6] = [
+    "std",
+    "core",
+    "eyre",
+    "backtrace",
+    "hartex_discord_eyre",
+    // workaround for lines like:
+    // <core::result::Result<T,F> as ...
+    "<core",
+];
