d01d25952e5e45747737f555c94fa77a68ea64a3
HTGAzureX1212 <39023054+HTG-YT@users.noreply.github.com>
2023-01-12 18:01:08 +0800

    cargo fmt

--- discord-frontend/hartex-discord-entitycache-entities/src/guild.rs
+++ discord-frontend/hartex-discord-entitycache-entities/src/guild.rs
@@ -20,8 +20,8 @@
  * with HarTex. If not, see <https://www.gnu.org/licenses/>.
  */
 
-use hartex_discord_core::discord::model::id::Id;
 use hartex_discord_core::discord::model::id::marker::GuildMarker;
+use hartex_discord_core::discord::model::id::Id;
 use hartex_discord_entitycache_core::Entity;
 
 #[derive(Entity)]
