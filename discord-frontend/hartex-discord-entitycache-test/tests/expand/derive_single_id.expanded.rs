161472cd4e16ebd81ee3ad15dfabee5fe9a28b1a
HTGAzureX1212 <39023054+HTG-YT@users.noreply.github.com>
2023-01-14 17:59:17 +0800

    remove wrapping dummy const

--- discord-frontend/hartex-discord-entitycache-test/tests/expand/derive_single_id.expanded.rs
+++ discord-frontend/hartex-discord-entitycache-test/tests/expand/derive_single_id.expanded.rs
@@ -3,13 +3,11 @@ pub struct DeriveSingleId {
     #[entity(id)]
     pub id: u64,
 }
-const _: () = {
-    extern crate hartex_discord_entitycache_core as _entitycache_core;
-    #[automatically_derived]
-    impl _entitycache_core::traits::Entity for DeriveSingleId {
-        type Id = u64;
-        fn id(&self) -> <Self as _entitycache_core::traits::Entity>::Id {
-            self.id
-        }
+extern crate hartex_discord_entitycache_core as _entitycache_core;
+#[automatically_derived]
+impl _entitycache_core::traits::Entity for DeriveSingleId {
+    type Id = u64;
+    fn id(&self) -> <Self as _entitycache_core::traits::Entity>::Id {
+        self.id
     }
-};
+}
