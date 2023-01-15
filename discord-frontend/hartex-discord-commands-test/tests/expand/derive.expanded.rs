81f2b1843f5f643987bbe727796099bf29dd65d1
HTGAzureX1212 <39023054+HTG-YT@users.noreply.github.com>
2023-01-15 15:42:59 +0800

    add description and description_localization properties

--- discord-frontend/hartex-discord-commands-test/tests/expand/derive.expanded.rs
+++ discord-frontend/hartex-discord-commands-test/tests/expand/derive.expanded.rs
@@ -1,6 +1,5 @@
 use hartex_discord_commands_core::CommandMetadata;
 #[metadata(command_type = 1)]
-#[metadata(description = "derive macro expand test")]
 #[metadata(interaction_only = true)]
 #[metadata(name = "derive")]
 pub struct Derive;
@@ -9,9 +8,6 @@ extern crate hartex_discord_commands_core as _commands_core;
 impl _commands_core::traits::CommandMetadata for Derive {
     fn command_type(&self) -> u8 {
         1
-    }
-    fn description(&self) -> String {
-        String::from("derive macro expand test")
     }
     fn interaction_only(&self) -> bool {
         true
