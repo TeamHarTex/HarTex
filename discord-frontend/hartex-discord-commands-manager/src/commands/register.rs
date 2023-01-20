40bd7688308ab3b54302afe2e337de6c963c1a10
HTGAzureX1212 <39023054+HTG-YT@users.noreply.github.com>
2023-01-20 23:34:40 +0800

    try to read command file - need debugging

--- discord-frontend/hartex-discord-commands-manager/src/commands/register.rs
+++ discord-frontend/hartex-discord-commands-manager/src/commands/register.rs
@@ -40,6 +40,7 @@ pub async fn register_command(matches: ArgMatches) -> hartex_discord_eyre::Resul
         "an error will occur if this command is not ran within the discord-frontend directory"
     );
 
+    log::debug!("{}", matches.contains_id("command"));
     let mut command = matches.get_one::<String>("command").unwrap().clone();
 
     if !command.to_ascii_lowercase().ends_with(".json") {
