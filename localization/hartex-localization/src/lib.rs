e11953d1c3093738432fce8bda6a862a34d8ca37
HTGAzureX1212 <39023054+HTG-YT@users.noreply.github.com>
2023-01-24 23:19:44 +0800

    add resource to bundle

--- localization/hartex-localization/src/lib.rs
+++ localization/hartex-localization/src/lib.rs
@@ -26,7 +26,10 @@
 #![deny(warnings)]
 #![feature(let_chains)]
 
+use std::ffi::OsStr;
+use std::fs;
 use std::path::PathBuf;
+use fluent_bundle::FluentResource;
 
 use unic_langid::langid;
 use unic_langid::LanguageIdentifier;
@@ -36,16 +39,12 @@ pub mod types;
 
 pub fn create_bundle(requested: Option<LanguageIdentifier>, modules: Vec<&str>) -> hartex_eyre::Result<types::LocalizationBundle> {
     let fallback = langid!("en-US");
-    let is_fallback = requested.as_ref() == Some(&fallback);
-
     let locale = requested.clone().unwrap_or(fallback);
-    let _ = types::LocalizationBundle::new(vec![locale]);
+    let mut bundle = types::LocalizationBundle::new(vec![locale]);
 
     let mut localizations_root = PathBuf::from("../localization/locales");
-    if !is_fallback && let Some(ident) = requested {
-        localizations_root.push(ident.to_string());
-        modules.iter().for_each(|module| localizations_root.push(module));
-    }
+    localizations_root.push(locale.to_string());
+    modules.iter().for_each(|module| localizations_root.push(module));
 
     if !localizations_root.try_exists()? {
         return Err(Report::msg(format!("localization root not found: {}", localizations_root.to_string_lossy())));
@@ -55,5 +54,17 @@ pub fn create_bundle(requested: Option<LanguageIdentifier>, modules: Vec<&str>) 
         return Err(Report::msg(format!("localization root is not a directory: {}", localizations_root.to_string_lossy())));
     }
 
-    todo!()
+    for result in localizations_root.read_dir()? {
+        let entry = result?;
+        let path = entry.path();
+        if path.extension().and_then(OsStr::to_str) != Some("ftl") {
+            continue;
+        }
+
+        let resource_string = fs::read_to_string(path)?;
+        let resource = FluentResource::try_new(resource_string).map_err(|(_, errors)| errors.last().unwrap().clone())?;
+        bundle.add_resource(resource).map_err(|errors| errors.last().unwrap().clone())?;
+    }
+
+    Ok(bundle)
 }
