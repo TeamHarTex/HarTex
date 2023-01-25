92ec3b0c263d75584bd1353742a3db2201040938
HTGAzureX1212 <39023054+HTG-YT@users.noreply.github.com>
2023-01-25 13:30:19 +0800

    impl edit command

--- discord-frontend/hartex-discord-commands-manager/src/commands/edit.rs
+++ discord-frontend/hartex-discord-commands-manager/src/commands/edit.rs
@@ -20,9 +20,110 @@
  * with HarTex. If not, see <https://www.gnu.org/licenses/>.
  */
 
+use std::env;
+use std::fs::File;
+use std::io::Read;
+use std::str;
+
 use clap::ArgMatches;
+use hartex_discord_core::dotenvy;
+use hartex_discord_core::log;
+use hartex_eyre::eyre::Report;
+use hyper::body::HttpBody;
+use hyper::header::ACCEPT;
+use hyper::header::AUTHORIZATION;
+use hyper::header::CONTENT_TYPE;
+use hyper::header::USER_AGENT;
+use hyper::Client;
+use hyper::Method;
+use hyper::Request;
+use hyper_trust_dns::TrustDnsResolver;
+use walkdir::WalkDir;
 
 #[allow(clippy::module_name_repetitions)]
-pub async fn edit_command(_: ArgMatches) -> hartex_eyre::Result<()> {
+pub async fn edit_command(matches: ArgMatches) -> hartex_eyre::Result<()> {
+    log::trace!("loading environment variables");
+    dotenvy::dotenv()?;
+
+    log::trace!("searching for the command specification");
+    log::warn!(
+        "an error will occur if this command is not ran within the discord-frontend directory"
+    );
+
+    let mut command = matches.get_one::<String>("command").unwrap().clone();
+    let command_id = matches.get_one::<String>("command-id").unwrap().clone();
+
+    if !command.to_ascii_lowercase().ends_with(".json") {
+        command.push_str(".json");
+    }
+
+    let mut iterator = WalkDir::new("hartex-discord-commands-spec")
+        .same_file_system(true)
+        .into_iter();
+    let entry_option = loop {
+        let option = iterator.next();
+        if option.is_none() {
+            break None;
+        }
+
+        let entry = option.unwrap()?;
+        if entry.metadata()?.is_dir() {
+            continue;
+        }
+
+        if entry.path().ends_with(&command) {
+            break Some(entry);
+        }
+    };
+
+    if entry_option.is_none() {
+        return Err(Report::msg(format!(
+            "command file {command} cannot be found"
+        )));
+    }
+
+    let mut file = File::open(entry_option.unwrap().path())?;
+    let mut json = String::new();
+    file.read_to_string(&mut json)?;
+
+    let client =
+        Client::builder().build(TrustDnsResolver::default().into_native_tls_https_connector());
+
+    let application_id = env::var("APPLICATION_ID")?;
+
+    let mut token = env::var("BOT_TOKEN")?;
+    if !token.starts_with("Bot ") {
+        token.insert_str(0, "Bot ");
+    }
+
+    let request = Request::builder()
+        .uri(format!(
+            "https://discord.com/api/v10/applications/{application_id}/commands/{command_id}"
+        ))
+        .method(Method::PATCH)
+        .header(ACCEPT, "application/json")
+        .header(AUTHORIZATION, token)
+        .header(CONTENT_TYPE, "application/json")
+        .header(
+            USER_AGENT,
+            "DiscordBot (https://github.com/TeamHarTex/HarTex, v0.1.0) CommandsManager",
+        )
+        .body(json)?;
+    let mut response = client.request(request).await?;
+    if !response.status().is_success() {
+        let mut full = String::new();
+        while let Some(result) = response.body_mut().data().await {
+            full.push_str(str::from_utf8(&result?)?);
+        }
+        log::error!("unsuccessful HTTP request, response: {full}");
+
+        return Err(Report::msg(format!(
+            "unsuccessful HTTP request, with status code {}",
+            response.status()
+        )));
+    }
+
+    log::info!("request succeeded: {}", response.status());
+
     Ok(())
 }
