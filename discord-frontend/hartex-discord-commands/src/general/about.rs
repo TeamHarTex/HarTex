7f24f7cc6958aa2ed5cb6dc7fc3eb36313bfcc3f
HTGAzureX1212 <39023054+HTG-YT@users.noreply.github.com>
2023-02-03 21:25:02 +0800

    create embed

--- discord-frontend/hartex-discord-commands/src/general/about.rs
+++ discord-frontend/hartex-discord-commands/src/general/about.rs
@@ -23,6 +23,11 @@
 use hartex_discord_commands_core::traits::Command;
 use hartex_discord_commands_core::CommandMetadata;
 use hartex_discord_core::discord::model::application::interaction::Interaction;
+use hartex_discord_core::discord::model::http::interaction::InteractionResponse;
+use hartex_discord_core::discord::model::http::interaction::InteractionResponseType;
+use hartex_discord_core::discord::util::builder::embed::EmbedBuilder;
+use hartex_discord_core::discord::util::builder::embed::ImageSource;
+use hartex_discord_core::discord::util::builder::InteractionResponseDataBuilder;
 use hartex_discord_core::log;
 use hartex_discord_utils::CLIENT;
 use hartex_eyre::eyre::Report;
@@ -38,7 +43,7 @@ pub struct About;
 impl Command for About {
     #[allow(clippy::unused_async)]
     async fn execute(&self, interaction: Interaction) -> hartex_eyre::Result<()> {
-        let _ = CLIENT.interaction(interaction.application_id);
+        let interaction_client = CLIENT.interaction(interaction.application_id);
         let bundle = create_bundle(
             interaction.locale.and_then(|locale| locale.parse().ok()),
             &["discord-frontend", "commands"],
@@ -46,11 +51,29 @@ impl Command for About {
 
         bundle_get!(bundle."about-embed-title": message, out [about_embed_title, errors]);
         let _ = about_embed_title;
+        let embed = EmbedBuilder::new()
+            .image(ImageSource::attachment("https://cdn.discordapp.com/avatars/936431574310879332/9a46b39c031ca84e8351ee97867afc96.png?size=256")?)
+            .build();
 
         log::warn!("fluent errors occurred:");
         for error in errors {
             println!("{:?}", Report::new(error));
         }
+
+        interaction_client
+            .create_response(
+                interaction.id,
+                &interaction.token,
+                &InteractionResponse {
+                    kind: InteractionResponseType::ChannelMessageWithSource,
+                    data: Some(
+                        InteractionResponseDataBuilder::new()
+                            .embeds(vec![embed])
+                            .build(),
+                    ),
+                },
+            )
+            .await?;
 
         Ok(())
     }
