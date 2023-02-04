1a592764a51f181e7d33703542857a53f04e07aa
HTGAzureX1212 <39023054+HTG-YT@users.noreply.github.com>
2023-02-04 08:15:18 +0800

    add embed colour

--- discord-frontend/hartex-discord-commands/src/general/about.rs
+++ discord-frontend/hartex-discord-commands/src/general/about.rs
@@ -58,6 +58,7 @@ impl Command for About {
                     .icon_url(ImageSource::url("https://cdn.discordapp.com/avatars/936431574310879332/9a46b39c031ca84e8351ee97867afc96.png")?)
                     .build()
             )
+            .color(0x41_A0_DE)
             .build();
 
         log::warn!("fluent errors occurred:");
