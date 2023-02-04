148d923ab23be231812dd553abc608d294a00942
HTGAzureX1212 <39023054+HTG-YT@users.noreply.github.com>
2023-02-04 08:12:30 +0800

    use url instead of attachment

--- discord-frontend/hartex-discord-commands/src/general/about.rs
+++ discord-frontend/hartex-discord-commands/src/general/about.rs
@@ -55,7 +55,7 @@ impl Command for About {
         let embed = EmbedBuilder::new()
             .author(
                 EmbedAuthorBuilder::new("About HarTex")
-                    .icon_url(ImageSource::attachment("https://cdn.discordapp.com/avatars/936431574310879332/9a46b39c031ca84e8351ee97867afc96.png")?)
+                    .icon_url(ImageSource::url("https://cdn.discordapp.com/avatars/936431574310879332/9a46b39c031ca84e8351ee97867afc96.png")?)
                     .build()
             )
             .build();
