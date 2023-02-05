c1ea821c05ad0fba310dddd249750973390a79a6
HTGAzureX1212 <39023054+HTG-YT@users.noreply.github.com>
2023-02-05 17:34:28 +0800

    forcibly use the command metadata

--- discord-frontend/hartex-discord-worker/src/interaction.rs
+++ discord-frontend/hartex-discord-worker/src/interaction.rs
@@ -23,6 +23,7 @@
 use hartex_discord_commands::general::about::About;
 use hartex_discord_commands::general::latency::Latency;
 use hartex_discord_commands_core::traits::Command;
+use hartex_discord_commands_core::traits::CommandMetadata;
 use hartex_discord_core::discord::model::application::interaction::InteractionData;
 use hartex_discord_core::discord::model::gateway::payload::incoming::InteractionCreate;
 
@@ -33,9 +34,9 @@ pub async fn application_command(
         unreachable!("this should not be possible")
     };
 
-    match command.name.as_str() {
-        "about" => About.execute(interaction_create.0).await,
-        "latency" => Latency.execute(interaction_create.0).await,
+    match command.name {
+        name if name == About.name() => About.execute(interaction_create.0).await,
+        name if name == Latency.name() => Latency.execute(interaction_create.0).await,
         _ => Ok(()),
     }
 }
