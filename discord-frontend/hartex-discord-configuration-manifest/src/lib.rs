66ff5501590d3fc31ce6e225c6b304a224833769
HTGAzureX1212 <39023054+HTG-YT@users.noreply.github.com>
2023-05-06 17:32:12 +0800

    add appearance configuration model

--- discord-frontend/hartex-discord-configuration-manifest/src/lib.rs
+++ discord-frontend/hartex-discord-configuration-manifest/src/lib.rs
@@ -25,8 +25,14 @@
 //! Specifies the configuration manifest in models that can be serialized from and deserialized
 //! into.
 
-use serde::{Deserialize, Serialize};
+use hartex_eyre::eyre::Report;
+use hcl::eval::Context;
+use hcl::eval::FuncDef;
+use hcl::eval::ParamType;
+use serde::Deserialize;
+use serde::Serialize;
 
+pub mod appearance;
 pub mod dashboard;
 
 /// The root of everything.
@@ -36,4 +42,18 @@ pub struct Configuration {
     pub version: u8,
     /// Dashboard access configurations.
     pub dashboard: dashboard::Dashboard,
+    /// Appearance of HarTex in the server.
+    pub appearance: appearance::Appearance,
+}
+
+pub fn deserialize_config(source_hcl: &str) -> hartex_eyre::Result<Configuration> {
+    let mut context = Context::new();
+    context.declare_func(
+        "rgb",
+        FuncDef::builder()
+            .params([ParamType::Number, ParamType::Number, ParamType::Number])
+            .build(appearance::hcl_rgb_function),
+    );
+
+    hcl::eval::from_str::<Configuration>(source_hcl, &context).map_err(Report::new)
 }
