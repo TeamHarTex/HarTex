430366b36d174810b4c118e6a08eca0480d06c8d
HTGAzureX1212 <39023054+HTG-YT@users.noreply.github.com>
2023-05-30 19:28:14 +0800

    implement get checks for pr

--- bors/hartex-bors-github/src/lib.rs
+++ bors/hartex-bors-github/src/lib.rs
@@ -29,6 +29,11 @@
 #![feature(if_let_guard)]
 #![feature(let_chains)]
 
+use std::collections::HashMap;
+use std::str;
+
+use hartex_bors_core::models::Check;
+use hartex_bors_core::models::CheckStatus;
 use hartex_bors_core::models::GithubRepositoryName;
 use hartex_bors_core::models::GithubRepositoryState;
 use hartex_bors_core::BorsState;
@@ -37,6 +42,7 @@ use hartex_bors_core::RepositoryClient;
 use hartex_bors_database::client::SeaORMDatabaseClient;
 use hartex_eyre::eyre::Report;
 use hartex_log::log;
+use http_body::Body;
 use jsonwebtoken::EncodingKey;
 use octocrab::models::issues::Comment;
 use octocrab::models::pulls::PullRequest;
@@ -46,7 +52,7 @@ use octocrab::models::{App, CommentId};
 use octocrab::Octocrab;
 use secrecy::ExposeSecret;
 use secrecy::SecretVec;
-use std::collections::HashMap;
+use serde::Deserialize;
 
 pub mod messages;
 mod operations;
@@ -146,6 +152,70 @@ impl RepositoryClient for GithubRepositoryClient {
             .update_comment(comment_id, text)
             .await
             .map_err(Report::new)
+    }
+
+    async fn get_checks_for_commit(
+        &mut self,
+        branch: &str,
+        commit_hash: &str,
+    ) -> hartex_eyre::Result<Vec<Check>> {
+        let mut response = self.client
+            ._get(
+                format!(
+                    "https://api.github.com/repos/{}/{}/commits/{}/check-suites",
+                    self.repository_name.owner(),
+                    self.repository_name.repository(),
+                    commit_hash
+                )
+            )
+            .await?;
+
+        #[derive(Deserialize)]
+        struct CheckSuitePayload<'a> {
+            conclusion: Option<&'a str>,
+            head_branch: &'a str
+        }
+
+        #[derive(Deserialize)]
+        struct CheckSuiteResponse<'a> {
+            #[serde(borrow)]
+            check_suites: Vec<CheckSuitePayload<'a>>,
+        }
+
+        let mut full = String::new();
+        while let Some(result) = response.body_mut().data().await {
+            full.push_str(str::from_utf8(&result?)?);
+        }
+
+        let response = serde_json::from_str::<CheckSuiteResponse<'_>>(&full)?;
+        let suites = response
+            .check_suites
+            .into_iter()
+            .filter(|suite| suite.head_branch == branch)
+            .map(|suite| Check {
+                status: match suite.conclusion {
+                    Some(status) => match status {
+                        "success" => CheckStatus::Success,
+                        "failure" | "neutral" | "cancelled" | "skipped" | "timed_out"
+                        | "action_required" | "startup_failure" | "stale" => {
+                            CheckStatus::Failure
+                        }
+                        _ => {
+                            log::warn!(
+                                "Received unknown check suite status for {}/{}: {status}",
+                                self.repository_name,
+                                commit_hash,
+                            );
+
+                            CheckStatus::Pending
+                        }
+                    },
+                    None => CheckStatus::Pending,
+                },
+            })
+            .collect();
+
+        Ok(suites)
     }
 
     async fn get_pull_request(&mut self, pr: u64) -> hartex_eyre::Result<PullRequest> {
