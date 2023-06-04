b995d8abda13dd17e0216272487b95821363cd56
HTGAzureX1212 <39023054+HTG-YT@users.noreply.github.com>
2023-06-04 15:19:39 +0800

    bors: website: get rid of backslashes

--- bors/hartex-bors-website/src/queue.rs
+++ bors/hartex-bors-website/src/queue.rs
@@ -34,12 +34,14 @@ struct QueueData {
 /// The endpoint returning the queue page.
 #[get("/queue/<repository..>")]
 pub async fn queue(repository: PathBuf) -> RawHtml<String> {
+    let mut repository_string = repository.to_string_lossy().to_string();
+
     RawHtml(
         crate::HANDLEBARS
             .render(
                 "queue",
                 &QueueData {
-                    repository: repository.to_string_lossy().to_string(),
+                    repository: repository_string.replace("\\", "/"),
                 },
             )
             .unwrap(),
