9d8f092b8af2591426ee425f060cad15a637b00c
HTGAzureX1212 <39023054+HTG-YT@users.noreply.github.com>
2023-06-07 21:07:50 +0800

    bors: database: set assignee field

--- bors/hartex-bors-database/src/client.rs
+++ bors/hartex-bors-database/src/client.rs
@@ -36,8 +36,8 @@ use hartex_bors_core::models::BorsWorkflowType;
 use hartex_bors_core::models::GithubRepositoryName;
 use hartex_bors_core::DatabaseClient;
 use hartex_eyre::eyre::Report;
-use octocrab::models::RunId;
 use octocrab::models::pulls::PullRequest;
+use octocrab::models::RunId;
 use sea_orm::prelude::DateTime;
 use sea_orm::prelude::DateTimeUtc;
 use sea_orm::sea_query::OnConflict;
@@ -192,18 +192,20 @@ impl DatabaseClient for SeaORMDatabaseClient {
         pr_number: u64,
     ) -> Pin<Box<dyn Future<Output = hartex_eyre::Result<BorsPullRequest>> + '_>> {
         Box::pin(async move {
-            //&github_pr.title.clone().unwrap(),
-            //&github_pr
-            //.head
-            //.label
-            //.clone()
-            //.unwrap_or("<unknown>".to_string()),
-
             let pr = entity::pull_request::ActiveModel {
                 repository: Set(format!("{name}")),
                 number: Set(pr_number as i32),
+                assignee: Set(github_pr
+                    .assignee
+                    .as_ref()
+                    .and_then(|author| Some(author.login.clone()))
+                    .unwrap_or(String::new())),
                 title: Set(github_pr.title.clone().unwrap()),
-                head_ref: Set(github_pr.head.label.clone().unwrap_or(String::from("<unknown>"))),
+                head_ref: Set(github_pr
+                    .head
+                    .label
+                    .clone()
+                    .unwrap_or(String::from("<unknown>"))),
                 url: Set(format!("https://github.com/{name}/pull/{pr_number}")),
                 ..Default::default()
             };
