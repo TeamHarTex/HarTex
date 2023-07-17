0b5ab86ee8e5c260fc0fd36c33229db511d95dd6
HTGAzureX1212 <39023054+HTG-YT@users.noreply.github.com>
2023-07-17 12:43:33 +0800

    validate embed and create response

--- discord-frontend/hartex-discord-commands/src/utilities/serverinfo.rs
+++ discord-frontend/hartex-discord-commands/src/utilities/serverinfo.rs
@@ -23,9 +23,13 @@
 use hartex_discord_commands_core::traits::Command;
 use hartex_discord_commands_core::CommandMetadata;
 use hartex_discord_core::discord::model::application::interaction::Interaction;
+use hartex_discord_core::discord::model::http::interaction::InteractionResponse;
+use hartex_discord_core::discord::model::http::interaction::InteractionResponseType;
 use hartex_discord_core::discord::util::builder::embed::EmbedBuilder;
+use hartex_discord_core::discord::util::builder::InteractionResponseDataBuilder;
 use hartex_discord_entitycache_core::traits::Repository;
 use hartex_discord_entitycache_repositories::guild::CachedGuildRepository;
+use hartex_discord_utils::CLIENT;
 use hartex_localization_core::create_bundle;
 use hartex_localization_core::handle_errors;
 use hartex_localization_macros::bundle_get_args;
@@ -40,6 +44,7 @@ pub struct ServerInfo;
 
 impl Command for ServerInfo {
     async fn execute(&self, interaction: Interaction) -> miette::Result<()> {
+        let interaction_client = CLIENT.interaction(interaction.application_id);
         let bundle = create_bundle(
             interaction.locale.and_then(|locale| locale.parse().ok()),
             &["discord-frontend", "commands"],
@@ -50,10 +55,29 @@ impl Command for ServerInfo {
         bundle_get_args!(bundle."serverinfo-embed-title": message, out [serverinfo_embed_title, errors], args ["serverName" to name]);
         handle_errors(errors)?;
 
-        let _ = EmbedBuilder::new()
+        let embed = EmbedBuilder::new()
             .color(0x41_A0_DE)
-            .title(serverinfo_embed_title);
+            .title(serverinfo_embed_title)
+            .validate()
+            .into_diagnostic()?
+            .build();
 
-        todo!()
+        interaction_client
+            .create_response(
+                interaction.id,
+                &interaction.token,
+                &InteractionResponse {
+                    kind: InteractionResponseType::ChannelMessageWithSource,
+                    data: Some(
+                        InteractionResponseDataBuilder::new()
+                            .embeds(vec![embed])
+                            .build(),
+                    ),
+                },
+            )
+            .await
+            .into_diagnostic()?;
+
+        Ok(())
     }
 }
