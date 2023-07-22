4a2cbed147cc8c1e68d551fd993cbd62403a2230
HTGAzureX1212 <39023054+HTG-YT@users.noreply.github.com>
2023-07-22 11:20:25 +0800

    outbound handling attempt

--- discord-frontend/hartex-discord-leader/src/main.rs
+++ discord-frontend/hartex-discord-leader/src/main.rs
@@ -44,10 +44,13 @@ use hartex_kafka_utils::traits::ClientConfigUtils;
 use hartex_kafka_utils::types::CompressionType;
 use hartex_log::log;
 use miette::IntoDiagnostic;
+use rdkafka::consumer::Consumer;
+use rdkafka::consumer::StreamConsumer;
 use rdkafka::producer::FutureProducer;
 use rdkafka::ClientConfig;
 
 mod inbound;
+mod outbound;
 mod queue;
 mod shards;
 
@@ -64,13 +67,22 @@ pub async fn main() -> miette::Result<()> {
         .split(';')
         .map(String::from)
         .collect::<Vec<_>>();
+    let topic = env::var("KAFKA_TOPIC_OUTBOUND_COMMUNICATION").into_diagnostic()?;
 
     let producer = ClientConfig::new()
-        .bootstrap_servers(bootstrap_servers.into_iter())
+        .bootstrap_servers(bootstrap_servers.clone().into_iter())
         .compression_type(CompressionType::Lz4)
         .delivery_timeout_ms(30000)
         .create::<FutureProducer>()
         .into_diagnostic()?;
+    let consumer = ClientConfig::new()
+        .bootstrap_servers(bootstrap_servers.into_iter())
+        .compression_type(CompressionType::Lz4)
+        .group_id("com.github.teamhartex.hartex.inbound.gateway.command.consumer")
+        .create::<StreamConsumer>()
+        .into_diagnostic()?;
+
+    consumer.subscribe(&[&topic]).into_diagnostic()?;
 
     log::trace!("building clusters");
     let num_shards = env::var("NUM_SHARDS")
@@ -88,6 +100,7 @@ pub async fn main() -> miette::Result<()> {
     tokio::spawn(async move {
         tokio::select! {
             _ = inbound::handle(shards.iter_mut(), producer) => {},
+            _ = outbound::handle(shards.iter_mut(), consumer) => {},
             _ = rx.changed() => {
                 future::join_all(shards.iter_mut().map(|shard: &mut Shard| shard.close(CloseFrame::RESUME))).await;
             },
