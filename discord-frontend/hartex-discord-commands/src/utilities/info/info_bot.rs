76872aa8126e616f7e8176353995adf8d0b622ca
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2023-07-25 16:43:59 +0800

    initial info bot things

--- discord-frontend/hartex-discord-commands/src/utilities/info/info_bot.rs
+++ discord-frontend/hartex-discord-commands/src/utilities/info/info_bot.rs
@@ -22,7 +22,48 @@
 
 use hartex_discord_core::discord::model::application::interaction::application_command::CommandDataOption;
 use hartex_discord_core::discord::model::application::interaction::Interaction;
+use hartex_discord_core::discord::model::http::interaction::InteractionResponse;
+use hartex_discord_core::discord::model::http::interaction::InteractionResponseType;
+use hartex_discord_core::discord::util::builder::embed::EmbedBuilder;
+use hartex_discord_core::discord::util::builder::InteractionResponseDataBuilder;
+use hartex_discord_utils::CLIENT;
+use hartex_localization_core::create_bundle;
+use hartex_localization_core::handle_errors;
+use miette::IntoDiagnostic;
+use hartex_localization_macros::bundle_get;
 
-pub async fn execute(_: Interaction, _: CommandDataOption) -> miette::Result<()> {
+pub async fn execute(interaction: Interaction, _: CommandDataOption) -> miette::Result<()> {
+    let interaction_client = CLIENT.interaction(interaction.application_id);
+    let bundle = create_bundle(
+        interaction.locale.and_then(|locale| locale.parse().ok()),
+        &["discord-frontend", "commands"],
+    )?;
+
+    bundle_get!(bundle."botinfo-embed-title": message, out [botinfo_embed_title, errors]);
+    handle_errors(errors)?;
+
+    let embed = EmbedBuilder::new()
+        .color(0x41_A0_DE)
+        .title(botinfo_embed_title)
+        .validate()
+        .into_diagnostic()?
+        .build();
+
+    interaction_client
+        .create_response(
+            interaction.id,
+            &interaction.token,
+            &InteractionResponse {
+                kind: InteractionResponseType::ChannelMessageWithSource,
+                data: Some(
+                    InteractionResponseDataBuilder::new()
+                        .embeds(vec![embed])
+                        .build(),
+                ),
+            },
+        )
+        .await
+        .into_diagnostic()?;
+
     Ok(())
 }
