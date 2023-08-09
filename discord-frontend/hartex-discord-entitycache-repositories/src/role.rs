33cb90dfc0495d38b002a21a2ddb8934929d4de0
HTGAzureX1212 <39023054+HTGAzureX1212@users.noreply.github.com>
2023-08-09 21:17:37 +0800

    add getting all roles in a guild

--- discord-frontend/hartex-discord-entitycache-repositories/src/role.rs
+++ discord-frontend/hartex-discord-entitycache-repositories/src/role.rs
@@ -22,14 +22,49 @@
 
 use std::env;
 
+use hartex_discord_core::discord::model::id::marker::GuildMarker;
+use hartex_discord_core::discord::model::id::marker::RoleMarker;
+use hartex_discord_core::discord::model::id::Id;
 use hartex_discord_entitycache_core::error::CacheResult;
 use hartex_discord_entitycache_core::traits::Entity;
 use hartex_discord_entitycache_core::traits::Repository;
 use hartex_discord_entitycache_entities::role::RoleEntity;
 use redis::AsyncCommands;
 use redis::Client;
+use serde_scan::scan;
 
 pub struct CachedRoleRepository;
+
+impl CachedRoleRepository {
+    #[allow(clippy::missing_errors_doc)]
+    #[allow(clippy::missing_panics_doc)]
+    pub async fn role_ids_in_guild(
+        &self,
+        guild_id: Id<GuildMarker>,
+    ) -> CacheResult<Vec<Id<RoleMarker>>> {
+        let pass = env::var("DOCKER_REDIS_REQUIREPASS")?;
+        let client = Client::open(format!("redis://:{pass}@127.0.0.1/"))?;
+        let mut sync_connection = client.get_connection()?;
+        let keys = redis::cmd("SCAN")
+            .cursor_arg(0)
+            .arg("MATCH")
+            .arg(format!("guild:{guild_id}:role:*:name"))
+            .arg("COUNT")
+            .arg("1000")
+            .clone()
+            .iter::<String>(&mut sync_connection)?
+            .collect::<Vec<_>>();
+
+        Ok(keys
+            .into_iter()
+            .map(|key| {
+                let (_, role_id): (u64, u64) = scan!("guild_:{}:role:{}:name" <- key).unwrap();
+
+                Id::new_checked(role_id).expect("unreachable")
+            })
+            .collect())
+    }
+}
 
 impl Repository<RoleEntity> for CachedRoleRepository {
     async fn get(&self, (guild_id, id): <RoleEntity as Entity>::Id) -> CacheResult<RoleEntity> {
@@ -41,11 +76,7 @@ impl Repository<RoleEntity> for CachedRoleRepository {
             .get::<String, String>(format!("guild:{guild_id}:role:{id}"))
             .await?;
 
-        Ok(RoleEntity {
-            guild_id,
-            id,
-            name,
-        })
+        Ok(RoleEntity { guild_id, id, name })
     }
 
     async fn upsert(&self, entity: RoleEntity) -> CacheResult<()> {
@@ -54,10 +85,7 @@ impl Repository<RoleEntity> for CachedRoleRepository {
         let mut connection = client.get_tokio_connection().await?;
         connection
             .set(
-                format!(
-                    "guild:{}:role:{}:name",
-                    entity.guild_id, entity.id
-                ),
+                format!("guild:{}:role:{}:name", entity.guild_id, entity.id),
                 entity.name,
             )
             .await?;
