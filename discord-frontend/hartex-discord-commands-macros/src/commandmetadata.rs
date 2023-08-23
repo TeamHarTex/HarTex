d7e577b33e6a56fbb1dcb46e137251bdb59c4b2e
HTGAzureX1212 <39023054+HTGAzureX1212@users.noreply.github.com>
2023-08-23 23:03:49 +0800

    allow dead code temp

--- discord-frontend/hartex-discord-commands-macros/src/commandmetadata.rs
+++ discord-frontend/hartex-discord-commands-macros/src/commandmetadata.rs
@@ -41,6 +41,7 @@ const LITERAL_PARAMETERS: [&str; 3] = ["command_type", "name", "minimum_level"];
 const VALID_ATTR_PARAMETER_NAMES: [&str; 4] =
     ["command_type", "interaction_only", "name", "minimum_level"];
 
+#[allow(dead_code)]
 pub struct MetadataMacroInput {
     pub(self) command_type_ident: Ident,
     pub(self) equal1: Token![=],
