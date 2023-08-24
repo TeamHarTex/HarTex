8184fe04b52a64f8cde471af2951351fc6ed5920
HTGAzureX1212 <39023054+HTGAzureX1212@users.noreply.github.com>
2023-08-24 19:05:18 +0800

    add some tests

---
+++ discord-frontend/hartex-discord-commands-test/tests/expand/attr.expanded.rs
@@ -0,0 +1,14 @@
+use hartex_discord_commands_core::metadata;
+extern crate hartex_discord_commands_core as _commands_core;
+#[automatically_derived]
+impl _commands_core::traits::CommandMetadata for Attr {
+    fn command_type(&self) -> u8 {
+        1
+    }
+    fn interaction_only(&self) -> bool {
+        true
+    }
+    fn name(&self) -> String {
+        String::from("attr")
+    }
+}
