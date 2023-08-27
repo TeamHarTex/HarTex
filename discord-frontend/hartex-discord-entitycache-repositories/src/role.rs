f806aef4e02e9e3ae6e28331b520bc35b33cec10
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2023-08-27 11:51:22 +0800

    cache: update role entity

--- discord-frontend/hartex-discord-entitycache-repositories/src/role.rs
+++ discord-frontend/hartex-discord-entitycache-repositories/src/role.rs
@@ -22,9 +22,11 @@
 
 use std::env;
 
+use hartex_discord_core::discord::model::guild::RoleFlags;
 use hartex_discord_core::discord::model::id::marker::GuildMarker;
 use hartex_discord_core::discord::model::id::marker::RoleMarker;
 use hartex_discord_core::discord::model::id::Id;
+use hartex_discord_core::discord::model::util::ImageHash;
 use hartex_discord_entitycache_core::error::CacheResult;
 use hartex_discord_entitycache_core::traits::Entity;
 use hartex_discord_entitycache_core::traits::Repository;
@@ -69,21 +71,97 @@ impl Repository<RoleEntity> for CachedRoleRepository {
         let client = Client::open(format!("redis://:{pass}@127.0.0.1/"))?;
         let mut connection = client.get_tokio_connection().await?;
 
+        let color = connection
+            .get::<String, u32>(format!("guild:{guild_id}:role:{id}:color"))
+            .await?;
+        let flags = connection
+            .get::<String, u64>(format!("guild:{guild_id}:role:{id}:flags"))
+            .await?;
         let name = connection
-            .get::<String, String>(format!("guild:{guild_id}:role:{id}"))
+            .get::<String, String>(format!("guild:{guild_id}:role:{id}:name"))
+            .await?;
+        let hoist = connection
+            .get::<String, bool>(format!("guild:{guild_id}:role:{id}:hoist"))
+            .await?;
+        let icon = connection
+            .get::<String, Option<String>>(format!("guild:{guild_id}:role:{id}:icon"))
+            .await?;
+        let managed = connection
+            .get::<String, bool>(format!("guild:{guild_id}:role:{id}:managed"))
+            .await?;
+        let mentionable = connection
+            .get::<String, bool>(format!("guild:{guild_id}:role:{id}:mentionable"))
+            .await?;
+        let position = connection
+            .get::<String, i64>(format!("guild:{guild_id}:role:{id}:position"))
             .await?;
 
-        Ok(RoleEntity { guild_id, id, name })
+        Ok(RoleEntity {
+            color,
+            flags: RoleFlags::from_bits_truncate(flags),
+            guild_id,
+            hoist,
+            icon: icon.map(|hash| ImageHash::parse(hash.as_bytes()).unwrap()),
+            id,
+            managed,
+            mentionable,
+            name,
+            position,
+        })
     }
 
     async fn upsert(&self, entity: RoleEntity) -> CacheResult<()> {
         let pass = env::var("DOCKER_REDIS_REQUIREPASS")?;
         let client = Client::open(format!("redis://:{pass}@127.0.0.1/"))?;
         let mut connection = client.get_tokio_connection().await?;
+
+
+        if let Some(icon) = entity.icon {
+            connection
+                .set(format!("guild:{}:role:{}:icon", entity.guild_id, entity.id), icon.to_string())
+                .await?;
+        }
+
+        connection
+            .set(
+                format!("guild:{}:role:{}:color", entity.guild_id, entity.id),
+                entity.color,
+            )
+            .await?;
+        connection
+            .set(
+                format!("guild:{}:role:{}:flags", entity.guild_id, entity.id),
+                entity.flags.bits(),
+            )
+            .await?;
+        connection
+            .set(
+                format!("guild:{}:role:{}:hoist", entity.guild_id, entity.id),
+                entity.hoist,
+            )
+            .await?;
+        connection
+            .set(
+                format!("guild:{}:role:{}:managed", entity.guild_id, entity.id),
+                entity.managed,
+            )
+            .await?;
+        connection
+            .set(
+                format!("guild:{}:role:{}:mentionable", entity.guild_id, entity.id),
+                entity.mentionable,
+            )
+            .await?;
         connection
             .set(
                 format!("guild:{}:role:{}:name", entity.guild_id, entity.id),
                 entity.name,
+            )
+            .await?;
+        connection
+            .set(
+                format!("guild:{}:role:{}:position", entity.guild_id, entity.id),
+                entity.position,
             )
             .await?;
 
