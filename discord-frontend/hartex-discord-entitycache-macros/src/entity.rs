e4636dd44e463ebeb478f33069d4795e648fa585
HTGAzureX1212 <39023054+HTGAzureX1212@users.noreply.github.com>
2023-09-17 12:04:38 +0800

    add general macro param structure

--- discord-frontend/hartex-discord-entitycache-macros/src/entity.rs
+++ discord-frontend/hartex-discord-entitycache-macros/src/entity.rs
@@ -20,18 +20,45 @@
  * with HarTex. If not, see <https://www.gnu.org/licenses/>.
  */
 
+use proc_macro2::Ident;
 use proc_macro2::TokenStream;
 use syn::parse::Parse;
 use syn::parse::ParseStream;
+use syn::ExprArray;
 use syn::ItemStruct;
+use syn::LitStr;
+use syn::Token;
 
+#[allow(dead_code)]
 pub struct EntityMacroInput {
-
+    pub(crate) from_ident: Ident,
+    pub(crate) equal1: Token![=],
+    pub(crate) from_lit: LitStr,
+    pub(crate) comma1: Token![,],
+    pub(crate) include_ident: Ident,
+    pub(crate) equal2: Token![=],
+    pub(crate) include_array: ExprArray,
+    pub(crate) comma2: Token![,],
+    pub(crate) omit_ident: Ident,
+    pub(crate) equal3: Token![=],
+    pub(crate) omit_array: ExprArray,
 }
 
 impl Parse for EntityMacroInput {
-    fn parse(_: ParseStream) -> syn::Result<Self> {
-        todo!()
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        Ok(Self {
+            from_ident: input.parse()?,
+            equal1: input.parse()?,
+            from_lit: input.parse()?,
+            comma1: input.parse()?,
+            include_ident: input.parse(),
+            equal2: input.parse()?,
+            include_array: input.parse(),
+            comma2: input.parse()?,
+            omit_ident: input.parse()?,
+            equal3: input.parse()?,
+            omit_array: input.parse()?,
+        })
     }
 }
 
