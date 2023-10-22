6c4ded3968b5902a24b31e197df0f9817738c767
HTGAzureX1212 <39023054+HTGAzureX1212@users.noreply.github.com>
2023-10-22 14:33:33 +0800

    fix tests

--- discord-frontend/hartex-discord-entitycache-test/tests/ui/fail/does_not_exist_or_older_version.rs
+++ discord-frontend/hartex-discord-entitycache-test/tests/ui/fail/does_not_exist_or_older_version.rs
@@ -22,7 +22,7 @@
 
 use hartex_discord_entitycache_macros::entity;
 
-#[entity(from = "twilight_model::DoesNotExistOrOlderVersion", id = [], include = [])]
+#[entity(from = "twilight_model::DoesNotExistOrOlderVersion", id = [], include = [], extra = [])]
 pub struct DoesNotExistOrOlderVersion;
 
 fn main() {}
