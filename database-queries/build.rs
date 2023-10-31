9902b07954836395084a9248ec22b125b57224bd
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2023-10-31 21:27:42 +0800

    add some stuff

--- database-queries/build.rs
+++ database-queries/build.rs
@@ -20,6 +20,25 @@
  * with HarTex. If not, see <https://www.gnu.org/licenses/>.
  */
 
+use std::env;
+
+use cornucopia::CodegenSettings;
+use postgres::Client;
+use postgres::NoTls;
+
 pub fn main() {
     dotenvy::dotenv().unwrap();
+
+    let queries_path = "queries";
+
+    let url = env::var("POSTGRES_PGSQL_URL").unwrap();
+    cornucopia::generate_live(
+        &mut Client::connect(&url, NoTls).unwrap(),
+        queries_path,
+        Some("generated/queries.rs"),
+        CodegenSettings {
+            derive_ser: false,
+            is_async: true,
+        }
+    ).unwrap();
 }
