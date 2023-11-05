2914bfeb1aa5a496c88c717b73c6bc21ba3ff993
HTGAzureX1212 <39023054+HTGAzureX1212@users.noreply.github.com>
2023-11-05 13:44:03 +0800

    actually, I can just add things to the backend...

--- bors/hartex-bors-dashboard/src/home.rs
+++ bors/hartex-bors-dashboard/src/home.rs
@@ -20,57 +20,18 @@
  * with HarTex. If not, see <https://www.gnu.org/licenses/>.
  */
 
-use hartex_bors_core::DatabaseClient;
 use leptos::component;
 use leptos::logging;
 use leptos::view;
-use leptos::CollectView;
-use leptos::ErrorBoundary;
-use leptos::Errors;
 use leptos::IntoView;
-use leptos::Resource;
-use leptos::RwSignal;
-use leptos::Transition;
-
-use crate::DatabaseClientContext;
 
 #[component]
 pub fn Home() -> impl IntoView {
     logging::log!("rendering Home component");
 
-    let context = leptos::expect_context::<DatabaseClientContext>();
-    let resource = Resource::once(|| async { context.0.get_repositories().await });
-
-    let error_fallback = move |_: RwSignal<Errors>| {
-        view! {
-            <p>"Could not load repositories due to an error."</p>
-        }
-    };
-
-    let loading_fallback = move || {
-        view! {
-            <p>"Loading repositories..."</p>
-        }
-    };
-
-    let repositories_view = move || {
-        resource.and_then(|data| {
-            data.iter()
-                .map(|repository| view! {
-                <li><a href={format!("queue/{}", repository.name.clone())}>{repository.name.clone()}</a></li>
-            })
-                .collect_view()
-        })
-    };
-
     view! {
         <h1>"Bors V2"</h1>
 
         <h2>"Repositories"</h2>
-        <ErrorBoundary fallback=error_fallback>
-            <Transition fallback=loading_fallback>
-                <ul>{repositories_view}</ul>
-            </Transition>
-        </ErrorBoundary>
     }
 }
