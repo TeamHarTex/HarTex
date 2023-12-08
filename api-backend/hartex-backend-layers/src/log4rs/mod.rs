d271acd87f70791eca948481800321ddc87a2148
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2023-12-08 21:11:12 +0800

    idk

--- api-backend/hartex-backend-layers/src/log4rs/mod.rs
+++ api-backend/hartex-backend-layers/src/log4rs/mod.rs
@@ -20,7 +20,62 @@
  * with HarTex. If not, see <https://www.gnu.org/licenses/>.
  */
 
+use std::fmt::Display;
+use std::task::Context;
+use std::task::Poll;
+
+use http::Request;
+use http::Response;
+use http_body::Body;
+use tower_http::classify::MakeClassifier;
+use tower_service::Service;
+pub use layer::Log4rsLayer;
+
+use crate::log4rs::make_metadata::DefaultMakeMetadata;
+
+mod body;
 mod layer;
 mod make_metadata;
 
-pub use layer::Log4rsLayer;
+#[derive(Clone, Copy, Debug)]
+pub struct Log4rs<S, M, MakeMetadata = DefaultMakeMetadata> {
+    pub(crate) inner: S,
+    pub(crate) make_classifier: M,
+    pub(crate) make_metadata: MakeMetadata,
+}
+
+impl<S, M> Log4rs<S, M> {
+    pub fn new(inner: S, make_classifier: M) -> Self {
+        Self {
+            inner,
+            make_classifier,
+            make_metadata: DefaultMakeMetadata::new(),
+        }
+    }
+
+    pub fn layer(make_classifier: M) -> Log4rsLayer<M> {
+        Log4rsLayer::new(make_classifier)
+    }
+}
+
+impl<S, M, RequestBodyT, ResponseBodyT> Service<Request<RequestBodyT>> for Log4rs<S, M>
+where
+    S: Service<Request<RequestBodyT>, Response = Response<ResponseBodyT>>,
+    RequestBodyT: Body,
+    ResponseBodyT: Body,
+    ResponseBodyT::Error: Display + 'static,
+    M: MakeClassifier,
+    M::Classifier: Clone,
+{
+    type Response = ();
+    type Error = S::Error;
+    type Future = ();
+
+    fn poll_ready(&mut self, _: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        todo!()
+    }
+
+    fn call(&mut self, _: Request<RequestBodyT>) -> Self::Future {
+        todo!()
+    }
+}
