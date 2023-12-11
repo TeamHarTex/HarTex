f0ba7491fe2d8c6c750d86bd195eefd929429e79
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2023-12-11 19:41:02 +0800

    remove unnecessary compression parameter for consumer

--- discord-frontend/hartex-discord-leader/src/main.rs
+++ discord-frontend/hartex-discord-leader/src/main.rs
@@ -76,7 +76,6 @@ pub async fn main() -> miette::Result<()> {
         .into_diagnostic()?;
     let consumer = ClientConfig::new()
         .bootstrap_servers(bootstrap_servers.into_iter())
-        .compression_type(CompressionType::Lz4)
         .group_id("com.github.teamhartex.hartex.inbound.gateway.command.consumer")
         .create::<StreamConsumer>()
         .into_diagnostic()?;
