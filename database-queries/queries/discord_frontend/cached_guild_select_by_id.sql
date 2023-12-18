98da1ff3c888e4119c245e48ed2a57e11f852c9c
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2023-12-18 22:18:15 +0800

    add new query

--- database-queries/queries/discord_frontend/cached_guild_select_by_id.sql
+++ database-queries/queries/discord_frontend/cached_guild_select_by_id.sql
@@ -1,5 +1,7 @@
 --! cached_guild_select_by_id : (default_message_notifications, explicit_content_filter, features, icon?, large, name, owner_id, id)
-SELECT * FROM
+SELECT 
+    *
+FROM
     "DiscordFrontendNightly".public."CachedGuilds"
 WHERE
     "id" = :id;
