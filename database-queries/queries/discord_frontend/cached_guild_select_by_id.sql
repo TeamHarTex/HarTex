dbe84c97a86e1ac15ee44da545e9dba7351ee28a
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2023-12-30 19:02:35 +0800

    update guild cache stuff

--- database-queries/queries/discord_frontend/cached_guild_select_by_id.sql
+++ database-queries/queries/discord_frontend/cached_guild_select_by_id.sql
@@ -1,4 +1,4 @@
---! cached_guild_select_by_id : (default_message_notifications, explicit_content_filter, features, icon?, large, name, owner_id, id)
+--! cached_guild_select_by_id : (default_message_notifications, explicit_content_filter, features, icon?, large, name, owner_id, id, premium_subscription_count?, premium_tier)
 SELECT 
     *
 FROM
