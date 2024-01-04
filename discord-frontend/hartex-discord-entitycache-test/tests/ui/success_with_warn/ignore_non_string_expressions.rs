642855dbd90f7c6bc307fad54db2b9be9292f2c4
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2024-01-04 19:34:38 +0800

    update macro tests

--- discord-frontend/hartex-discord-entitycache-test/tests/ui/success_with_warn/ignore_non_string_expressions.rs
+++ discord-frontend/hartex-discord-entitycache-test/tests/ui/success_with_warn/ignore_non_string_expressions.rs
@@ -22,7 +22,13 @@
 
 use hartex_discord_entitycache_macros::entity;
 
-#[entity(from = "twilight_model::channel::Channel", id = [], include = [true], extra = [])]
+#[entity(
+    from = "twilight_model::channel::Channel",
+    id = [],
+    include = [true],
+    extra = [],
+    relates = [],
+)]
 pub struct IgnoreNonStringExpressions;
 
 fn main() {}
