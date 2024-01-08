57027a405cc2937c74fdd7b09c0f606aec3022c0
Ariz <63549306+Lunari8546@users.noreply.github.com>
2024-01-08 20:42:12 +1100

    CtA button.

--- web-frontend/App.vue
+++ web-frontend/App.vue
@@ -37,7 +37,8 @@ with HarTex. If not, see <https://www.gnu.org/licenses/>.
 }
 
 a {
-  @apply decoration-none text-tertiary transition-colors;
+  @apply decoration-none select-none;
+  @apply text-tertiary transition-colors;
 }
 
 a:hover {
@@ -63,6 +64,15 @@ section:first-of-type {
 section:last-of-type {
   @apply mb-0;
 }
+
+.primary-btn {
+      @apply mx-6 px-4 py-2 text-2xl;
+      @apply border-solid border-rd border-1;
+    }
+
+    .primary-btn:hover {
+      @apply bg-secondary text-primary;
+    }
 
 .inline-btn {
   @apply text-primary underline;
