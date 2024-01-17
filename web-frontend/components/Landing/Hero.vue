665bd95ef91b000eea58223222d82b37f661dca9
Ariz <63549306+Lunari8546@users.noreply.github.com>
2024-01-17 15:20:26 +1100

    Modularise landing components.

---
+++ web-frontend/components/Landing/Hero.vue
@@ -0,0 +1,31 @@
+<template>
+  <header>
+    <h1>
+      Discord server management,<br/>
+      <span>reimagined</span>.
+    </h1>
+    <NuxtLink class="primary-btn">Add to Discord</NuxtLink>
+  </header>
+</template>
+
+<style scoped lang="postcss">
+header {
+  @apply h-[95vh] flex flex-col items-center justify-center;
+
+  h1 {
+    @apply text-5xl md:text-6xl 5xl:text-8xl font-500 text-center;
+
+    span {
+      @apply text-secondary font-serif;
+    }
+  }
+
+  .primary-btn {
+    @apply mt-20 cursor-not-allowed opacity-50;
+  }
+
+  .primary-btn:hover {
+    @apply bg-primary text-tertiary;
+  }
+}
+</style>
