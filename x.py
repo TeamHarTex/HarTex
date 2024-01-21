b9b130a3a62d6e210784676276047afc34031255
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2024-01-21 23:51:30 +0800

    update errors

--- x.py
+++ x.py
@@ -12,18 +12,16 @@ if __name__ == "__main__":
 
     skip = os.environ.get("HARTEX_IGNORE_OLD_PYTHON") == "1"
     if not skip and (major < 3 or (major == 3 and minor < 11)):
-        msg = cleandoc(f"""
-            Using Python {major}.{minor}, but Python version 3.11 or higher is recommended.
-            This Python version should work for the near future, but eventually this will be
-            changed.
+        print(f"""DISCLAIMER:
+Using Python {major}.{minor}, but Python version 3.11 or higher is recommended.
+This Python version should work for the near future, but eventually this will be
+changed.
 
-            This message can be suppressed by setting `HARTEX_IGNORE_OLD_PYTHON=1`.
+This message can be suppressed by setting `HARTEX_IGNORE_OLD_PYTHON=1`.
         """)
 
-        warnings.warn(msg, stacklevel=1)
+    pwd = os.path.dirname(os.path.abspath(__file__))
+    sys.path.insert(0, os.path.join(pwd, "bootstrap"))
 
-        pwd = os.path.dirname(os.path.absname(__file__))
-        sys.path.insert(0, os.path.join(pwd, "bootstrap"))
-
-        import bootstrap
-        bootstrap.main()
+    import bootstrap
+    bootstrap.main()
