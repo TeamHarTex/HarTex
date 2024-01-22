38eee410114b42d0a59d95d0f13764cdd1a20799
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2024-01-22 15:41:16 +0800

    add deserialize ini

--- bootstrap/src/config/mod.rs
+++ bootstrap/src/config/mod.rs
@@ -20,13 +20,18 @@
  * with HarTex. If not, see <https://www.gnu.org/licenses/>.
  */
 
+use std::fs;
 use std::path::absolute;
+use std::path::Path;
 use std::path::PathBuf;
+use std::process::exit;
 
 use self::flags::BootstrapSubcommand;
 use self::flags::Flags;
+use self::ini::IniConfig;
 
 pub mod flags;
+pub mod ini;
 
 pub struct Config {
     pub bypass_fs_lock: bool,
@@ -38,10 +43,21 @@ pub struct Config {
 
 impl Config {
     pub fn parse_from_args(args: &[String]) -> Self {
-        Self::parse_from_args_inner(args)
+        Self::parse_from_args_inner(args, |path| {
+            let content = fs::read_to_string(path)
+                .expect(&format!("configuration file not found: {}", path.display()));
+
+            serde_ini::from_str(&content).unwrap_or_else(|error| {
+                eprintln!(
+                    "failed to parse configuration file {}: {error}",
+                    path.display()
+                );
+                exit(2);
+            })
+        })
     }
 
-    fn parse_from_args_inner(args: &[String]) -> Self {
+    fn parse_from_args_inner(args: &[String], get_ini: impl Fn(&Path) -> IniConfig) -> Self {
         let flags = Flags::parse_from_args(args);
         let mut config = Self::default();
 
@@ -53,11 +69,14 @@ impl Config {
                 .expect("failed to resolve absolute path of output directory");
         }
 
-        if let Some(config_path) = config.config_path.clone()
+        let _ = if let Some(config_path) = config.config_path.clone()
             && !config_path.exists()
         {
             config.config_path.replace(config.root.join(config_path));
-        }
+            get_ini(config.config_path.as_ref().unwrap())
+        } else {
+            IniConfig::default()
+        };
 
         config
     }
@@ -69,7 +88,7 @@ impl Default for Config {
 
         Self {
             bypass_fs_lock: false,
-            config_path: Some(PathBuf::from("config.toml")),
+            config_path: Some(PathBuf::from("hartex.conf")),
             output_dir: PathBuf::from("build"),
             root: manifest_dir.parent().unwrap().to_owned(),
             subcommand: BootstrapSubcommand::Build,
