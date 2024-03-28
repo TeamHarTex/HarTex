2d8c03508fd254247031ca734a42a677247c15dc
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2024-03-28 17:30:57 +0800

    improve debugging

--- discord-frontend/hartex-discord-worker/src/interaction.rs
+++ discord-frontend/hartex-discord-worker/src/interaction.rs
@@ -20,6 +20,7 @@
  * with HarTex. If not, see <https://www.gnu.org/licenses/>.
  */
 
+use miette::IntoDiagnostic;
 use hartex_discord_commands::general::about::About;
 use hartex_discord_commands::general::contributors::Contributors;
 use hartex_discord_commands::utilities::info::Info;
@@ -27,7 +28,10 @@ use hartex_discord_commands_core::traits::Command;
 use hartex_discord_commands_core::traits::CommandMetadata;
 use hartex_discord_core::discord::model::application::interaction::InteractionData;
 use hartex_discord_core::discord::model::gateway::payload::incoming::InteractionCreate;
-use tracing::log;
+use hartex_discord_core::discord::model::http::interaction::{InteractionResponse, InteractionResponseType};
+use hartex_discord_core::discord::util::builder::InteractionResponseDataBuilder;
+use hartex_discord_utils::CLIENT;
+use hartex_log::log;
 
 /// Handle an application command interaction
 #[allow(clippy::large_futures)]
@@ -39,10 +43,32 @@ pub async fn application_command(interaction_create: Box<InteractionCreate>) -> 
 
     log::trace!("running interaction command {}", &command.name);
 
-    match command.name {
-        name if name == About.name() => About.execute(interaction_create.0).await,
-        name if name == Contributors.name() => Contributors.execute(interaction_create.0).await,
-        name if name == Info.name() => Info.execute(interaction_create.0).await,
+    let cloned = interaction_create.clone();
+    if let Err(error) = match command.name {
+        name if name == About.name() => About.execute(cloned.0).await,
+        name if name == Contributors.name() => Contributors.execute(cloned.0).await,
+        name if name == Info.name() => Info.execute(cloned.0).await,
         _ => Ok(()),
+    } {
+        let interaction_client = CLIENT.interaction(interaction_create.application_id);
+        interaction_client
+            .create_response(
+                interaction_create.id,
+                &interaction_create.token,
+                &InteractionResponse {
+                    kind: InteractionResponseType::ChannelMessageWithSource,
+                    data: Some(
+                        InteractionResponseDataBuilder::new()
+                            .content("This command encountered an unexpected error.")
+                            .build(),
+                    ),
+                },
+            )
+            .await
+            .into_diagnostic()?;
+
+        log::warn!("unexpected error occurred for command {}: {error:?}", &command.name);
     }
+
+    Ok(())
 }
