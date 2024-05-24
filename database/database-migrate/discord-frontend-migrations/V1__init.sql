02ecbf51b3ad4020d0501d06ee116da7ea7b6843
HTGAzureX1212 <39023054+HTGAzureX1212@users.noreply.github.com>
2024-05-24 13:37:00 +0800

    update database frontend

--- database/database-migrate/discord-frontend-migrations/V1__init.sql
+++ database/database-migrate/discord-frontend-migrations/V1__init.sql
@@ -7,3 +7,50 @@ CREATE TABLE IF NOT EXISTS "Nightly"."CachedEmojis" (
     "name" TEXT NOT NULL,
     "managed" BOOLEAN NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS "Nightly"."CachedGuilds" (
+    "default_message_notifications" SMALLINT NOT NULL,
+    "explicit_content_filter" SMALLINT NOT NULL,
+    "features" TEXT[] NOT NULL,
+    "icon" TEXT,
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "large" BOOLEAN NOT NULL,
+    "mfa_level" SMALLINT NOT NULL,
+    "name" TEXT NOT NULL,
+    "owner_id" TEXT NOT NULL,
+    "premium_subscription_count" BIGINT,
+    "premium_tier" SMALLINT NOT NULL,
+    "verification_level" SMALLINT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS "Nightly"."CachedMembers" (
+    "flags" BIGINT NOT NULL,
+    "joined_at" TIMESTAMP WITH TIME ZONE
+    "nick" TEXT,
+    "roles" TEXT[] NOT NULL,
+    "guild_id" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    PRIMARY KEY("guild_id", "user_id")
+);
+
+CREATE TABLE IF NOT EXISTS "Nightly"."CachedRoles" (
+    "color" BIGINT NOT NULL,
+    "flags" INTEGER NOT NULL,
+    "guild_id" TEXT NOT NULL,
+    "hoist" BOOLEAN NOT NULL,
+    "icon" TEXT,
+    "id" TEXT NOT NULL,
+    "managed" BOOLEAN NOT NULL,
+    "mentionable" BOOLEAN NOT NULL,
+    "position" INTEGER NOT NULL,
+    PRIMARY KEY("guild_id", "id")
+);
+
+CREATE TABLE IF NOT EXISTS "Nightly"."CachedUsers" (
+    "avatar" TEXT,
+    "bot" BOOLEAN NOT NULL,
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "discriminator" TEXT NOT NULL,
+    "global_name" TEXT,
+    "name" TEXT NOT NULL
+);
