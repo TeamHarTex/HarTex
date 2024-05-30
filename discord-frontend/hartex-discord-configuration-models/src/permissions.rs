b11dbbb6c7cee6efdf3ad103d9fa3751093b261e
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2024-05-30 22:13:39 +0800

    add permissions object stub for configuration models

---
+++ discord-frontend/hartex-discord-configuration-models/src/permissions.rs
@@ -0,0 +1,43 @@
+/*
+ * SPDX-License-Identifier: AGPL-3.0-only
+ *
+ * This file is part of HarTex.
+ *
+ * HarTex
+ * Copyright (c) 2021-2024 HarTex Project Developers
+ *
+ * HarTex is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * HarTex is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with HarTex. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+use std::collections::HashMap;
+
+use mlua::FromLua;
+use mlua::Lua;
+use mlua::MultiValue;
+use serde::Serialize;
+
+/// The permissions configuration object,
+#[derive(Debug, Serialize)]
+pub struct Permissions {
+    /// Permissions map for roles.
+    pub roles: HashMap<String, u8>,
+    /// Permissions map for users.
+    pub users: HashMap<String, u8>,
+}
+
+impl<'lua> FromLua<'lua> for Permissions {
+    fn from_lua(_: MultiValue<'lua>, _: &'lua Lua) -> mlua::Result<Self> {
+        todo!()
+    }
+}
