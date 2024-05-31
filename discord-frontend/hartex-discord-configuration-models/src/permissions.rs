4856cf1b9f16459b49101d4aef17513fb5192b02
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2024-05-31 10:22:30 +0800

    attempt to fix errors

--- discord-frontend/hartex-discord-configuration-models/src/permissions.rs
+++ discord-frontend/hartex-discord-configuration-models/src/permissions.rs
@@ -69,15 +69,21 @@ impl<'lua> FromLua<'lua> for Permissions {
             roles: roles
                 .pairs::<String, u8>()
                 .map_ok(|(key, value)| {
-                    (Id::from_str(key.as_str()), value)
+                    Id::from_str(key.as_str())
+                        .map(|id| (id, value))
+                        .map_err(|error| Error::runtime("invalid id"))
                 })
-                .collect(),
+                .map(|result| result.flatten())
+                .collect::<Result<HashMap<_, _>, _>>()?,
             users: users
                 .pairs::<String, u8>()
                 .map_ok(|(key, value)| {
-                    (Id::from_str(key.as_str()), value)
+                    Id::from_str(key.as_str())
+                        .map(|id| (id, value))
+                        .map_err(|error| Error::runtime("invalid id"))
                 })
-                .collect(),
+                .map(|result| result.flatten())
+                .collect::<Result<HashMap<_, _>, _>>()?,
         })
     }
 }
