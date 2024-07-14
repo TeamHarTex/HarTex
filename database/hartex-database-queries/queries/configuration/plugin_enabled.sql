8cef047869216f977f4bc180441309bf5f8e4930
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2024-07-14 16:25:17 +0800

    fix plugin enabled query

--- database/hartex-database-queries/queries/configuration/plugin_enabled.sql
+++ database/hartex-database-queries/queries/configuration/plugin_enabled.sql
@@ -1,8 +1,10 @@
 --! plugin_enabled (guild_id, plugin)
-SELECT
-    TRUE
-FROM
-    "Nightly"."GuildConfigurations"
-WHERE
-    "enabled_plugins" @> array[ :plugin ] AND
-    "guild_id" = :guild_id;
+SELECT EXISTS(
+    SELECT
+        TRUE
+    FROM
+        "Nightly"."GuildConfigurations"
+    WHERE
+        "enabled_plugins" @> array[ :plugin ] AND
+        "guild_id" = :guild_id
+);
