18216191df06bcd64ecb6be5d3eced66777ef99b
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2024-07-14 09:17:52 +0800

    use arrays for enabled plugins

--- database/hartex-database-queries/generated/configuration.rs
+++ database/hartex-database-queries/generated/configuration.rs
@@ -3,79 +3,26 @@
 #[allow(clippy::all, clippy::pedantic)] #[allow(unused_variables)]
 #[allow(unused_imports)] #[allow(dead_code)] pub mod types { }#[allow(clippy::all, clippy::pedantic)] #[allow(unused_variables)]
 #[allow(unused_imports)] #[allow(dead_code)] pub mod queries
-{ pub mod management_plugin_enabled
-{ use futures::{{StreamExt, TryStreamExt}};use futures; use cornucopia_async::GenericClient;pub struct SerdejsonValueQuery<'a, C: GenericClient, T, const N: usize>
+{ pub mod plugin_enabled
+{ use futures::{{StreamExt, TryStreamExt}};use futures; use cornucopia_async::GenericClient;#[derive( Debug)] pub struct PluginEnabledParams<T1: cornucopia_async::StringSql,T2: cornucopia_async::StringSql,> { pub plugin: T1,pub guild_id: T2,}#[derive( Debug, Clone, PartialEq,)] pub struct PluginEnabled
+{ pub guild_id : String,pub dashboard_admins : Vec<String>,pub dashboard_editors : Vec<String>,pub dashboard_viewers : Vec<String>,pub appearance_nickname : String,pub appearance_colour : i64,pub enabled_plugins : Vec<String>,}pub struct PluginEnabledBorrowed<'a> { pub guild_id : &'a str,pub dashboard_admins : cornucopia_async::ArrayIterator<'a, &'a str>,pub dashboard_editors : cornucopia_async::ArrayIterator<'a, &'a str>,pub dashboard_viewers : cornucopia_async::ArrayIterator<'a, &'a str>,pub appearance_nickname : &'a str,pub appearance_colour : i64,pub enabled_plugins : cornucopia_async::ArrayIterator<'a, &'a str>,}
+impl<'a> From<PluginEnabledBorrowed<'a>> for PluginEnabled
+{
+    fn from(PluginEnabledBorrowed { guild_id,dashboard_admins,dashboard_editors,dashboard_viewers,appearance_nickname,appearance_colour,enabled_plugins,}: PluginEnabledBorrowed<'a>) ->
+    Self { Self { guild_id: guild_id.into(),dashboard_admins: dashboard_admins.map(|v| v.into()).collect(),dashboard_editors: dashboard_editors.map(|v| v.into()).collect(),dashboard_viewers: dashboard_viewers.map(|v| v.into()).collect(),appearance_nickname: appearance_nickname.into(),appearance_colour,enabled_plugins: enabled_plugins.map(|v| v.into()).collect(),} }
+}pub struct PluginEnabledQuery<'a, C: GenericClient, T, const N: usize>
 {
     client: &'a  C, params:
     [&'a (dyn postgres_types::ToSql + Sync); N], stmt: &'a mut
-    cornucopia_async::private::Stmt, extractor: fn(&tokio_postgres::Row) -> postgres_types::Json<& serde_json::value::RawValue>,
-    mapper: fn(postgres_types::Json<& serde_json::value::RawValue>) -> T,
-} impl<'a, C, T:'a, const N: usize> SerdejsonValueQuery<'a, C, T, N> where C:
+    cornucopia_async::private::Stmt, extractor: fn(&tokio_postgres::Row) -> PluginEnabledBorrowed,
+    mapper: fn(PluginEnabledBorrowed) -> T,
+} impl<'a, C, T:'a, const N: usize> PluginEnabledQuery<'a, C, T, N> where C:
 GenericClient
 {
-    pub fn map<R>(self, mapper: fn(postgres_types::Json<& serde_json::value::RawValue>) -> R) ->
-    SerdejsonValueQuery<'a,C,R,N>
+    pub fn map<R>(self, mapper: fn(PluginEnabledBorrowed) -> R) ->
+    PluginEnabledQuery<'a,C,R,N>
     {
-        SerdejsonValueQuery
-        {
-            client: self.client, params: self.params, stmt: self.stmt,
-            extractor: self.extractor, mapper,
-        }
-    } pub async fn one(self) -> Result<T, tokio_postgres::Error>
-    {
-        let stmt = self.stmt.prepare(self.client).await?; let row =
-        self.client.query_one(stmt, &self.params).await?;
-        Ok((self.mapper)((self.extractor)(&row)))
-    } pub async fn all(self) -> Result<Vec<T>, tokio_postgres::Error>
-    { self.iter().await?.try_collect().await } pub async fn opt(self) ->
-    Result<Option<T>, tokio_postgres::Error>
-    {
-        let stmt = self.stmt.prepare(self.client).await?;
-        Ok(self.client.query_opt(stmt, &self.params) .await?
-        .map(|row| (self.mapper)((self.extractor)(&row))))
-    } pub async fn iter(self,) -> Result<impl futures::Stream<Item = Result<T,
-    tokio_postgres::Error>> + 'a, tokio_postgres::Error>
-    {
-        let stmt = self.stmt.prepare(self.client).await?; let it =
-        self.client.query_raw(stmt,
-        cornucopia_async::private::slice_iter(&self.params)) .await?
-        .map(move |res|
-        res.map(|row| (self.mapper)((self.extractor)(&row)))) .into_stream();
-        Ok(it)
-    }
-}pub fn management_plugin_enabled() -> ManagementPluginEnabledStmt
-{ ManagementPluginEnabledStmt(cornucopia_async::private::Stmt::new("SELECT
-    configuration -> 'plugins' -> 'management' -> 'enabled'
-FROM
-    \"Nightly\".\"GuildConfigurations\"
-WHERE
-    guild_id = $1")) } pub struct
-ManagementPluginEnabledStmt(cornucopia_async::private::Stmt); impl ManagementPluginEnabledStmt
-{ pub fn bind<'a, C:
-GenericClient,T1:
-cornucopia_async::StringSql,>(&'a mut self, client: &'a  C,
-guild_id: &'a T1,) -> SerdejsonValueQuery<'a,C,
-serde_json::Value, 1>
-{
-    SerdejsonValueQuery
-    {
-        client, params: [guild_id,], stmt: &mut self.0, extractor:
-        |row| { row.get(0) }, mapper: |it| { serde_json::from_str(it.0.get()).unwrap() },
-    }
-} }}pub mod plugin_enabled
-{ use futures::{{StreamExt, TryStreamExt}};use futures; use cornucopia_async::GenericClient;#[derive( Debug)] pub struct PluginEnabledParams<T1: cornucopia_async::StringSql,T2: cornucopia_async::StringSql,> { pub plugin: T1,pub guild_id: T2,}pub struct StringQuery<'a, C: GenericClient, T, const N: usize>
-{
-    client: &'a  C, params:
-    [&'a (dyn postgres_types::ToSql + Sync); N], stmt: &'a mut
-    cornucopia_async::private::Stmt, extractor: fn(&tokio_postgres::Row) -> & str,
-    mapper: fn(& str) -> T,
-} impl<'a, C, T:'a, const N: usize> StringQuery<'a, C, T, N> where C:
-GenericClient
-{
-    pub fn map<R>(self, mapper: fn(& str) -> R) ->
-    StringQuery<'a,C,R,N>
-    {
-        StringQuery
+        PluginEnabledQuery
         {
             client: self.client, params: self.params, stmt: self.stmt,
             extractor: self.extractor, mapper,
@@ -104,90 +51,32 @@ GenericClient
     }
 }pub fn plugin_enabled() -> PluginEnabledStmt
 { PluginEnabledStmt(cornucopia_async::private::Stmt::new("SELECT
-    COALESCE(MAX(configuration -> 'plugins' -> $1 ->> 'enabled'), 'false')
+    *
 FROM
     \"Nightly\".\"GuildConfigurations\"
 WHERE
-    guild_id = $2")) } pub struct
+    \"enabled_plugins\" @> array[ $1 ] AND
+    \"guild_id\" = $2")) } pub struct
 PluginEnabledStmt(cornucopia_async::private::Stmt); impl PluginEnabledStmt
 { pub fn bind<'a, C:
 GenericClient,T1:
 cornucopia_async::StringSql,T2:
 cornucopia_async::StringSql,>(&'a mut self, client: &'a  C,
-plugin: &'a T1,guild_id: &'a T2,) -> StringQuery<'a,C,
-String, 2>
+plugin: &'a T1,guild_id: &'a T2,) -> PluginEnabledQuery<'a,C,
+PluginEnabled, 2>
 {
-    StringQuery
+    PluginEnabledQuery
     {
         client, params: [plugin,guild_id,], stmt: &mut self.0, extractor:
-        |row| { row.get(0) }, mapper: |it| { it.into() },
+        |row| { PluginEnabledBorrowed { guild_id: row.get(0),dashboard_admins: row.get(1),dashboard_editors: row.get(2),dashboard_viewers: row.get(3),appearance_nickname: row.get(4),appearance_colour: row.get(5),enabled_plugins: row.get(6),} }, mapper: |it| { <PluginEnabled>::from(it) },
     }
 } }impl <'a, C: GenericClient,T1: cornucopia_async::StringSql,T2: cornucopia_async::StringSql,> cornucopia_async::Params<'a,
-PluginEnabledParams<T1,T2,>, StringQuery<'a, C,
-String, 2>, C> for PluginEnabledStmt
+PluginEnabledParams<T1,T2,>, PluginEnabledQuery<'a, C,
+PluginEnabled, 2>, C> for PluginEnabledStmt
 {
     fn
     params(&'a mut self, client: &'a  C, params: &'a
-    PluginEnabledParams<T1,T2,>) -> StringQuery<'a, C,
-    String, 2>
+    PluginEnabledParams<T1,T2,>) -> PluginEnabledQuery<'a, C,
+    PluginEnabled, 2>
     { self.bind(client, &params.plugin,&params.guild_id,) }
-}}pub mod utilities_plugin_enabled
-{ use futures::{{StreamExt, TryStreamExt}};use futures; use cornucopia_async::GenericClient;pub struct SerdejsonValueQuery<'a, C: GenericClient, T, const N: usize>
-{
-    client: &'a  C, params:
-    [&'a (dyn postgres_types::ToSql + Sync); N], stmt: &'a mut
-    cornucopia_async::private::Stmt, extractor: fn(&tokio_postgres::Row) -> postgres_types::Json<& serde_json::value::RawValue>,
-    mapper: fn(postgres_types::Json<& serde_json::value::RawValue>) -> T,
-} impl<'a, C, T:'a, const N: usize> SerdejsonValueQuery<'a, C, T, N> where C:
-GenericClient
-{
-    pub fn map<R>(self, mapper: fn(postgres_types::Json<& serde_json::value::RawValue>) -> R) ->
-    SerdejsonValueQuery<'a,C,R,N>
-    {
-        SerdejsonValueQuery
-        {
-            client: self.client, params: self.params, stmt: self.stmt,
-            extractor: self.extractor, mapper,
-        }
-    } pub async fn one(self) -> Result<T, tokio_postgres::Error>
-    {
-        let stmt = self.stmt.prepare(self.client).await?; let row =
-        self.client.query_one(stmt, &self.params).await?;
-        Ok((self.mapper)((self.extractor)(&row)))
-    } pub async fn all(self) -> Result<Vec<T>, tokio_postgres::Error>
-    { self.iter().await?.try_collect().await } pub async fn opt(self) ->
-    Result<Option<T>, tokio_postgres::Error>
-    {
-        let stmt = self.stmt.prepare(self.client).await?;
-        Ok(self.client.query_opt(stmt, &self.params) .await?
-        .map(|row| (self.mapper)((self.extractor)(&row))))
-    } pub async fn iter(self,) -> Result<impl futures::Stream<Item = Result<T,
-    tokio_postgres::Error>> + 'a, tokio_postgres::Error>
-    {
-        let stmt = self.stmt.prepare(self.client).await?; let it =
-        self.client.query_raw(stmt,
-        cornucopia_async::private::slice_iter(&self.params)) .await?
-        .map(move |res|
-        res.map(|row| (self.mapper)((self.extractor)(&row)))) .into_stream();
-        Ok(it)
-    }
-}pub fn utilities_plugin_enabled() -> UtilitiesPluginEnabledStmt
-{ UtilitiesPluginEnabledStmt(cornucopia_async::private::Stmt::new("SELECT
-    configuration -> 'plugins' -> 'utilities' -> 'enabled'
-FROM
-    \"Nightly\".\"GuildConfigurations\"
-WHERE
-    guild_id = $1")) } pub struct
-UtilitiesPluginEnabledStmt(cornucopia_async::private::Stmt); impl UtilitiesPluginEnabledStmt
-{ pub fn bind<'a, C:
-GenericClient,T1:
-cornucopia_async::StringSql,>(&'a mut self, client: &'a  C,
-guild_id: &'a T1,) -> SerdejsonValueQuery<'a,C,
-serde_json::Value, 1>
-{
-    SerdejsonValueQuery
-    {
-        client, params: [guild_id,], stmt: &mut self.0, extractor:
-        |row| { row.get(0) }, mapper: |it| { serde_json::from_str(it.0.get()).unwrap() },
-    }
-} }}}
+}}}
