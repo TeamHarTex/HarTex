f1c8619294e3c4863af29a7c9279b08e2316cc0b
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2024-07-16 23:42:14 +0800

    fix typo

--- discord-frontend/hartex-discord-configuration-models/src/appearance.rs
+++ discord-frontend/hartex-discord-configuration-models/src/appearance.rs
@@ -41,7 +41,7 @@ impl<'lua> FromLua<'lua> for Appearance {
     fn from_lua(lua_value: Value<'lua>, _: &'lua Lua) -> mlua::Result<Self> {
         let Value::Table(table) = lua_value.clone() else {
             return Err(Error::RuntimeError(format!(
-                "Appearance: mismatched value type, exoected table, found: {}",
+                "Appearance: mismatched value type, expected table, found: {}",
                 lua_value.type_name()
             )));
         };
