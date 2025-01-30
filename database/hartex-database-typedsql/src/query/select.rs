84530c006f96eb32b026e7d9f7d81eb0cb56380c
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2025-01-30 14:31:01 +0800

    database: start parsing sql

--- database/hartex-database-typedsql/src/query/select.rs
+++ database/hartex-database-typedsql/src/query/select.rs
@@ -22,7 +22,12 @@
 
 use std::collections::HashMap;
 
+use convert_case::Case;
+use convert_case::Casing;
+use sqlparser::ast::ObjectName;
 use sqlparser::ast::Select;
+use sqlparser::ast::SelectItem;
+use sqlparser::ast::TableFactor;
 
 use crate::schema::ColumnInfo;
 use crate::schema::SchemaInfo;
@@ -32,17 +37,46 @@ use crate::schema::TableInfo;
 pub(crate) enum SelectWhat {
     Everything,
     Columns(Vec<ColumnInfo>),
+    Exists(Box<SelectQueryInfo>),
+    True,
 }
 
 #[derive(Clone, Debug)]
 pub(crate) struct SelectQueryInfo {
     pub(crate) what: SelectWhat,
-    pub(crate) from: TableInfo,
+    pub(crate) from: Option<TableInfo>,
 }
 
 pub(crate) fn parse_select_query(
     select: Select,
-    _: HashMap<String, SchemaInfo>,
+    schema_infos: HashMap<String, SchemaInfo>,
 ) -> crate::error::Result<super::QueryInfo> {
-    Err(crate::error::Error::QueryFile("todo"))
+    let what = match select.projection.first() {
+        Some(SelectItem::Wildcard(_)) if select.projection.len() == 1 => SelectWhat::Everything,
+        _ => {
+            return Err(crate::error::Error::QueryFile(
+                "unsupported selection projection",
+            ));
+        }
+    };
+
+    let from = if let Some(tablewj) = select.from.first()
+        && let TableFactor::Table { ref name, .. } = tablewj.relation
+    {
+        let schema_name = name
+            .0
+            .first()
+            .ok_or(crate::error::Error::QueryFile("schema name not found"))?;
+        let key = schema_name.value.to_case(Case::Snake);
+        let schema_info = schema_infos
+            .get(&key)
+            .ok_or(crate::error::Error::QueryFile("schema not found"))?;
+
+        let table_key = ObjectName(name.0[1..].to_vec()).to_string();
+        schema_info.tables.get(&table_key).cloned()
+    } else {
+        None
+    };
+
+    Ok(super::QueryInfo::Select(SelectQueryInfo { what, from }))
 }
