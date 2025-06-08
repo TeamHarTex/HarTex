ca2097936f5741b9253f04419092c8adbb20332f
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2025-06-08 11:54:55 +0800

    migrate to discord frontend

---
+++ rust-utilities/hartex-discord-utils/src/database.rs
@@ -0,0 +1,42 @@
+/*
+ * SPDX-License-Identifier: AGPL-3.0-only
+ *
+ * This file is part of HarTex.
+ *
+ * HarTex
+ * Copyright (c) 2021-2025 HarTex Project Developers
+ *
+ * HarTex is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * HarTex is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with HarTex. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+use std::env;
+use std::str::FromStr;
+use std::time::Duration;
+
+use async_lazy::Lazy;
+use log::LevelFilter;
+use sqlx::ConnectOptions;
+use sqlx::PgPool;
+use sqlx::postgres::PgConnectOptions;
+use sqlx::postgres::PgPoolOptions;
+
+/// An asynchronously lazyily initialized database pool.
+pub static DISCORD_FRONTEND: Lazy<PgPool> = Lazy::new(|| Box::pin(async {
+    let hartex_pgsql_url = env::var("DISCORD_FRONTEND_PGSQL_URL").unwrap();
+    let options = PgConnectOptions::from_str(&hartex_pgsql_url)
+        .unwrap()
+        .log_statements(LevelFilter::Debug)
+        .log_slow_statements(LevelFilter::Debug, Duration::from_secs(1));
+    PgPoolOptions::new().connect_with(options).await.unwrap()
+}));
