cf6250cc13aac99659fe8f77f9eb020d665d0ac6
HTGAzureX1212 <39023054+HTGAzureX1212@users.noreply.github.com>
2025-06-09 13:06:29 +0800

    revert post

--- api-backend/hartex-backend-routes/src/uptime.rs
+++ api-backend/hartex-backend-routes/src/uptime.rs
@@ -43,7 +43,7 @@ use hartex_discord_utils::database::API_BACKEND;
 #[allow(clippy::missing_panics_doc)] // this function cannot panic
 #[allow(clippy::module_name_repetitions)]
 #[utoipa::path(
-    post,
+    get,
     path = "/api/v1/stats/uptime",
     params(UptimeQuery),
     responses(
@@ -53,7 +53,7 @@ use hartex_discord_utils::database::API_BACKEND;
         (status = 500, description = "Generic internal server error")
     )
 )]
-pub async fn post_uptime(
+pub async fn get_uptime(
     WithRejection(Query(query), _): WithRejection<Query<UptimeQuery>, UptimeQueryRejection>,
 ) -> (StatusCode, Json<Response<UptimeResponse, String>>) {
     hartex_tracing::trace!("querying timestamp");
