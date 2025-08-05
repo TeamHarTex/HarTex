27812be45f52a87e539d4b910c7ee5a9962c8ed0
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2025-08-05 14:28:50 +0800

    appease clippy

--- discord-frontend/hartex-discord-worker/src/grpc.rs
+++ discord-frontend/hartex-discord-worker/src/grpc.rs
@@ -126,7 +126,7 @@ impl Gateway for GatewayWorkerServer {
         tokio::spawn(async move {
             while let Some(result) = stream.next().await {
                 let Ok(message) = result else {
-                    let Ok(_) = response_tx
+                    let Ok(()) = response_tx
                         .send(Err(Status::aborted(
                             "failed to retrieve message from payload",
                         )))
@@ -143,7 +143,7 @@ impl Gateway for GatewayWorkerServer {
                     message.total_chunks,
                     message.shard_id,
                 ) {
-                    let Ok(_) = response_tx
+                    let Ok(()) = response_tx
                         .send(Err(Status::invalid_argument(
                             "inconsistent total_chunks for same event_seq",
                         )))
@@ -163,7 +163,7 @@ impl Gateway for GatewayWorkerServer {
                     message.chunk_data,
                     &internal_tx,
                 ) {
-                    let Ok(_) = response_tx
+                    let Ok(()) = response_tx
                         .send(Err(Status::invalid_argument(format!(
                             "duplicate chunk {} in payload",
                             message.nth_chunk
