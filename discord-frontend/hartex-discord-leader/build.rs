9d8acf2f33445771d0d00fb13572ddc1e487018d
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2025-12-18 22:32:53 +0800

    feat: add compile time information about version

--- discord-frontend/hartex-discord-leader/build.rs
+++ discord-frontend/hartex-discord-leader/build.rs
@@ -20,24 +20,14 @@
  * with HarTex. If not, see <https://www.gnu.org/licenses/>.
  */
 
-use git_version::git_version;
-use rootcause::Report;
-use tracing::subscriber;
+use time::{Month, OffsetDateTime};
 
-// TODO: put this somewhere else
-type HarTexResult<T> = Result<T, Report>;
-
-#[tokio::main]
-pub async fn main() -> HarTexResult<()> {
-    subscriber::set_global_default(hartex_tracing::subscriber())?;
-
-    tracing::info!(
-        "HarTex {} ({} {})",
-        env!("CARGO_PKG_VERSION"),
-        git_version!(),
-        env!("CARGO_BUILD_DATE")
+pub fn main() {
+    let time = OffsetDateTime::now_utc();
+    println!(
+        "cargo::rustc-env=CARGO_BUILD_DATE={}-{}-{}",
+        time.year(),
+        <Month as Into<u8>>::into(time.month()),
+        time.day()
     );
-    tracing::debug!("starting up...");
-
-    Ok(())
 }
